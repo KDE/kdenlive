a18e81a33ba94058a7cabc2ac3ee13a2eb6616cf
Jason Wood <jasonwood@blueyonder.co.uk>
2002-12-15 00:39:44 +0000

    Added KMoveClipsCommand - undo/redo functionality class
    svn path=/trunk/kdenlive/; revision=108

---
+++ kdenlive/kmoveclipscommand.cpp
@@ -0,0 +1,69 @@
+/***************************************************************************
+                          kmoveclipscommand.cpp  -  description
+                             -------------------
+    begin                : Thu Dec 12 2002
+    copyright            : (C) 2002 by Jason Wood
+    email                : jasonwood@blueyonder.co.uk
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "kmoveclipscommand.h"
+#include "kdenlivedoc.h"
+#include "kmmtimeline.h"
+#include <klocale.h>
+#include <kdebug.h>
+
+KMoveClipsCommand::KMoveClipsCommand(KMMTimeLine *timeline, KdenliveDoc *doc, DocClipBase *master)
+													: KCommand()
+{
+	m_doc = doc;
+	m_timeline = timeline;
+	m_startTrack = doc->trackIndex(doc->findTrack(master));
+	if(m_startTrack == -1) {
+		kdError() << "KMoveClipCommand created, but master clip is not in document!" << endl;
+		return;
+	}
+
+	// Choose a "safe" time in the middle of the master clip, so that we are certain to be able to find it again when
+	// we need to.
+	m_startTime = master->trackStart();
+}
+
+KMoveClipsCommand::~KMoveClipsCommand()
+{
+}
+
+/** Returns the (translated) name of this command */
+QString KMoveClipsCommand::name() const
+{
+	return i18n("Move Clips");
+}
+
+/** Specifies where the master clip should be moved to by this command. */
+void KMoveClipsCommand::setEndLocation(DocClipBase *master)
+{
+	m_endTrack = m_doc->trackIndex(m_doc->findTrack(master));
+	m_endTime = master->trackStart();
+}
+
+/** Executes this command */
+void KMoveClipsCommand::execute()
+{
+	m_doc->moveSelectedClips(m_endTime - m_startTime, m_endTrack - m_startTrack);
+	m_timeline->drawTrackViewBackBuffer();
+}
+
+/** Unexecute this command */
+void KMoveClipsCommand::unexecute()
+{
+	m_doc->moveSelectedClips(m_startTime - m_endTime, m_startTrack - m_endTrack);
+	m_timeline->drawTrackViewBackBuffer();	
+}
---
+++ kdenlive/kmoveclipscommand.h
@@ -0,0 +1,61 @@
+/***************************************************************************
+                          kmoveclipscommand.h  -  description
+                             -------------------
+    begin                : Thu Dec 12 2002
+    copyright            : (C) 2002 by Jason Wood
+    email                : jasonwood@blueyonder.co.uk
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KMOVECLIPSCOMMAND_H
+#define KMOVECLIPSCOMMAND_H
+
+#include <qwidget.h>
+#include <qstring.h>
+#include <kcommand.h>
+#include <gentime.h>
+
+class KdenliveDoc;
+class DocClipBase;
+class KMMTimeLine;
+
+/**This command handles the moving of clips, and the "unexecuting" of command, for undo/redo pruposes.
+  *@author Jason Wood
+  */
+
+class KMoveClipsCommand : public KCommand  {
+public: 
+	KMoveClipsCommand(KMMTimeLine *timeline, KdenliveDoc *doc, DocClipBase *master);
+	~KMoveClipsCommand();
+  /** Returns the (translated) name of this command */
+  QString name() const;
+  /** Unexecute this command */
+  void unexecute();
+  /** Executes this command */
+  void execute();
+  /** Specifies where the master clip should be moved to by this command. */
+  void setEndLocation(DocClipBase *master);
+private:
+	KdenliveDoc *m_doc;
+	KMMTimeLine *m_timeline;
+
+	/** The track the master clip is on before execution */
+	int m_startTrack;
+	/** The start of the master clip before execution */
+	GenTime m_startTime;
+
+	/** The track the master clip is on after execution */	
+	int m_endTrack;
+	/** The start of the master clip after execution */	
+	GenTime m_endTime;
+};
+
+#endif
