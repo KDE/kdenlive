ebe57e242baadefc7bbb82a2b4648cae9b7f5060
Jason Wood <jasonwood@blueyonder.co.uk>
2003-03-29 21:22:55 +0000

    Lots of changes, mostly to do with the behaviour of kdenlive when piave is not available.
    svn path=/trunk/kdenlive/; revision=202

---
+++ kdenlive/clippropertiesdialog.cpp
@@ -0,0 +1,31 @@
+/***************************************************************************
+                          clippropertiesdialog.cpp  -  description
+                             -------------------
+    begin                : Mon Mar 17 2003
+    copyright            : (C) 2003 by Jason Wood
+    email                : jasonwood@blueyonder.co.uk
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "clippropertiesdialog.h"
+
+ClipPropertiesDialog::ClipPropertiesDialog(QWidget *parent, const char *name ):
+                          KDialog(parent,name)
+{
+}
+
+ClipPropertiesDialog::~ClipPropertiesDialog()
+{
+}
+
+void ClipPropertiesDialog::setClip(const AVFile *clip)
+{
+}
---
+++ kdenlive/clippropertiesdialog.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+                          clippropertiesdialog.h  -  description
+                             -------------------
+    begin                : Mon Mar 17 2003
+    copyright            : (C) 2003 by Jason Wood
+    email                : jasonwood@blueyonder.co.uk
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef CLIPPROPERTIESDIALOG_H
+#define CLIPPROPERTIESDIALOG_H
+
+#include <qwidget.h>
+#include <kdialog.h>
+
+class AVFile;
+
+/**Displays the properties of a sepcified clip.
+  *@author Jason Wood
+  */
+
+class ClipPropertiesDialog : public KDialog  {
+   Q_OBJECT
+public: 
+	ClipPropertiesDialog(QWidget *parent=0, const char *name=0);
+	~ClipPropertiesDialog();
+  /** Specifies the clip that we wish to display the properties of. */
+  void setClip(const AVFile *clip);
+};
+
+#endif
