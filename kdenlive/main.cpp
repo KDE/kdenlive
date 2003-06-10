5181e927c36586f74d1b7c6d91a3056f060e7d63
Caulier Gilles <cauliergilles@users.sourceforge.net>
2003-06-10 20:24:26 +0000

    Add URL and messages on about dialog box
    svn path=/trunk/kdenlive/; revision=217

--- kdenlive/main.cpp
+++ kdenlive/main.cpp
@@ -21,11 +21,10 @@
 
 #include "kdenlive.h"
 
-static const char *description =
-	I18N_NOOP("Kdenlive");
-// INSERT A DESCRIPTION FOR YOUR APPLICATION HERE
-	
-	
+
+static const char *description = I18N_NOOP("Kdenlive"
+                    "\nA non-linear video editor for KDE");
+
 static KCmdLineOptions options[] =
 {
   { "+[File]", I18N_NOOP("file to open"), 0 },
@@ -35,10 +34,20 @@ static KCmdLineOptions options[] =
 
 int main(int argc, char *argv[])
 {
-	KAboutData aboutData( "kdenlive", I18N_NOOP("Kdenlive"),
-		VERSION, description, KAboutData::License_GPL,
-		"(c) 2002, Jason Wood", 0, 0, "jasonwood@blueyonder.co.uk");
-	aboutData.addAuthor("Jason Wood",0, "jasonwood@blueyonder.co.uk");
+	KAboutData aboutData("kdenlive",
+	                     I18N_NOOP("Kdenlive"),
+		             VERSION,
+			     description,
+			     KAboutData::License_GPL,
+		             "(c) 2002-2003, Jason Wood",
+			     0,
+			     "http://www.uchian.pwp.blueyonder.co.uk/kdenlive.html",
+			     "jasonwood@blueyonder.co.uk");
+
+	aboutData.addAuthor("Jason Wood",
+			    I18N_NOOP("Author and Maintainer"),
+			    "jasonwood@blueyonder.co.uk");
+
 	KCmdLineArgs::init( argc, argv, &aboutData );
 	KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
 
