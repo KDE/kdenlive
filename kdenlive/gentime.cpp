a7a7065bc3d1c4cc1f2c673ca3439aa94584af08
Jean-Baptiste Mardelle <jb@kdenlive.org>
2006-02-28 20:56:56 +0000

    Fix annoying bug that caused wrong timings
    svn path=/trunk/kdenlive/; revision=500

--- kdenlive/gentime.cpp
+++ kdenlive/gentime.cpp
@@ -46,7 +46,7 @@ double GenTime::ms() const
 /** Returns the time in frames, after being given the number of frames per second */
 double GenTime::frames(double framesPerSecond) const
 {
-    return m_time * framesPerSecond;
+    return (int) floor(m_time * framesPerSecond + 0.5);
 }
 
 GenTime::~GenTime()
