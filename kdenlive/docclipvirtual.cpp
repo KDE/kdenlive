1c0e2578b0b9b055ea28330b692cf96859158745
Jean-Baptiste Mardelle <jb@kdenlive.org>
2006-11-04 22:40:39 +0000

    oups, missing files
    svn path=/trunk/kdenlive/; revision=1007

---
+++ kdenlive/docclipvirtual.cpp
@@ -0,0 +1,265 @@
+/***************************************************************************
+                          doccliptextfile.cpp  -  description
+                             -------------------
+    begin                : Jan 31 2006
+    copyright            : (C) 2006 by Jean-Baptiste Mardelle
+    email                : jb@ader.ch
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "docclipvirtual.h"
+
+#include <iostream>
+#include <assert.h>
+
+#include <qfileinfo.h>
+
+#include <kdebug.h>
+#include <klocale.h>
+
+#include "clipmanager.h"
+#include "kdenlivesettings.h"
+
+
+DocClipVirtual::DocClipVirtual(const KURL & url, const QString & name, const QString & text, GenTime start, GenTime end, uint id):DocClipBase(), m_start(start), m_end(end), m_clipType(VIRTUAL), m_duration(end - start), m_durationKnown(true), m_framesPerSecond(KdenliveSettings::defaultfps()), m_url(url)
+{
+    setName(name);
+    setId(id);
+    setDescription( text );
+    thumbCreator = new KThumb();
+    //setThumbnail(NULL);
+}
+
+
+DocClipVirtual::DocClipVirtual(QDomDocument node):DocClipBase(),m_clipType(VIRTUAL), m_duration(0.0),  m_durationKnown(false), m_framesPerSecond(KdenliveSettings::defaultfps())
+{
+QDomElement element = node.documentElement(); 
+ if (element.tagName() != "kdenliveclip") {
+	kdWarning() <<
+	    "DocClipRef::createClip() element has unknown tagName : " <<
+	    element.tagName() << endl;
+	return;
+    }
+
+    QDomNode n = element.firstChild();
+
+    while (!n.isNull()) {
+	QDomElement e = n.toElement();
+	if (!e.isNull()) {
+	    //kdWarning() << "DocClipRef::createClip() tag = " << e.tagName() << endl;
+	    if (e.tagName() == "avfile") {
+		kdDebug()<<"++ FOUND CLIP"<<endl;
+                setId(e.attribute("id", "-1").toInt());
+		m_url = KURL(e.attribute("url", QString::null));
+		m_width = e.attribute("width", "0").toInt();
+		m_height = e.attribute("height", "0").toInt();
+		m_start = GenTime(e.attribute("virtualstart", "0").toInt(), m_framesPerSecond);
+		m_end = GenTime(e.attribute("virtualend", "0").toInt(), m_framesPerSecond);
+		m_durationKnown = e.attribute("durationknown", "0" ).toInt();
+		m_duration = m_end - m_start;
+		setName(e.attribute("name", QString::null));
+		setDescription(e.attribute("description", QString::null));
+	    }
+	}
+	n = n.nextSibling();
+    }
+    thumbCreator = new KThumb();
+
+}
+
+DocClipVirtual::~DocClipVirtual()
+{
+}
+
+const GenTime & DocClipVirtual::duration() const
+{
+    return m_duration;
+}
+
+const DocClipBase::CLIPTYPE & DocClipVirtual::clipType() const
+{
+    return m_clipType;
+}
+
+
+const KURL & DocClipVirtual::fileURL() const
+{
+    return m_url;
+}
+
+
+DocClipVirtual *DocClipVirtual::createClip(const QDomElement element)
+{
+}
+
+bool DocClipVirtual::durationKnown() const
+{
+    return m_durationKnown;
+}
+
+// virtual
+double DocClipVirtual::framesPerSecond() const
+{
+    return m_framesPerSecond;
+}
+
+//returns clip video properties -reh
+uint DocClipVirtual::clipHeight() const
+{
+    return KdenliveSettings::defaultheight();
+}
+
+uint DocClipVirtual::clipWidth() const
+{
+    return KdenliveSettings::defaultwidth();
+}
+
+// virtual
+QDomDocument DocClipVirtual::sceneToXML(const GenTime & startTime,
+    const GenTime & endTime) const
+{
+}
+
+// virtual
+QDomDocument DocClipVirtual::generateSceneList(bool) const
+{
+
+    QDomDocument sceneList;
+    QDomElement westley = sceneList.createElement("westley");
+    sceneList.appendChild(westley);
+
+    QDomElement producer = sceneList.createElement("producer");
+    producer.setAttribute("id", 0);
+    producer.setAttribute("mlt_service", "westley");
+    producer.setAttribute("resource", fileURL().path());
+    westley.appendChild(producer);
+    QDomElement playlist = sceneList.createElement("playlist");
+    playlist.setAttribute("in", "0");
+    playlist.setAttribute("out", QString::number(duration().frames(m_framesPerSecond)));
+    QDomElement entry = sceneList.createElement("entry");
+    entry.setAttribute("producer", 0);
+    playlist.appendChild(entry);
+    westley.appendChild(playlist);
+
+    return sceneList;
+
+}
+
+void DocClipVirtual::populateSceneTimes(QValueVector < GenTime >
+    &toPopulate) const
+{
+    toPopulate.append(GenTime(0));
+    toPopulate.append(duration());
+}
+
+GenTime DocClipVirtual::virtualStartTime() const
+{
+    return m_start;
+}
+
+GenTime DocClipVirtual::virtualEndTime() const
+{
+    return m_end;
+}
+
+uint DocClipVirtual::fileSize() const
+{
+    return 0;
+}
+
+uint DocClipVirtual::numReferences() const
+{
+#warning TODO - write this funtion.
+}
+
+
+// virtual
+bool DocClipVirtual::referencesClip(DocClipBase * clip) const
+{
+    return this == clip;
+}
+
+// virtual
+QDomDocument DocClipVirtual::toXML() const
+{
+    QDomDocument doc = DocClipBase::toXML();
+    QDomNode node = doc.firstChild();
+
+    while (!node.isNull()) {
+	QDomElement element = node.toElement();
+	if (!element.isNull()) {
+	    if (element.tagName() == "kdenliveclip") {
+		QDomElement avfile = doc.createElement("avfile");
+		avfile.setAttribute("type", m_clipType);
+                avfile.setAttribute("id", getId());
+		avfile.setAttribute("url", m_url.path());
+		avfile.setAttribute("duration", m_duration.frames(m_framesPerSecond));
+		avfile.setAttribute("virtualstart", m_start.frames(m_framesPerSecond));
+		avfile.setAttribute("virtualend", m_end.frames(m_framesPerSecond));
+		avfile.setAttribute("durationknown", m_durationKnown );
+		avfile.setAttribute("description", description());
+		avfile.setAttribute("name", name());
+		avfile.setAttribute("width", m_width);
+		avfile.setAttribute("height", m_height);
+		element.appendChild(avfile);
+		
+		return doc;
+	    }
+	}
+	node = node.nextSibling();
+    }
+
+
+    assert(node.isNull());
+
+    /* This final return should never be reached, it is here to remove compiler warning. */
+    return doc;
+}
+
+// virtual
+bool DocClipVirtual::matchesXML(const QDomElement & element) const
+{
+    bool result = false;
+
+    if (element.tagName() == "kdenliveclip") {
+	bool found = false;
+	QDomNode n = element.firstChild();
+	while (!n.isNull()) {
+	    QDomElement avElement = n.toElement();	// try to convert the node to an element.
+
+	    if (!avElement.isNull()) {
+		if (avElement.tagName() == "avfile") {
+		    if (found) {
+			kdWarning() <<
+			    "Clip contains multiple avclip definitions, only matching XML of the first one,"
+			    << endl;
+			break;
+		    } else {
+			found = true;
+			//if(avElement.attribute("url") == fileURL().url()) {
+			if (avElement.attribute("id") ==
+			    QString::number(getId())) {
+			    result = true;
+			}
+		    }
+		}
+	    }
+
+	    n = n.nextSibling();
+	}
+    }
+
+    return result;
+}
+
+
+
---
+++ kdenlive/docclipvirtual.h
@@ -0,0 +1,143 @@
+/***************************************************************************
+                          doccliptextfile.h  -  description
+                             -------------------
+    begin                : Jan 31 2006
+    copyright            : (C) 2006 by Jean-Baptiste Mardelle
+    email                : jb@ader.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef DOCCLIPVIRTUAL_H
+#define DOCCLIPVIRTUAL_H
+
+#include <qstring.h>
+#include <qdom.h>
+
+#include "gentime.h"
+#include "docclipbase.h"
+
+class ClipManager;
+
+/**
+	* Encapsulates a video, audio, picture, title, or any other kind of file that Kdenlive can support.
+	* Each type of file should be encapsulated in it's own class, which should inherit this one.
+  *@author Jason Wood
+  */
+
+class DocClipVirtual:public DocClipBase {
+  Q_OBJECT public:
+
+    DocClipVirtual(const KURL & url, const QString & name, const QString & text, GenTime start, GenTime end, uint id);
+    DocClipVirtual(QDomDocument node);
+
+    ~DocClipVirtual();
+    QString fileName();
+    double aspectRatio() const;
+
+	/** Returns the filesize of the underlying avfile. */
+    virtual uint fileSize() const;
+
+	/** Returns the number of references to the underlying avfile. */
+    uint numReferences() const;
+
+	/** Returns the duration of the file */
+    const GenTime & duration() const;
+
+    GenTime virtualEndTime() const;
+    GenTime virtualStartTime() const;
+
+	/** Set clip duration */
+    void setDuration(const GenTime & duration);
+
+	/** Returns the type of this clip */
+    const DocClipBase::CLIPTYPE & clipType() const;
+
+    QDomDocument toXML() const;
+	/** Returns the url of the AVFile this clip contains */
+    
+    const KURL & fileURL() const;
+	/** Creates a clip from the passed QDomElement. This only pertains to those details specific
+	 *  to DocClipVirtual. This action should only occur via the clipManager.*/
+    static DocClipVirtual *createClip(const QDomElement element);
+	/** Returns true if the clip duration is known, false otherwise. */
+    virtual bool durationKnown() const;
+    // Returns the number of frames per second that this clip should play at.
+    virtual double framesPerSecond() const;
+
+    //returns clip video properties -reh
+    uint clipWidth() const;
+    uint clipHeight() const;
+    // Appends scene times for this clip to the passed vector.
+    virtual void populateSceneTimes(QValueVector < GenTime >
+	&toPopulate) const;
+
+    virtual QDomDocument generateSceneList(bool addProducers = true) const;
+
+    // Returns an XML document that describes part of the current scene.
+    virtual QDomDocument sceneToXML(const GenTime & startTime,
+	const GenTime & endTime) const;
+
+	/** Returns true if this clip refers to the clip passed in. For a DocClipVirtual, this
+	 * is true if this == clip */
+    virtual bool referencesClip(DocClipBase * clip) const;
+
+	/** Returns true if this clip has a meaningful filesize. */
+    virtual bool hasFileSize() const {
+	return true;
+    }
+	/** returns true if the xml passed matches the values in the clip */
+	virtual bool matchesXML(const QDomElement & element) const;
+
+    virtual bool isDocClipVirtual() const {
+	return true;
+    } 
+
+    virtual bool isDocClipTextFile() const {
+      return false;
+    } 
+    
+    virtual DocClipVirtual *toDocClipVirtual() {
+	return this;
+  }
+  
+  virtual bool isDocClipAVFile() const {
+      return false;
+  } 
+    
+  virtual DocClipAVFile *toDocClipAVFile() {
+      return 0;
+  }
+
+  virtual DocClipTextFile *toDocClipTextFile() {
+      return 0;
+  }  
+   
+    private:
+	/** A play object factory, used for calculating information, and previewing files */
+	/** Determines whether this file contains audio, video or both. */
+     DocClipBase::CLIPTYPE m_clipType;
+	/** The duration of this file. */
+    GenTime m_duration;
+	/** True if the duration of this AVFile is known, false otherwise. */
+    bool m_durationKnown;
+	/** The number of frames per second that this AVFile runs at. */
+    double m_framesPerSecond;
+	/** a unique numeric id */
+    uint m_id;
+    GenTime m_start;
+    GenTime m_end;
+    KURL m_url;
+    //extended video file properties -reh
+    uint m_height;
+    uint m_width;
+};
+
+#endif
