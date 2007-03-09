2aab8a1298daaa7e373e13661e5b57f797239707
Jean-Baptiste Mardelle <jb@kdenlive.org>
2007-03-09 09:52:49 +0000

    Proper fix for compile with latest FFMPEG
    svn path=/trunk/kdenlive/; revision=1415

--- kdenlive/krender.cpp
+++ kdenlive/krender.cpp
@@ -22,11 +22,6 @@
  *                                                                         *
  ***************************************************************************/
 
-#ifndef INT64_C
-#define INT64_C(c) (c##LL)
-#define UINT64_C(c) (c##ULL)
-#endif
-
 extern "C" {
 #include <ffmpeg/avformat.h>
 }
