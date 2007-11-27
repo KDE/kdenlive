d7cfedcdcb682d334c010417097ee8c72a6d60e7
Jean-Baptiste Mardelle <jb@kdenlive.org>
2007-11-27 19:07:01 +0000

    revert last change that broke compilation
    svn path=/trunk/kdenlive/; revision=1741

--- cmake/modules/FindLIBMLT.cmake
+++ cmake/modules/FindLIBMLT.cmake
@@ -8,8 +8,8 @@ MESSAGE(STATUS "Found MLT INSTALL PATH: ${MLT_CONFIG_EXECUTABLE} , ${MLT_PATH}")
 SET(CMAKE_CXX_FLAGS -DMLT_PREFIX=\\\"\"${MLT_PATH}\"\\\")
 
 FIND_PATH(LIBMLT_INCLUDE_DIR 
-  NAMES mlt/framework/mlt.h
-  PATHS ${MLT_PATH}/include /usr/local/include /usr/include
+  NAMES framework/mlt.h
+  PATHS ${MLT_PATH}/include/mlt /usr/local/include/mlt /usr/include/mlt
   NO_DEFAULT_PATH
 )
 
