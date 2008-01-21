4683df18f692cd52738d5a5ac0b28d8a47cb72f5
Jean-Baptiste Mardelle <jb@kdenlive.org>
2008-01-21 22:19:15 +0000

    fix label repaint
    svn path=/branches/KDE4/; revision=1812

--- src/labelitem.cpp
+++ src/labelitem.cpp
@@ -46,17 +46,14 @@ int LabelItem::type () const
  {
     kDebug()<<"REPAINT LABEL ------------------------";
     QRectF rep = option->exposedRect;
-    painter->setClipRect(rep);
+    //painter->setClipRect(rep);
     QGraphicsRectItem *parent = (QGraphicsRectItem *) parentItem();
     QRectF par = mapFromScene(parent->rect()).boundingRect();
-    //kDebug()<<"REPAINT RECT: "<<par.width();
-    //kDebug()<<"REPAINT RECT: "<<rep.x()<<", "<<rep.y()<<", "<<rep.width()<<", "<<rep.height();
-    //kDebug()<<"PARENT RECT: "<<par.x()<<", "<<par.y()<<", "<<par.width()<<", "<<par.height();
     QRectF parrect = option->matrix.map(mapFromScene(par)).boundingRect();
-    painter->setClipRect( parrect, Qt::IntersectClip ); //option->exposedRect );
-    /*QPainterPath path;
-    path.addRoundRect(parrect, 40);
-    painter->fillPath(path, QColor(200, 50, 200, 100));*/
+    painter->setClipRect( parrect ); //option->exposedRect );
+    QPainterPath path;
+    path.addRoundRect(boundingRect(), 40);
+    painter->fillPath(path, QColor(200, 200, 200, 100));
     //painter->fillRect(parrect, QColor(200, 50, 200, 100));
     painter->drawText(boundingRect(), Qt::AlignCenter, text());
  }
