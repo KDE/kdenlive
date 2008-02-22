25bd88ba6d62be833eef2e4b1bbf4b81fbdbddb8
Marco Gittler <marco@gitma.de>
2008-02-22 12:47:30 +0000

    one missing check, but this does not always protect for crashs on loading a video
    svn path=/branches/KDE4/; revision=1903

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -391,14 +391,14 @@ void Render::getFileProperties(const QDomElement &xml, int clipId)
 	if (context != NULL) {
 		// Get the video_index
 		int index = mlt_properties_get_int( properties, "video_index" );
-		if (context->streams [index] && context->streams[ index ]->codec && context->streams[ index ]->codec->codec->name )
+		if (context->streams && context->streams [index] && context->streams[ index ]->codec && context->streams[ index ]->codec->codec->name )
 			filePropertyMap["videocodec"] = context->streams[ index ]->codec->codec->name;
 	}
 	context = (AVFormatContext *) mlt_properties_get_data( properties, "audio_context", NULL );
 	if (context != NULL) {
 		// Get the video_index
 		int index = mlt_properties_get_int( properties, "audio_index" );
-		if (context->streams [index] && context->streams[ index ]->codec && context->streams[ index ]->codec->codec->name )
+		if (context->streams && context->streams [index] && context->streams[ index ]->codec && context->streams[ index ]->codec->codec->name )
 			filePropertyMap["audiocodec"] = context->streams[ index ]->codec->codec->name;
 	}
 
