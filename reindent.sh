d29894b03675d06dd30ef3b28f75ce8e2d661c19
Marco Gittler <marco@gitma.de>
2008-03-03 12:51:11 +0000

    ability to reindent only a part of files (args of reindent.sh)
    svn path=/branches/KDE4/; revision=1990

--- reindent.sh
+++ reindent.sh
@@ -1,12 +1,19 @@
 #! /bin/sh
 
+FILES="`find -type f -wholename './src/*.cpp'` `find -type f -wholename './src/*.h'`"
+
+if [ $# -gt 0 ]
+then
+	FILES=$@
+fi
+
 astyle --indent=spaces=4 --brackets=attach \
        --indent-labels --unpad=paren \
        --pad=oper --convert-tabs \
        --indent-preprocessor \
-       `find -type f -wholename './src/*.cpp'` `find -type f -wholename './src/*.h'`
+       ${FILES}
 
-echo -e "Delete .orig files (y/N) ?"
+echo -n "Delete .orig files (y/N) ? "
 read del
 
 if [ "x${del}" = "xy" ]
