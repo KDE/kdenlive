ed661f8c9924d223fc2c6e8cfa82f8b05f83f81d
Marco Gittler <marco@gitma.de>
2008-03-03 12:44:03 +0000

    ask for deleting .orig files
    svn path=/branches/KDE4/; revision=1989

--- reindent.sh
+++ reindent.sh
@@ -5,3 +5,14 @@ astyle --indent=spaces=4 --brackets=attach \
        --pad=oper --convert-tabs \
        --indent-preprocessor \
        `find -type f -wholename './src/*.cpp'` `find -type f -wholename './src/*.h'`
+
+echo -e "Delete .orig files (y/N) ?"
+read del
+
+if [ "x${del}" = "xy" ]
+then
+   echo "removing .orig files"
+   find src -iname "*.orig" -exec rm {} ";"
+else
+   echo "Not deleting .orig files"
+fi
