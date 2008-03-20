0dcd41420d1ee6825be2f01306dbe71ad339d225
Marco Gittler <marco@gitma.de>
2008-03-20 15:51:45 +0000

    missing fileso
    svn path=/branches/KDE4/; revision=2087

---
+++ src/edittransitioncommand.cpp
@@ -0,0 +1,54 @@
+/***************************************************************************
+                          edittransitioncommand.cpp  -  description
+                             -------------------
+    begin                : 2008
+    copyright            : (C) 2008 by Marco Gittler
+    email                : g.marco@freenet.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include <KLocale>
+
+#include "edittransitioncommand.h"
+
+EditTransitionCommand::EditTransitionCommand(CustomTrackView *view, const int track, GenTime pos, QDomElement oldeffect, QDomElement effect, bool doIt)
+        : m_view(view), m_track(track), m_pos(pos), m_oldeffect(oldeffect), m_doIt(doIt) {
+    m_effect = effect.cloneNode().toElement();
+    QString effectName;
+    QDomNode namenode = effect.elementsByTagName("name").item(0);
+    if (!namenode.isNull()) effectName = i18n(namenode.toElement().text().toUtf8().data());
+    else effectName = i18n("effect");
+    setText(i18n("Edit transition %1", effectName));
+}
+
+// virtual
+int EditTransitionCommand::id() const {
+    return 1;
+}
+
+// virtual
+bool EditTransitionCommand::mergeWith(const QUndoCommand * other) {
+    if (other->id() != id()) return false;
+    if (m_track != static_cast<const EditTransitionCommand*>(other)->m_track) return false;
+    if (m_pos != static_cast<const EditTransitionCommand*>(other)->m_pos) return false;
+    m_effect = static_cast<const EditTransitionCommand*>(other)->m_effect;
+    return true;
+}
+
+// virtual
+void EditTransitionCommand::undo() {
+    m_view->updateTransition(m_track, m_pos, m_effect, m_oldeffect);
+}
+// virtual
+void EditTransitionCommand::redo() {
+    m_view->updateTransition(m_track, m_pos,m_oldeffect, m_effect);
+}
+
+#include "editeffectcommand.moc"
---
+++ src/edittransitioncommand.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+                          edittransitioncommand.h  -  description
+                             -------------------
+    begin                : 2008
+    copyright            : (C) 2008 by Marco Gittler
+    email                : g.marco@freenet.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef EDITTRANSITIONCOMMAND_H
+#define EDITTRANSITIONCOMMAND_H
+
+#include <QUndoCommand>
+#include <KDebug>
+
+#include "customtrackview.h"
+
+class EditTransitionCommand : public QUndoCommand {
+public:
+    EditTransitionCommand(CustomTrackView *view, const int track, GenTime pos, QDomElement oldeffect, QDomElement effect, bool doIt);
+
+    virtual int id() const;
+    virtual bool mergeWith(const QUndoCommand * command);
+    virtual void undo();
+    virtual void redo();
+
+private:
+    CustomTrackView *m_view;
+    int m_track;
+    QDomElement m_effect;
+    QDomElement m_oldeffect;
+    GenTime m_pos;
+    bool m_doIt;
+};
+
+#endif
+
