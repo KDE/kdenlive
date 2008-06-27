1aa66ffc988de969a45dcab7fa80ed20b114da01
Jean-Baptiste Mardelle <jb@kdenlive.org>
2008-06-27 18:10:23 +0000

    Fix effect stack corruption when opening project
    svn path=/branches/KDE4/; revision=2282

--- src/trackview.cpp
+++ src/trackview.cpp
@@ -461,6 +461,7 @@ int TrackView::slotAddProjectTrack(int ix, QDomElement xml, bool videotrack) {
                             }
                         }
                         item->addEffect(clipeffect, false);
+			item->effectsCounter();
                     }
                 }
 
