88a4da87fafd5950703dcfef162fefcc139636da
Marco Gittler <marco@gitma.de>
2008-07-08 19:07:20 +0000

    removed not present *.h in Messages.sh
    svn path=/branches/KDE4/; revision=2301

--- Messages.sh
+++ Messages.sh
@@ -6,5 +6,5 @@ kdenlive_subdirs="src src/widgets `pwd`"
 
 $EXTRACTRC `find $kdenlive_subdirs -name \*.ui` >> rc.cpp || exit 11
 $EXTRACTRC `find $kdenlive_subdirs -name \*.rc` >> rc.cpp || exit 11
-$XGETTEXT `find $kdenlive_subdirs -name \*.cpp -o -name \*.h` *.cpp *.h -o $podir/kdenlive.pot
+$XGETTEXT `find $kdenlive_subdirs -name \*.cpp -o -name \*.h` *.cpp -o $podir/kdenlive.pot
 rm -f rc.cpp
