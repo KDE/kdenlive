1a2992bda90d670e0ef1ee79f34491e54bd248aa
Jean-Baptiste Mardelle <jb@kdenlive.org>
2008-07-15 15:26:50 +0000

    Fix version number (now 0.7), add myself as author
    svn path=/branches/KDE4/; revision=2314

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Marco Gittler (g.marco@freenet.de)              *
+ *   Copyright (C) 2008 by Jean-Baptiste Mardelle (jb@kdenlive.org)        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA          *
+ ***************************************************************************/
+
+
 #include <KApplication>
 #include <KAboutData>
+#include <KDebug>
 #include <KCmdLineArgs>
 #include <KUrl> //new
 
@@ -7,11 +29,13 @@
 
 int main(int argc, char *argv[]) {
     KAboutData aboutData("kdenlive", "kdenlive",
-                         ki18n("Kdenlive"), "1.0",
-                         ki18n("A simple text area which can load and save."),
+                         ki18n("Kdenlive"), "0.7",
+                         ki18n("An open source video editor."),
                          KAboutData::License_GPL,
-                         ki18n("Copyright (c) 2007 Developer"));
+                         ki18n("Copyright (c) 2008 Development team"));
+    aboutData.addAuthor(ki18n("Jean-Baptiste Mardelle"), ki18n("Mlt porting, KDE4 porting, Main developer"), "jb@kdenlive.org");
     aboutData.addAuthor(ki18n("Marco Gittler"), ki18n("MltConnection, Transition, Effect, Timeline Developer"), "g.marco@freenet.de");
+    aboutData.setHomepage("http://kdenlive.org");
     KCmdLineArgs::init(argc, argv, &aboutData);
 
     KCmdLineOptions options; //new
@@ -20,12 +44,29 @@ int main(int argc, char *argv[]) {
 
     KApplication app;
 
-    MainWindow* window = new MainWindow();
-    window->show();
+    // see if we are starting with session management
+    if (app.isSessionRestored()) {
+        int n = 1;
+        while (KMainWindow::canBeRestored(n)) {
+            const QString className = KXmlGuiWindow::classNameOfToplevel(n);
+            if (className == QLatin1String("MainWindow")) {
+                MainWindow* win = new MainWindow();
+                win->restore(n);
+            } else {
+                kWarning() << "Unknown class " << className << " in session saved data!";
+            }
+            ++n;
+        }
+    } else {
+        MainWindow* window = new MainWindow();
+        window->show();
 
-    KCmdLineArgs *args = KCmdLineArgs::parsedArgs(); //new
-    if (args->count()) { //new
-        window->openFile(args->url(0)); //new
+        KCmdLineArgs *args = KCmdLineArgs::parsedArgs(); //new
+        if (args->count()) { //new
+            window->openFile(args->url(0)); //new
+        }
+
+        args->clear();
     }
 
     return app.exec();
