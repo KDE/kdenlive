28a98505168f7301fcfceb2043cd458844f12d3b
Marco Gittler <marco@gitma.de>
2008-08-03 20:20:37 +0000

    only move the rectwidget for the geometry
    svn path=/branches/KDE4/; revision=2359

--- src/geometryval.cpp
+++ src/geometryval.cpp
@@ -21,17 +21,36 @@
 #include <QVBoxLayout>
 #include <QGraphicsScene>
 #include "graphicsscenerectmove.h"
+#include <QGraphicsRectItem>
 
 Geometryval::Geometryval(QWidget* parent): QWidget(parent) {
     ui.setupUi(this);
     QVBoxLayout* vbox = new QVBoxLayout(this);
     ui.widget->setLayout(vbox);
     QGraphicsView *view = new QGraphicsView(this);
+    //view->setBackgroundBrush(QBrush(QColor(0,0,0)));
     vbox->addWidget(view);
-    //scene= new QGraphicsScene;
+
+    ui.frame->setTickPosition(QSlider::TicksBelow);
 
     scene = new GraphicsSceneRectMove(this);
+    scene->setTool(TITLE_SELECT);
     view->setScene(scene);
+
+    QGraphicsRectItem *m_frameBorder = new QGraphicsRectItem(QRectF(-40, -30, 40, 30));
+    m_frameBorder->setZValue(-1100);
+    m_frameBorder->setBrush(QColor(255, 255, 255, 0));
+
+    scene->addItem(m_frameBorder);
+
+    QGraphicsRectItem *m_frameBorder1 = new QGraphicsRectItem(QRectF(-30, -20, 30, 20));
+
+    m_frameBorder1->setZValue(0);
+    m_frameBorder1->setBrush(QColor(255, 0, 0, 0));
+
+    scene->addItem(m_frameBorder1);
+
+    scene->setSceneRect(-80, -60, 80, 60);
 }
 QDomElement Geometryval::getParamDesc() {}
 
