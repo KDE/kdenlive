bd198278681547d71d18362e4163554248a698e4
Jean-Baptiste Mardelle <jb@kdenlive.org>
2008-10-25 14:24:44 +0000

    Change po folder structure, each language has it's own directory, do not merge translations when building
    svn path=/branches/KDE4/; revision=2560

--- po/message-merge.sh
+++ po/message-merge.sh
@@ -3,7 +3,8 @@
 for f in *; do
   if test -d "$f"; then
     for pot in *.pot; do
-      test -f "$f/${pot%*.*}.po" && msgmerge -U "$f/${pot%*.*}.po" "$pot";
+      test -f "$f/$f.po" && msgmerge -U "$f/$f.po" "$pot";
+      echo "processing $f/$f.po";
     done
   fi
 done
