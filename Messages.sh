40a8b46ceb89d44600d00a64117753d3ca1bc264
Jean-Baptiste Mardelle <jb@kdenlive.org>
2008-10-25 01:11:52 +0000

    Extract some missing strings
    svn path=/branches/KDE4/; revision=2552

--- Messages.sh
+++ Messages.sh
@@ -8,5 +8,5 @@ kdenlive_subdirs="src src/widgets renderer `pwd`"
 $EXTRACTRC --tag=name --tag=description --tag=label effects/*.xml >> rc.cpp || exit 11
 $EXTRACTRC `find $kdenlive_subdirs -name \*.ui` >> rc.cpp || exit 11
 $EXTRACTRC `find $kdenlive_subdirs -name \*.rc` >> rc.cpp || exit 11
-$XGETTEXT `find $kdenlive_subdirs -name \*.cpp -o -name \*.h` *.cpp -o $podir/kdenlive.pot
+$XGETTEXT -C -kki18n -ki18n -ktr2i18n -kI18N_NOOP -ktr `find $kdenlive_subdirs -name \*.cpp -o -name \*.h` *.cpp -o $podir/kdenlive.pot
 rm -f rc.cpp
