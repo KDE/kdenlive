623b531071f66d990d2ff544ea629484f3238a5a
Jean-Baptiste Mardelle <jb@kdenlive.org>
2008-10-26 14:56:07 +0000

    Update script for new po structure
    svn path=/branches/KDE4/; revision=2569

--- po/message-merge.sh
+++ po/message-merge.sh
@@ -3,8 +3,8 @@
 for f in *; do
   if test -d "$f"; then
     for pot in *.pot; do
-      test -f "$f/$f.po" && msgmerge -U "$f/$f.po" "$pot";
-      echo "processing $f/$f.po";
+      test -f "$f/kdenlive.po" && msgmerge -U "$f/kdenlive.po" "$pot";
+      echo "processing $f/kdenlive.po";
     done
   fi
 done
