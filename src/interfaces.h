fa56b652451148e6ba31f7ba8e657cdce967dd6b
Jean-Baptiste Mardelle <jb@kdenlive.org>
2009-01-04 06:29:16 +0000

    Forgot to commit that one in previous change
    svn path=/branches/KDE4/; revision=2867

---
+++ src/interfaces.h
@@ -0,0 +1,41 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Jean-Baptiste Mardelle (jb@kdenlive.org)        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA          *
+ ***************************************************************************/ 
+
+#ifndef INTERFACES_H
+#define INTERFACES_H
+
+#include <QStringList>
+#include <QUrl>
+#include <QtPlugin>
+
+class ClipGenerator
+ {
+ public:
+     virtual ~ClipGenerator() {}
+
+     virtual QStringList generators() const = 0;
+     virtual KUrl generatedClip(const QString &generator, const KUrl &projectFolder, const QStringList &lumaNames, const QStringList &lumaFiles, const double fps, const int width, const int height) = 0;
+ };
+
+ Q_DECLARE_INTERFACE(ClipGenerator,
+                     "com.kdenlive.ClipGenerator.ClipGeneratorInterface/1.0")
+
+#endif
+
+
