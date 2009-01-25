d31733fca8b471f5c2303645b71d7d4931125cca
Simon A. Eugster <simon.eu@gmail.com>
2009-01-25 18:41:21 +0000

    Typo: no space before question mark in English
    svn path=/branches/KDE4/; revision=2958

--- src/effectstackview.cpp
+++ src/effectstackview.cpp
@@ -86,7 +86,7 @@ void EffectStackView::slotSaveEffect() {
     if (name.isEmpty()) return;
     QString path = KStandardDirs::locateLocal("appdata", "effects/", true);
     path = path + name + ".xml";
-    if (QFile::exists(path)) if (KMessageBox::questionYesNo(this, i18n("File already exists.\nDo you want to overwrite it ?")) == KMessageBox::No) return;
+    if (QFile::exists(path)) if (KMessageBox::questionYesNo(this, i18n("File already exists.\nDo you want to overwrite it?")) == KMessageBox::No) return;
 
     int i = ui.effectlist->currentRow();
     QDomDocument doc;
