0c1001c7f5cdc4e576e21712609de370b9831685
Jean-Baptiste Mardelle <jb@kdenlive.org>
2009-03-04 09:04:01 +0000

    missing header
    svn path=/trunk/kdenlive/; revision=3101

--- src/projectlist.cpp
+++ src/projectlist.cpp
@@ -52,6 +52,7 @@
 #include <QIcon>
 #include <QMenu>
 #include <QProcess>
+#include <QHeaderView>
 
 ProjectList::ProjectList(QWidget *parent)
         : QWidget(parent), m_render(NULL), m_fps(-1), m_commandStack(NULL), m_selectedItem(NULL), m_infoQueue(QMap <QString, QDomElement> ()), m_thumbnailQueue(QList <QString> ()), m_refreshed(false), m_editAction(NULL), m_openAction(NULL), m_deleteAction(NULL), m_reloadAction(NULL) {
