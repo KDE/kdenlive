1e77ec0d109351b3221d4d9611f3f5ca72d702e7
Simon A. Eugster <simon.eu@gmail.com>
2009-12-01 13:06:02 +0000

    z-index icons
    svn path=/trunk/kdenlive/; revision=4164

---
+++ icons/cp-oxicons-hisc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cp ox16-action-kdenlive-zindex-up.png hi16-action-kdenlive-zindex-up.png
+cp oxsc-action-kdenlive-zindex-up.svgz hisc-action-kdenlive-zindex-up.svgz
+cp ox16-action-kdenlive-zindex-down.png hi16-action-kdenlive-zindex-down.png
+cp oxsc-action-kdenlive-zindex-down.svgz hisc-action-kdenlive-zindex-down.svgz
+cp ox16-action-kdenlive-zindex-top.png hi16-action-kdenlive-zindex-top.png
+cp oxsc-action-kdenlive-zindex-top.svgz hisc-action-kdenlive-zindex-top.svgz
+cp ox16-action-kdenlive-zindex-bottom.png hi16-action-kdenlive-zindex-bottom.png
+cp oxsc-action-kdenlive-zindex-bottom.svgz hisc-action-kdenlive-zindex-bottom.svgz
