ecd76800819317b812dd986d931bf6ab774e3334
Dan Dennedy <dan@dennedy.org>
2010-05-01 06:10:24 +0000

    Fix initialization of AffineComposite Transition geometry (1539)
    svn path=/trunk/kdenlive/; revision=4407

--- src/initeffects.cpp
+++ src/initeffects.cpp
@@ -723,7 +723,7 @@ void initEffects::fillTransitionsList(Mlt::Repository * repository, EffectsList*
                 paramList.append(quickParameterFill(ret, "Fix Shear Z", "fix_shear_z", "double", "0", "0", "360"));
                 paramList.append(quickParameterFill(ret, "Mirror", "mirror_off", "bool", "0", "0", "1"));
                 paramList.append(quickParameterFill(ret, "Repeat", "repeat_off", "bool", "0", "0", "1"));
-                paramList.append(quickParameterFill(ret, "Geometry", "geometry", "geometry",  "0;0;100;100;100", "0;0;100;100;100", "0;0;100;100;100"));
+                paramList.append(quickParameterFill(ret, "Geometry", "geometry", "geometry",  "0,0,100%,100%,100%", "0,0,100%,100%,100%", "0,0,100%,100%,100%"));
                 tname.appendChild(ret.createTextNode("Composite"));
             } else if (name == "region") {
                 tname.appendChild(ret.createTextNode("Region"));
