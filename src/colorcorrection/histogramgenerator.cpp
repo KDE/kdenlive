b19a4181e99dca6e66f91e2ea8b297254b5c9b60
Simon A. Eugster <simon.eu@gmail.com>
2010-08-31 12:38:56 +0000

    Cleanup
    svn path=/trunk/kdenlive/; revision=4793

--- src/colorcorrection/histogramgenerator.cpp
+++ src/colorcorrection/histogramgenerator.cpp
@@ -21,7 +21,6 @@ HistogramGenerator::HistogramGenerator()
 QImage HistogramGenerator::calculateHistogram(const QSize &paradeSize, const QImage &image, const int &components,
                                               HistogramGenerator::Rec rec, const bool &unscaled, const uint &accelFactor) const
 {
-//    qDebug() << "Histogram rect size is: " << paradeSize.width() << "/" << paradeSize.height();
     if (paradeSize.height() <= 0 || paradeSize.width() <= 0 || image.width() <= 0 || image.height() <= 0) {
         return QImage();
     }
@@ -96,34 +95,29 @@ QImage HistogramGenerator::calculateHistogram(const QSize &paradeSize, const QIm
     histogram.fill(qRgba(0, 0, 0, 0));
 
     if (drawY) {
-//        qDebug() << "Drawing Y at " << wy << " with height " << partH;
         drawComponentFull(&davinci, y, scaling, QRect(0, wy, ww, partH + dist), QColor(220, 220, 210, 255), dist, unscaled, 256);
 
         wy += partH + d;
     }
 
     if (drawSum) {
-//        qDebug() << "Drawing S at " << wy << " with height " << partH;
         drawComponentFull(&davinci, s, scaling/3, QRect(0, wy, ww, partH + dist), QColor(220, 220, 210, 255), dist, unscaled, 256);
 
         wy += partH + d;
     }
 
     if (drawR) {
-//        qDebug() << "Drawing R at " << wy << " with height " << partH;
         drawComponentFull(&davinci, r, scaling, QRect(0, wy, ww, partH + dist), QColor(255, 128, 0, 255), dist, unscaled, 256);
 
         wy += partH + d;
     }
 
     if (drawG) {
-//        qDebug() << "Drawing G at " << wy << " with height " << partH;
         drawComponentFull(&davinci, g, scaling, QRect(0, wy, ww, partH + dist), QColor(128, 255, 0, 255), dist, unscaled, 256);
         wy += partH + d;
     }
 
     if (drawB) {
-//        qDebug() << "Drawing B at " << wy << " with height " << partH;
         drawComponentFull(&davinci, b, scaling, QRect(0, wy, ww, partH + dist), QColor(0, 128, 255, 255), dist, unscaled, 256);
 
         wy += partH + d;
