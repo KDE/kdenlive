6a0d49620915e4991944f5b5dda57c889a5694e1
Jean-Baptiste Mardelle <jb@kdenlive.org>
2010-12-26 12:01:04 +0000

    Check for xine in config wizard: http://www.kdenlive.org/mantis/view.php?id=1133
    svn path=/trunk/kdenlive/; revision=5202

--- src/wizard.cpp
+++ src/wizard.cpp
@@ -414,7 +414,13 @@ void Wizard::slotCheckPrograms()
             item->setIcon(0, m_okIcon);
         } else item->setIcon(0, m_badIcon);
     } else item->setIcon(0, m_okIcon);
-
+    
+    item = new QTreeWidgetItem(m_check.programList, QStringList() << QString() << i18n("xine"));
+    item->setData(1, Qt::UserRole, i18n("Required to preview your DVD"));
+    item->setSizeHint(0, itemSize);
+    if (KStandardDirs::findExe("xine").isEmpty()) item->setIcon(0, m_badIcon);
+    else item->setIcon(0, m_okIcon); 
+    
     // set up some default applications
     QString program;
     if (KdenliveSettings::defaultimageapp().isEmpty()) {
