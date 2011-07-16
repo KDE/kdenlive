75051894798b16801af81dd7831e49a7f643073b
Dan Dennedy <dan@dennedy.org>
2011-07-16 18:09:57 +0000

    Limit MLT processing threads to 3.
    svn path=/trunk/kdenlive/; revision=5796

--- src/kdenlivesettingsdialog.cpp
+++ src/kdenlivesettingsdialog.cpp
@@ -83,7 +83,7 @@ KdenliveSettingsDialog::KdenliveSettingsDialog(const QMap<QString, QString>& map
     m_configEnv.mltpathurl->setMode(KFile::Directory);
     m_configEnv.mltpathurl->lineEdit()->setObjectName("kcfg_mltpath");
     m_configEnv.rendererpathurl->lineEdit()->setObjectName("kcfg_rendererpath");
-    m_configEnv.kcfg_mltthreads->setMaximum(QThread::idealThreadCount());
+    m_configEnv.kcfg_mltthreads->setMaximum( QThread::idealThreadCount() < 4 ? QThread::idealThreadCount() : 3 );
     m_configEnv.tmppathurl->setMode(KFile::Directory);
     m_configEnv.tmppathurl->lineEdit()->setObjectName("kcfg_currenttmpfolder");
     m_configEnv.projecturl->setMode(KFile::Directory);
