c6f581eb95db46086221714f84f7fd56a3e8153b
Till Theato <root@ttill.de>
2011-08-28 23:31:12 +0000

    Revert commit 5845. The consumer producer does not automatically get invoked.
    svn path=/trunk/kdenlive/; revision=5846

--- renderer/renderjob.cpp
+++ renderer/renderjob.cpp
@@ -72,7 +72,10 @@ RenderJob::RenderJob(bool erase, bool usekuiserver, const QString& renderer, con
 
     m_args << preargs;
     //qDebug()<<"PRE ARGS: "<<preargs;
-    m_args << "-profile" << profile;
+    if (scenelist.startsWith("consumer:")) {
+        // Use MLT's producer_consumer, needs a different syntax for profile:
+        m_args << "profile=" + profile;
+    } else m_args << "-profile" << profile;
     m_args << "-consumer" << rendermodule + ':' + m_dest << "progress=1" << args;
 
     m_dualpass = false;
