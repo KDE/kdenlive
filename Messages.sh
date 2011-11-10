d42c81ffc8e1a437fd68890795d2331395dd7bce
Albert Astals Cid <tsdgeos@terra.es>
2011-11-10 13:21:38 +0100

    Do not try to extract from encodingprofiles.rc
    It is not a "KDE rc file" and extractrc complains

--- Messages.sh
+++ Messages.sh
@@ -3,7 +3,7 @@
 kdenlive_subdirs="plugins renderer src src/widgets"
 
 $EXTRACTRC --tag=name --tag=description --tag=label --tag=comment --tag=paramlistdisplay effects/*.xml data/kdenliveeffectscategory.rc >> rc.cpp
-$EXTRACTRC `find $kdenlive_subdirs -name \*.rc -o -name \*.ui` >> rc.cpp
+$EXTRACTRC `find $kdenlive_subdirs -name \*.rc -a ! -name encodingprofiles.rc -o -name \*.ui` >> rc.cpp
 
 $XGETTEXT `find $kdenlive_subdirs -name \*.cpp -o -name \*.h` *.cpp -o $podir/kdenlive.pot
 rm -f rc.cpp
