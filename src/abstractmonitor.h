bc8bc0fa42e3c4535b7dfef75a1fb99b35c295aa
Jean-Baptiste Mardelle <jb@kdenlive.org>
2012-07-23 10:57:31 +0200

    abstractmonitor.h: initialize analyseAudio to false [Coverity 6/14] by Mikko Rapeli

--- src/abstractmonitor.h
+++ src/abstractmonitor.h
@@ -43,7 +43,7 @@ Q_OBJECT public:
      *  @param name A unique identifier for this renderer
      *  @param winid The parent widget identifier (required for SDL display). Set to 0 for OpenGL rendering
      *  @param profile The MLT profile used for the renderer (default one will be used if empty). */
-    explicit AbstractRender(Kdenlive::MONITORID name, QWidget *parent = 0):QObject(parent), sendFrameForAnalysis(false), m_name(name) {};
+    explicit AbstractRender(Kdenlive::MONITORID name, QWidget *parent = 0):QObject(parent), sendFrameForAnalysis(false), analyseAudio(false), m_name(name) {};
 
     /** @brief Destroy the MLT Renderer. */
     virtual ~AbstractRender() {};
