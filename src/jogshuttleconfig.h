c89dd4dcf5a15dacb0aa9f21db0fef2bf8ac7429
Jean-Baptiste Mardelle <jb@kdenlive.org>
2012-07-23 08:15:36 +0200

    Fix copyrights [krazy 3/37] by Mikko Rapeli

--- src/jogshuttleconfig.h
+++ src/jogshuttleconfig.h
@@ -1,6 +1,5 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) <year>  <name of author>
+    Copyright (C) 2012 Jean-Baptiste Mardelle (jb@kdenlive.org)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
