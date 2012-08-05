27042c59e09ebdb24188b7d59d4ac9a44e914df4
Jean-Baptiste Mardelle <jb@kdenlive.org>
2012-08-05 15:09:47 +0200

    Add basic support for loading video clips in project

---
+++ src/plugins/cliptypes/videocliptype/videoprojectclip.cpp
@@ -0,0 +1,89 @@
+/*
+Copyright (C) 2012  Till Theato <root@ttill.de>
+This file is part of kdenlive. See www.kdenlive.org.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+*/
+
+#include "videoprojectclip.h"
+#include "videoclipplugin.h"
+#include "videotimelineclip.h"
+#include "core/project/projectfolder.h"
+#include "core/project/producerwrapper.h"
+#include "core/project/project.h"
+#include "core/project/binmodel.h"
+#include "core/timecodeformatter.h"
+#include "core/timecode.h"
+#include "src/core/kdenlivesettings.h"
+#include <mlt++/Mlt.h>
+#include <KUrl>
+#include <KFileMetaInfo>
+#include <QDomElement>
+
+
+VideoProjectClip::VideoProjectClip(const KUrl& url, ProjectFolder* parent, VideoClipPlugin const *plugin) :
+    AbstractProjectClip(url, parent, plugin)
+{
+    m_baseProducer = new ProducerWrapper(*bin()->project()->profile(), url.path());
+
+    init();
+}
+
+VideoProjectClip::VideoProjectClip(ProducerWrapper* producer, ProjectFolder* parent, VideoClipPlugin const *plugin) :
+    AbstractProjectClip(producer, parent, plugin)
+{
+    init();
+}
+
+VideoProjectClip::VideoProjectClip(const QDomElement& description, ProjectFolder* parent, VideoClipPlugin const *plugin) :
+    AbstractProjectClip(description, parent, plugin)
+{
+    Q_ASSERT(description.attribute("producer_type") == "avformat");
+
+    m_baseProducer = new ProducerWrapper(*(bin()->project()->profile()), m_url.path());
+
+    Q_ASSERT(m_baseProducer->property("mlt_service") == description.attribute("producer_type"));
+
+    kDebug() << "video project clip created" << id();
+
+    init();
+}
+
+VideoProjectClip::~VideoProjectClip()
+{
+}
+
+AbstractTimelineClip* VideoProjectClip::addInstance(ProducerWrapper* producer, TimelineTrack* parent)
+{
+    VideoTimelineClip *instance = new VideoTimelineClip(producer, this, parent);
+    m_instances.append(instance);
+    return static_cast<AbstractTimelineClip *>(instance);
+}
+
+
+QPixmap VideoProjectClip::thumbnail()
+{
+    if (m_thumbnail.isNull()) {
+        m_thumbnail = m_baseProducer->pixmap().scaledToHeight(100, Qt::SmoothTransformation);
+    }
+    return m_thumbnail;
+}
+
+void VideoProjectClip::init()
+{
+    Q_ASSERT(m_baseProducer && m_baseProducer->is_valid());
+    Q_ASSERT(m_baseProducer->property("mlt_service") == "avformat");
+
+    m_hasLimitedDuration = true;
+    m_thumbnail = m_baseProducer->pixmap().scaledToHeight(100, Qt::SmoothTransformation);
+
+    kDebug() << "new project clip created " << m_baseProducer->get("resource") << m_baseProducer->get_length();
+}
+
+
+
+
+#include "videoprojectclip.moc"
---
+++ src/plugins/cliptypes/videocliptype/videoprojectclip.h
@@ -0,0 +1,40 @@
+/*
+Copyright (C) 2012  Till Theato <root@ttill.de>
+This file is part of kdenlive. See www.kdenlive.org.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+*/
+
+#ifndef VIDEOPROJECTCLIP_H
+#define VIDEOPROJECTCLIP_H
+
+#include "core/project/abstractprojectclip.h"
+#include <QPixmap>
+
+class VideoClipPlugin;
+class VideoTimelineClip;
+
+
+class VideoProjectClip : public AbstractProjectClip
+{
+    Q_OBJECT
+
+public:
+    VideoProjectClip(const KUrl& url, ProjectFolder* parent, VideoClipPlugin const *plugin);
+    VideoProjectClip(ProducerWrapper* producer, ProjectFolder* parent, VideoClipPlugin const *plugin);
+    VideoProjectClip(const QDomElement &description, ProjectFolder *parent, VideoClipPlugin const *plugin);
+    ~VideoProjectClip();
+
+    AbstractTimelineClip *addInstance(ProducerWrapper *producer, TimelineTrack *parent);
+
+    QPixmap thumbnail();
+
+private:
+    void init();
+    QList<VideoTimelineClip *> m_instances;
+};
+
+#endif
