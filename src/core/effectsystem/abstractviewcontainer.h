7ea11da975d6c3bbd0598f79794f3e8871b96a07
Jean-Baptiste Mardelle <jb@kdenlive.org>
2013-06-04 21:42:01 +0200

    Add missing files

---
+++ src/core/effectsystem/abstractviewcontainer.h
@@ -0,0 +1,32 @@
+/*
+Copyright (C) 2013  Jean-Baptiste Mardelle <jb@kdenlive.org>
+This file is part of kdenlive. See www.kdenlive.org.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+*/
+
+#ifndef ABSTRACTVIEWCONTAINER_H
+#define ABSTRACTVIEWCONTAINER_H
+
+#include <QWidget>
+#include <kdemacros.h>
+
+class QToolButton;
+class QFrame;
+
+
+class KDE_EXPORT AbstractViewContainer : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit AbstractViewContainer(QWidget* parent = 0);
+    virtual ~AbstractViewContainer();
+    virtual void addChild(QWidget *child) = 0;
+
+};
+
+#endif
