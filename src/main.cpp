8f16f0db43e5397bda08038d71c4f6cd9da76c9a
Vincent PINON <vincent.pinon@laposte.net>
2014-02-19 00:39:59 +0100

    clean encoding profiles using MLT presets & profiles add myself to contributors

--- src/main.cpp
+++ src/main.cpp
@@ -34,8 +34,9 @@ int main(int argc, char *argv[])
                          ki18n("Kdenlive"), VERSION,
                          ki18n("An open source video editor."),
                          KAboutData::License_GPL,
-                         ki18n("Copyright © 2007–2013 Kdenlive authors"));
+                         ki18n("Copyright © 2007–2014 Kdenlive authors"));
     aboutData.addAuthor(ki18n("Jean-Baptiste Mardelle"), ki18n("MLT and KDE SC 4 porting, main developer and maintainer"), "jb@kdenlive.org");
+    aboutData.addAuthor(ki18n("Vincent Pinon"), ki18n("Interim maintainer, bugs fixing, minor functions, profiles updates, etc."), "vpinon@april.org");
     aboutData.addAuthor(ki18n("Laurent Montel"), ki18n("Bugs fixing, clean up code, optimization etc."), "montel@kde.org");
     aboutData.addAuthor(ki18n("Marco Gittler"), ki18n("MLT transitions and effects, timeline, audio thumbs"), "g.marco@freenet.de");
     aboutData.addAuthor(ki18n("Dan Dennedy"), ki18n("Bug fixing, etc."), "dan@dennedy.org");
