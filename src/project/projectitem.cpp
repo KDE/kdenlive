777d0baa971434201ec7f3bac2e2ccff60219bf2
Vincent PINON <vincent.pinon@laposte.net>
2014-08-13 12:41:36 +0000

    fix importing a 50fps project into a 25fps (#3320) REVIEW: 119749 When importing a one project into another, compute the durations of the new clips based upon the clip's actual FPS value - not the imported-into projects value.

--- src/project/projectitem.cpp
+++ src/project/projectitem.cpp
@@ -225,12 +225,12 @@ void ProjectItem::setProperties(const QMap < QString, QString > &attributes, con
     else if (attributes.contains("frame_size")) slotSetToolTip();
     
     if (attributes.contains("duration")) {
-        GenTime duration = GenTime(attributes.value("duration").toInt(), KdenliveSettings::project_fps());
+        GenTime duration = GenTime(attributes.value("duration").toInt(), attributes.value("fps").toInt());
         QString itemdata = data(0, DurationRole).toString();
         if (itemdata.contains('/')) itemdata = itemdata.section('/', 0, 0) + "/ ";
         else itemdata.clear();
         if (prefix.isEmpty()) prefix = itemdata;
-        setData(0, DurationRole, prefix + Timecode::getEasyTimecode(duration, KdenliveSettings::project_fps()));
+	setData(0, DurationRole, prefix + Timecode::getEasyTimecode(duration, attributes.value("fps").toInt()));
         m_clip->setDuration(duration);
     } else  {
         // No duration known, use an arbitrary one until it is.
