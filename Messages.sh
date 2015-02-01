0f0c582bf07bad44fcff6f9b0ee5db2c381c8ba0
Jean-Baptiste Mardelle <jb@kdenlive.org>
2015-02-01 17:04:33 +0100

    Fix i18n for effects, patch by Dmitry Mikhirev

--- Messages.sh
+++ Messages.sh
@@ -2,7 +2,7 @@
 
 kdenlive_subdirs="plugins renderer src src/ui"
 
-$EXTRACTRC --tag=name --tag=description --tag=label --tag=comment --tag=paramlistdisplay effects/*.xml data/kdenliveeffectscategory.rc >> rc.cpp
+$EXTRACTRC --tag=name --tag=description --tag=label --tag=comment --tag=paramlistdisplay data/effects/*.xml data/kdenliveeffectscategory.rc >> rc.cpp
 $EXTRACTRC `find $kdenlive_subdirs -name \*.rc -a ! -name encodingprofiles.rc -o -name \*.ui` >> rc.cpp
 
 $XGETTEXT `find $kdenlive_subdirs -name \*.cpp -o -name \*.h` *.cpp -o $podir/kdenlive.pot
