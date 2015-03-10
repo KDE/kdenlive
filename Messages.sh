ee65d2cac4cfe66ff65076508896ab8e9d0f8f85
Albert Astals Cid <aacid@kde.org>
2015-03-11 00:24:00 +0100

    camcorderfilters.rc is not an extracable rc either

--- Messages.sh
+++ Messages.sh
@@ -3,7 +3,7 @@
 kdenlive_subdirs="plugins renderer data src src/ui"
 
 $EXTRACTRC --tag=name --tag=description --tag=label --tag=comment --tag=paramlistdisplay data/effects/*.xml data/kdenliveeffectscategory.rc >> rc.cpp
-$EXTRACTRC `find $kdenlive_subdirs -name \*.rc -a ! -name encodingprofiles.rc -o -name \*.ui` >> rc.cpp
+$EXTRACTRC `find $kdenlive_subdirs -name \*.rc -a ! -name encodingprofiles.rc -a ! -name camcorderfilters.rc -o -name \*.ui` >> rc.cpp
 
 $XGETTEXT `find $kdenlive_subdirs -name \*.cpp -o -name \*.h` *.cpp -o $podir/kdenlive.pot
 rm -f rc.cpp
