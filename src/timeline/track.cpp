6fa07f39d90fff044ef3c5ea03ce233d0dc80425
Vincent Pinon <vpinon@kde.org>
2015-10-25 21:46:17 +0100

    fix coverity 1323678 (dead code)

--- src/timeline/track.cpp
+++ src/timeline/track.cpp
@@ -453,8 +453,6 @@ Mlt::Producer *Track::clipProducer(Mlt::Producer *parent, PlaylistState::ClipSta
     QString idForTrack = originalId + QLatin1Char('_') + m_playlist.get("id");
     if (state == PlaylistState::AudioOnly) {
         idForTrack.append("_audio");
-    } else if (state == PlaylistState::VideoOnly) {
-        idForTrack.append("_video");
     }
     if (!forceCreation) {
         for (int i = 0; i < m_playlist.count(); i++) {
@@ -469,8 +467,6 @@ Mlt::Producer *Track::clipProducer(Mlt::Producer *parent, PlaylistState::ClipSta
     prod->set("id", idForTrack.toUtf8().constData());
     if (state == PlaylistState::AudioOnly) {
         prod->set("video_index", -1);
-    } else if (state == PlaylistState::VideoOnly) {
-        prod->set("audio_index", -1);
     }
     return prod;
 }
