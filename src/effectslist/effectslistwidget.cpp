298c6563e51de78610fb7d33d0eff49c229a1379
Jean-Baptiste Mardelle <jb@kdenlive.org>
2015-12-14 00:05:49 +0100

    Fix effects not draggable when switching category and search is not empty BUG: 356626

--- src/effectslist/effectslistwidget.cpp
+++ src/effectslist/effectslistwidget.cpp
@@ -486,19 +486,19 @@ void EffectsListWidget::createFavorites(QList <QTreeWidgetItem *>list)
     // Favorites is a pseudo-folder used to store items, not visible to end user, so don't i18n its name
     QTreeWidgetItem *misc = findFolder(QStringLiteral("Favorites"));
     if (misc == NULL) {
-        misc = new QTreeWidgetItem((QTreeWidget*)0, QStringList(QStringLiteral("Favorites")));
+        misc = new QTreeWidgetItem(this, QStringList(QStringLiteral("Favorites")));
         misc->setData(0, TypeRole, QString::number((int) EFFECT_FOLDER));
         misc->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
-        insertTopLevelItem(0, misc);
     }
     else qDeleteAll(misc->takeChildren());
-    setRootIndex(indexFromItem(misc));
+    setIndentation(0);
     misc->addChildren(list);
     for (int j = 0; j < misc->childCount(); ++j) {
         QTreeWidgetItem *child = misc->child(j);
         child->setHidden(false);
         child->setData(0, Qt::UserRole, EFFECT_FAVORITES);
     }
+    setRootIndex(indexFromItem(misc));
 }
 
 void EffectsListWidget::resetFavorites()
