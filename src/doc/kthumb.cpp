48a9e3b3f2c8fa6b75c89bda20ece00c93f871e8
Jean-Baptiste Mardelle <jb@kdenlive.org>
2016-07-16 00:37:01 +0200

    Remove old unused code with warnings and wrong pasted license CCBUG: 364734

--- src/doc/kthumb.cpp
+++ src/doc/kthumb.cpp
@@ -1,120 +1,37 @@
-/***************************************************************************
-                        krender.cpp  -  description
-                           -------------------
-  begin                : Fri Nov 22 2002
-  copyright            : (C) 2002 by Jason Wood
-  email                : jasonwood@blueyonder.co.uk
-  copyright            : (C) 2005 Lcio Fl�io Corr�
-  email                : lucio.correa@gmail.com
-  copyright            : (C) Marco Gittler
-  email                : g.marco@freenet.de
+/*
+Copyright (C) 2016  Jean-Baptiste Mardelle <jb@kdenlive.org>
+This file is part of Kdenlive. See www.kdenlive.org.
 
-***************************************************************************/
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License or (at your option) version 3 or any later version
+accepted by the membership of KDE e.V. (or its successor approved
+by the membership of KDE e.V.), which shall act as a proxy 
+defined in Section 14 of version 3 of the license.
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "kthumb.h"
-
-#include "project/clipmanager.h"
-#include "renderer.h"
 #include "kdenlivesettings.h"
 
 #include <mlt++/Mlt.h>
 
-#include <QDebug>
-#include <kfileitem.h>
-#include <kmessagebox.h>
-
-#include <qxml.h>
 #include <QImage>
-#include <QVarLengthArray>
 #include <QPainter>
-#include <QtConcurrent>
-
-static QMutex m_intraMutex;
-
-KThumb::KThumb(ClipManager *clipManager, const QUrl &url, const QString &id, const QString &hash, QObject * parent) :
-    QObject(parent),
-    m_url(url),
-    m_dar(1),
-    m_ratio(1),
-    m_producer(NULL),
-    m_clipManager(clipManager),
-    m_id(id)
-{
-}
-
-KThumb::~KThumb()
-{
-    if (m_producer) m_clipManager->stopThumbs(m_id);
-    m_producer = NULL;
-    m_intraFramesQueue.clear();
-    m_intra.waitForFinished();
-}
-
-void KThumb::setProducer(Mlt::Producer *producer)
-{
-    if (m_producer) m_clipManager->stopThumbs(m_id);
-    m_intraFramesQueue.clear();
-    m_intra.waitForFinished();
-    QMutexLocker lock(&m_mutex);
-    m_producer = producer;
-    // FIXME: the profile() call leaks an object, but trying to free
-    // it leads to a double-free in Profile::~Profile()
-    if (producer) {
-        Mlt::Profile *profile = producer->profile();
-        m_dar = profile->dar();
-        m_ratio = (double) profile->width() / profile->height();
-    }
-}
-
-void KThumb::clearProducer()
-{
-    if (m_producer) setProducer(NULL);
-}
-
-bool KThumb::hasProducer() const
-{
-    return m_producer != NULL;
-}
 
 //static
 QPixmap KThumb::getImage(const QUrl &url, int width, int height)
 {
     if (!url.isValid()) return QPixmap();
     return getImage(url, 0, width, height);
-}
-
-void KThumb::extractImage(const QList<int> &frames)
-{
-    if (!KdenliveSettings::videothumbnails() || m_producer == NULL) return;
-    m_clipManager->slotRequestThumbs(m_id, frames);
-}
-
-
-void KThumb::getThumb(int frame)
-{
-    const int theight = Kdenlive::DefaultThumbHeight;
-    const int dwidth = (int)(theight * m_dar + 0.5);
-    QImage img = getProducerFrame(frame, dwidth, theight);
-    emit thumbReady(frame, img);
-}
-
-QImage KThumb::extractImage(int frame, int width, int height)
-{
-    if (m_producer == NULL) {
-        QImage img(width, height, QImage::Format_ARGB32_Premultiplied);
-        img.fill(QColor(Qt::black).rgb());
-        return img;
-    }
-    return getProducerFrame(frame, width, height);
 }
 
 //static
@@ -130,29 +47,6 @@ QPixmap KThumb::getImage(const QUrl &url, int frame, int width, int height)
     pix = QPixmap::fromImage(getFrame(producer, frame, width, height));
     delete producer;
     return pix;
-}
-
-QImage KThumb::getProducerFrame(int framepos, int displayWidth, int height)
-{
-    if (m_producer == NULL || !m_producer->is_valid()) {
-        QImage p(displayWidth, height, QImage::Format_ARGB32_Premultiplied);
-        p.fill(QColor(Qt::red).rgb());
-        return p;
-    }
-    if (m_producer->is_blank()) {
-        QImage p(displayWidth, height, QImage::Format_ARGB32_Premultiplied);
-        p.fill(QColor(Qt::black).rgb());
-        return p;
-    }
-    QMutexLocker lock(&m_mutex);
-    m_producer->seek(framepos);
-    Mlt::Frame *frame = m_producer->get_frame();
-    /*frame->set("rescale.interp", "nearest");
-    frame->set("deinterlace_method", "onefield");
-    frame->set("top_field_first", -1 );*/
-    QImage p = getFrame(frame, displayWidth, height);
-    delete frame;
-    return p;
 }
 
 //static
@@ -237,128 +131,4 @@ uint KThumb::imageVariance(const QImage &image )
         return delta/STEPS;
     else
         return 0;
-}
-
-/*
-void KThumb::getImage(QUrl url, int frame, int width, int height)
-{
-    if (!url.isValid()) return;
-    QPixmap image(width, height);
-    Mlt::Producer m_producer(url.path().toUtf8().constData());
-    image.fill(Qt::black);
-
-    if (m_producer.is_blank()) {
- emit thumbReady(frame, image);
- return;
-    }
-    Mlt::Filter m_convert("avcolour_space");
-    m_convert.set("forced", mlt_image_rgb24a);
-    m_producer.attach(m_convert);
-    m_producer.seek(frame);
-    Mlt::Frame * m_frame = m_producer.get_frame();
-    mlt_image_format format = mlt_image_rgb24a;
-    width = width - 2;
-    height = height - 2;
-    if (m_frame && m_frame->is_valid()) {
-     uint8_t *thumb = m_frame->get_image(format, width, height);
-     QImage tmpimage(thumb, width, height, 32, NULL, 0, QImage::IgnoreEndian);
-     if (!tmpimage.isNull()) bitBlt(&image, 1, 1, &tmpimage, 0, 0, width + 2, height + 2);
-    }
-    if (m_frame) delete m_frame;
-    emit thumbReady(frame, image);
-}
-
-void KThumb::getThumbs(QUrl url, int startframe, int endframe, int width, int height)
-{
-    if (!url.isValid()) return;
-    QPixmap image(width, height);
-    Mlt::Producer m_producer(url.path().toUtf8().constData());
-    image.fill(QColor(Qt::black).rgb());
-
-    if (m_producer.is_blank()) {
- emit thumbReady(startframe, image);
- emit thumbReady(endframe, image);
- return;
-    }
-    Mlt::Filter m_convert("avcolour_space");
-    m_convert.set("forced", mlt_image_rgb24a);
-    m_producer.attach(m_convert);
-    m_producer.seek(startframe);
-    Mlt::Frame * m_frame = m_producer.get_frame();
-    mlt_image_format format = mlt_image_rgb24a;
-    width = width - 2;
-    height = height - 2;
-
-    if (m_frame && m_frame->is_valid()) {
-     uint8_t *thumb = m_frame->get_image(format, width, height);
-     QImage tmpimage(thumb, width, height, 32, NULL, 0, QImage::IgnoreEndian);
-     if (!tmpimage.isNull()) bitBlt(&image, 1, 1, &tmpimage, 0, 0, width - 2, height - 2);
-    }
-    if (m_frame) delete m_frame;
-    emit thumbReady(startframe, image);
-
-    image.fill(Qt::black);
-    m_producer.seek(endframe);
-    m_frame = m_producer.get_frame();
-
-    if (m_frame && m_frame->is_valid()) {
-     uint8_t *thumb = m_frame->get_image(format, width, height);
-     QImage tmpimage(thumb, width, height, 32, NULL, 0, QImage::IgnoreEndian);
-     if (!tmpimage.isNull()) bitBlt(&image, 1, 1, &tmpimage, 0, 0, width - 2, height - 2);
-    }
-    if (m_frame) delete m_frame;
-    emit thumbReady(endframe, image);
-}
-*/
-
-void KThumb::queryIntraThumbs(const QSet <int> &missingFrames)
-{
-    m_intraMutex.lock();
-    if (m_intraFramesQueue.length() > 20) {
-        // trim query list
-        int maxsize = m_intraFramesQueue.length() - 10;
-        if (missingFrames.contains(m_intraFramesQueue.first())) {
-            for( int i = 0; i < maxsize; i ++ )
-                m_intraFramesQueue.removeLast();
-        }
-        else if (missingFrames.contains(m_intraFramesQueue.last())) {
-            for( int i = 0; i < maxsize; i ++ )
-                m_intraFramesQueue.removeFirst();
-        }
-        else m_intraFramesQueue.clear();
-    }
-    QSet<int> set = m_intraFramesQueue.toSet();
-    set.unite(missingFrames);
-    m_intraFramesQueue = set.toList();
-    qSort(m_intraFramesQueue);
-    m_intraMutex.unlock();
-    if (!m_intra.isRunning()) {
-        m_intra = QtConcurrent::run(this, &KThumb::slotGetIntraThumbs);
-    }
-}
-
-void KThumb::slotGetIntraThumbs()
-{
-    const int theight = KdenliveSettings::trackheight();
-    const int displayWidth = (int)(theight * m_dar + 0.5);
-    QString path = m_url.path() + '_';
-    bool addedThumbs = false;
-    while (true) {
-        m_intraMutex.lock();
-        if (m_intraFramesQueue.isEmpty()) {
-            m_intraMutex.unlock();
-            break;
-        }
-        int pos = m_intraFramesQueue.takeFirst();
-        m_intraMutex.unlock();
-        const QString key = path + QString::number(pos);
-        if (!m_clipManager->pixmapCache->contains(key)) {
-            QImage img = getProducerFrame(pos, displayWidth, theight);
-            if (m_clipManager->pixmapCache->insertImage(key, img)) {
-                addedThumbs = true;
-            }
-            else qDebug()<<"// INSERT FAILD FOR: "<<pos;
-        }
-    }
-    if (addedThumbs) emit thumbsCached();
 }
--- src/doc/kthumb.h
+++ src/doc/kthumb.h
@@ -1,39 +1,31 @@
-/***************************************************************************
-                         kthumb.h  -  description
-                            -------------------
-   begin                : Fri Nov 22 2002
-   copyright            : (C) 2002 by Jason Wood
-   email                : jasonwood@blueyonder.co.uk
-***************************************************************************/
+/*
+Copyright (C) 2016  Jean-Baptiste Mardelle <jb@kdenlive.org>
+This file is part of Kdenlive. See www.kdenlive.org.
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License or (at your option) version 3 or any later version
+accepted by the membership of KDE e.V. (or its successor approved
+by the membership of KDE e.V.), which shall act as a proxy 
+defined in Section 14 of version 3 of the license.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef KTHUMB_H
 #define KTHUMB_H
 
-#include <QPixmap>
-#include <QDomElement>
-#include <QFuture>
-
+#include <QImage>
 #include <QUrl>
 
 #include <mlt++/Mlt.h>
-
-
-/**KRender encapsulates the client side of the interface to a renderer.
-From Kdenlive's point of view, you treat the KRender object as the
-renderer, and simply use it as if it was local. Calls are asyncrhonous -
-you send a call out, and then receive the return value through the
-relevant signal that get's emitted once the call completes.
-  *@author Jason Wood
-  */
 
 
 namespace Mlt
@@ -42,60 +34,16 @@ class Producer;
 class Frame;
 }
 
-class ClipManager;
-
-typedef QMap <int, QMap <int, QByteArray> > audioByteArray;
-
-class KThumb: public QObject
+namespace KThumb
 {
-   Q_OBJECT 
-public:
-
-    explicit KThumb(ClipManager *clipManager, const QUrl &url, const QString &id, const QString &hash, QObject * parent = 0);
-    ~KThumb();
-    void setProducer(Mlt::Producer *producer);
-    bool hasProducer() const;
-    void clearProducer();
-    void extractImage(const QList<int> &frames);
-    QImage extractImage(int frame, int width, int height);
-    /** @brief Request thumbnails for the frame range. */
-    void queryIntraThumbs(const QSet <int> &missingFrames);
-    void getThumb(int frame);
-
-public:
-    static QPixmap getImage(const QUrl &url, int width, int height = -1);
-    static QPixmap getImage(const QUrl &url, int frame, int width, int height = -1);
-    static QImage getFrame(Mlt::Producer *producer, int framepos, int displayWidth, int height);
-    static QImage getFrame(Mlt::Frame *frame, int width, int height);
+    QPixmap getImage(const QUrl &url, int width, int height = -1);
+    QPixmap getImage(const QUrl &url, int frame, int width, int height = -1);
+    QImage getFrame(Mlt::Producer *producer, int framepos, int displayWidth, int height);
+    QImage getFrame(Mlt::Frame *frame, int width, int height);
     /** @brief Calculates image variance, useful to know if a thumbnail is interesting. 
      *  @return an integer between 0 and 100. 0 means no variance, eg. black image while bigger values mean contrasted image
      * */
-    static uint imageVariance(const QImage &image);
-
-private slots:
-    /** @brief Fetch all requested frames. */ 
-    void slotGetIntraThumbs();
-
-private:
-    QUrl m_url;
-    double m_dar;
-    double m_ratio;
-    Mlt::Producer *m_producer;
-    ClipManager *m_clipManager;
-    QString m_id;
-    /** @brief Controls the intra frames thumbnails process (cached thumbnails). */
-    QFuture<void> m_intra;
-    /** @brief List of frame numbers from which we want to extract thumbnails. */
-    QList <int> m_intraFramesQueue;
-    QMutex m_mutex;
-    QImage getProducerFrame(int framepos, int displayWidth, int height);
-
-signals:
-    void thumbReady(int, const QImage&);
-    void mainThumbReady(const QString &, const QPixmap&);
-    void audioThumbReady(const audioByteArray&);
-    /** @brief We have finished caching all requested thumbs. */
-    void thumbsCached();
+    uint imageVariance(const QImage &image);
 };
 
 #endif
