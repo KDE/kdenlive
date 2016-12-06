388717238b03ca663187ec0e05209dcd0dfe5dfb
Vincent Pinon <vpinon@kde.org>
2016-12-06 08:29:46 +0100

    fix clip loading on windows

--- src/dialogs/clipcreationdialog.cpp
+++ src/dialogs/clipcreationdialog.cpp
@@ -423,7 +423,7 @@ void ClipCreationDialog::createClipsCommand(KdenliveDoc *doc, const QList<QUrl> 
         QDomElement prod = xml.createElement(QStringLiteral("producer"));
         xml.appendChild(prod);
         QMap <QString, QString> properties;
-        properties.insert(QStringLiteral("resource"), file.path());
+        properties.insert(QStringLiteral("resource"), file.toLocalFile());
         if (!groupInfo.isEmpty()) {
             properties.insert(QStringLiteral("kdenlive:folderid"), groupInfo.at(0));
         }
@@ -433,7 +433,6 @@ void ClipCreationDialog::createClipsCommand(KdenliveDoc *doc, const QList<QUrl> 
             i.next();
             properties.insert(i.key(), i.value());
         }
-        //prod.setAttribute("resource", file.path());
         uint id = bin->getFreeClipId();
         prod.setAttribute(QStringLiteral("id"), QString::number(id));
         QMimeDatabase db;
