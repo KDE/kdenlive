69220ab2e0045927297a0f70affe828a1edf2339
Jean-Baptiste Mardelle <jb@kdenlive.org>
2016-12-20 22:00:36 +0100

    Fix dbus communication broken by recent move of KDBusService BUG: 367875

--- src/main.cpp
+++ src/main.cpp
@@ -52,6 +52,7 @@ int main(int argc, char *argv[])
     // Init application
     QApplication app(argc, argv);
     app.setApplicationName(QStringLiteral("kdenlive"));
+    app.setOrganizationDomain("kde.org");
     app.setWindowIcon(QIcon(":/pics/kdenlive.png"));
     KLocalizedString::setApplicationDomain("kdenlive");
     KSharedConfigPtr config = KSharedConfig::openConfig();
