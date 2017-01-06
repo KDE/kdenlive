KDE/kdenlive

commit a97e13d9f1ab16d6d9fde11136591cea47c2f7dd

Force curves widget to be square

src/effectstack/widgets/curves/cubic/kis_curve_widget.cpp
@@ -51,27 +51,17 @@ KisCurveWidget::KisCurveWidget(QWidget *parent)
     : AbstractCurveWidget(parent)
 {
     setObjectName(QStringLiteral("KisCurveWidget"));
-    m_currentPointIndex = -1;
     m_guideVisible   = false;
-    m_pixmapIsDirty = true;
-    m_pixmapCache = Q_NULLPTR;
 
     m_intIn = Q_NULLPTR;
     m_intOut = Q_NULLPTR;
 
     m_maxPoints = -1;
 
-    setMouseTracking(true);
-    setAutoFillBackground(false);
-    setAttribute(Qt::WA_OpaquePaintEvent);
-    setMinimumSize(150, 150);
-    setMaximumSize(1000, 1000);
-    setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
     update();
     emit modified();
 
-    setFocusPolicy(Qt::StrongFocus);
     m_grabOffsetX = 0;
     m_grabOffsetY = 0;
     m_grabOriginalX = 0;

src/effectstack/widgets/curves/abstractcurvewidget.h
@@ -122,6 +122,7 @@ public:
     void slotZoomOut();
 protected:
     void paintBackground(QPainter * p);
+    int heightForWidth(int w) const Q_DECL_OVERRIDE;
     void resizeEvent(QResizeEvent *event) Q_DECL_OVERRIDE;
     void leaveEvent(QEvent *) Q_DECL_OVERRIDE;
     void mouseReleaseEvent(QMouseEvent *e) Q_DECL_OVERRIDE;

src/effectstack/widgets/curves/abstractcurvewidget.ipp
@@ -29,8 +29,18 @@ AbstractCurveWidget<Curve_t>::AbstractCurveWidget(QWidget *parent):
     , m_pixmapIsDirty(true)
     , m_currentPointIndex(-1)
     , m_maxPoints(1000000)
+    , m_state(State_t::NORMAL)
 
 {
+    setMouseTracking(true);
+    setAutoFillBackground(false);
+    setAttribute(Qt::WA_OpaquePaintEvent);
+    setMinimumSize(150, 150);
+    setMaximumSize(500, 500);
+    QSizePolicy sp(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    sp.setHeightForWidth(true); //force widget to have a height dependent on width;
+    setSizePolicy(sp);
+    setFocusPolicy(Qt::StrongFocus);
 }
 
 template<typename Curve_t>
@@ -237,4 +247,10 @@ void AbstractCurveWidget<Curve_t>::keyPressEvent(QKeyEvent *e)
     } else {
         QWidget::keyPressEvent(e);
     }
+}
+
+template<typename Curve_t>
+int AbstractCurveWidget<Curve_t>::heightForWidth(int w) const
+{
+    return w;
 }

src/effectstack/widgets/curves/bezier/beziersplineeditor.cpp
@@ -32,13 +32,7 @@ BezierSplineEditor::BezierSplineEditor(QWidget *parent) :
     , m_grabOffsetX(0)
     , m_grabOffsetY(0)
 {
-    m_state = State_t::NORMAL;
     m_curve = CubicBezierSpline();
-    setMouseTracking(true);
-    setAutoFillBackground(false);
-    setAttribute(Qt::WA_OpaquePaintEvent);
-    setMinimumSize(150, 150);
-    setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 }
 
 BezierSplineEditor::~BezierSplineEditor()

commit d4fd601f76a1ed043da78244bc3d6f52e542c13c

Fix typo introduced in a97e13d9f1ab16d6d9fde11136591cea47c2f7dd

src/effectstack/widgets/curves/cubic/kis_curve_widget.cpp
@@ -217,7 +217,7 @@ void KisCurveWidget::mousePressEvent(QMouseEvent *e)
     int closest_point_index = nearestPointInRange(QPointF(x, y), width(), height());
 
     if (e->button() == Qt::RightButton && closest_point_index > 0 && closest_point_index < m_curve.points().count() - 1) {
-        m_currentPointIndex = closestPointIndex;
+        m_currentPointIndex = closest_point_index;
         slotDeleteCurrentPoint();
     } else if (e->button() != Qt::LeftButton) {
         return;