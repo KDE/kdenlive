90c3294884c29940e2e3c3af7b3795cbeb15c2cc
Nicolas Carion <french.ebook.lover@gmail.com>
2017-02-03 12:34:53 +0100

    [Timeline2][Model] Missing file

---
+++ src/timeline2/model/undohelper.cpp
@@ -0,0 +1,49 @@
+/***************************************************************************
+ *   Copyright (C) 2017 by Nicolas Carion                                  *
+ *   This file is part of Kdenlive. See www.kdenlive.org.                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) version 3 or any later version accepted by the       *
+ *   membership of KDE e.V. (or its successor approved  by the membership  *
+ *   of KDE e.V.), which shall act as a proxy defined in Section 14 of     *
+ *   version 3 of the license.                                             *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#include "undohelper.hpp"
+#include <QDebug>
+
+
+FunctionalUndoCommand::FunctionalUndoCommand(const Fun& undo, const Fun& redo, QString text, QUndoCommand *parent) :
+    QUndoCommand(parent),
+    m_undo(undo),
+    m_redo(redo),
+    m_undone(false)
+{
+    setText(text);
+}
+
+
+void FunctionalUndoCommand::undo()
+{
+    m_undone = true;
+    bool res = m_undo();
+    Q_ASSERT(res);
+}
+
+void FunctionalUndoCommand::redo()
+{
+    if (m_undone) {
+        bool res = m_redo();
+        Q_ASSERT(res);
+    }
+}
