2ed43d34a124609aef092164e2e58e5476228209
Jean-Baptiste Mardelle <jb@kdenlive.org>
2017-06-19 13:21:16 +0200

    Fix compilation

--- src/scopes/audioscopes/spectrogram.cpp
+++ src/scopes/audioscopes/spectrogram.cpp
@@ -241,8 +241,8 @@ QImage Spectrogram::renderHUD(uint)
                 x = leftDist + (m_innerScopeRect.width() - 1) * ((float)hz) / m_freqMax;
 
                 // Hide text if it would overlap with the text drawn at the mouse position
-                hideText = m_aTrackMouse->isChecked() && m_mouseWithinWidget && abs(x - (leftDist + mouseX + 20)) < (int) minDistX + 16
-                           && mouseX < m_innerScopeRect.width() && mouseX >= 0;
+                hideText = m_aTrackMouse->isChecked() && m_mouseWithinWidget && abs(x - (int)(leftDist + mouseX + 20)) < (int) minDistX + 16 
+                && mouseX < m_innerScopeRect.width() && mouseX >= 0;
 
                 if (x <= rightBorder) {
                     davinci.drawLine(x, topDist, x, topDist + m_innerScopeRect.height() + 6);
@@ -268,7 +268,7 @@ QImage Spectrogram::renderHUD(uint)
             }
             // Draw the line at the very right (maximum frequency)
             x = leftDist + m_innerScopeRect.width() - 1;
-            hideText = m_aTrackMouse->isChecked() && m_mouseWithinWidget && abs(x - (leftDist + mouseX + 30)) < (int) minDistX
+            hideText = m_aTrackMouse->isChecked() && m_mouseWithinWidget && qAbs(x - (int)(leftDist + mouseX + 30)) < (int) minDistX
                        && mouseX < m_innerScopeRect.width() && mouseX >= 0;
             davinci.drawLine(x, topDist, x, topDist + m_innerScopeRect.height() + 6);
             if (!hideText) {
