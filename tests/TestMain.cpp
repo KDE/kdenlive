f3ec78c9aea7912b87d275b8c192b7b87e7b2463
Nicolas Carion <french.ebook.lover@gmail.com>
2017-07-30 14:23:02 +0200

    Add application to tests to fix various issues

--- tests/TestMain.cpp
+++ tests/TestMain.cpp
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 
 #include "core.h"
+#include <QApplication>
 #include <mlt++/MltFactory.h>
 #include <mlt++/MltRepository.h>
 /* This file is intended to remain empty.
@@ -9,6 +10,7 @@ Write your tests in a file with a name corresponding to what you're testing */
 
 int main(int argc, char *argv[])
 {
+    QApplication app(argc, argv);
     std::unique_ptr<Mlt::Repository> repo(Mlt::Factory::init(nullptr));
     Core::build();
 
