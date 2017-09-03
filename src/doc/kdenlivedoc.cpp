67c4958b65a296cbdf0763d1540ee08d33ff0e91
Jean-Baptiste Mardelle <jb@kdenlive.org>
2017-09-03 08:12:23 +0200

    Allow disabling autosave until we improve it BUG: 384197

--- src/doc/kdenlivedoc.cpp
+++ src/doc/kdenlivedoc.cpp
@@ -902,7 +902,7 @@ void KdenliveDoc::slotModified()
 void KdenliveDoc::setModified(bool mod)
 {
     // fix mantis#3160: The document may have an empty URL if not saved yet, but should have a m_autosave in any case
-    if (m_autosave && mod) {
+    if (m_autosave && mod && KdenliveSettings::crashrecovery()) {
         emit startAutoSave();
     }
     if (mod == m_modified) {
