d4bc618915fea2b8a71a4d1583c844bb9050f2d9
Vincent Pinon <vpinon@kde.org>
2017-09-13 19:55:25 +0200

    Fix build with KIO < 5.33

---
+++ src/effects/effectstack/view/builtstack.cpp
@@ -0,0 +1,64 @@
+/***************************************************************************
+ *   Copyright (C) 2017 by Jean-Baptiste Mardelle (jb@kdenlive.org)        *
+ *   This file is part of Kdenlive. See www.kdenlive.org.                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) version 3 or any later version accepted by the       *
+ *   membership of KDE e.V. (or its successor approved  by the membership  *
+ *   of KDE e.V.), which shall act as a proxy defined in Section 14 of     *
+ *   version 3 of the license.                                             *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#include "builtstack.hpp"
+#include "assets/assetpanel.hpp"
+#include "core.h"
+#include "effects/effectstack/model/effectstackmodel.hpp"
+
+#include <KDeclarative/KDeclarative>
+#include <QQmlContext>
+#include <QQuickItem>
+
+BuiltStack::BuiltStack(AssetPanel *parent)
+    : QQuickWidget(parent)
+    , m_model(nullptr)
+{
+    KDeclarative::KDeclarative kdeclarative;
+    kdeclarative.setDeclarativeEngine(engine());
+    kdeclarative.setupBindings();
+    setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
+    setMinimumHeight(100);
+    //setClearColor(palette().base().color());
+    setSource(QUrl(QStringLiteral("qrc:/qml/BuiltStack.qml")));
+    setFocusPolicy(Qt::StrongFocus);
+    QQuickItem *root = rootObject();
+    QObject::connect(root, SIGNAL(valueChanged(QString, int)),
+                  parent, SLOT(parameterChanged(QString, int)));
+    setResizeMode(QQuickWidget::SizeRootObjectToView);
+}
+
+BuiltStack::~BuiltStack()
+{
+}
+
+void BuiltStack::setModel(std::shared_ptr<EffectStackModel> model, ObjectId ownerId)
+{
+    m_model = model;
+    if (ownerId.first == ObjectType::TimelineClip) {
+        QVariant current_speed((int) (100.0 * pCore->getClipSpeed(ownerId.second)));
+        qDebug()<<" CLIP SPEED OFR: "<<ownerId.second<<" = "<<current_speed;
+        QMetaObject::invokeMethod(rootObject(), "setSpeed", Qt::QueuedConnection, Q_ARG(QVariant, current_speed));
+    }
+    QMetaObject::invokeMethod(rootObject(), "resetStack", Qt::QueuedConnection);
+}
+
+
