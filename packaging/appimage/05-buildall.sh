f658dfb3b2ca66be7c0e57abdf8534d21a01590f
Jean-Baptiste Mardelle <jb@kdenlive.org>
2017-11-15 07:39:17 +0100

    Fix AppImage instructions

--- packaging/appimage/05-buildall.sh
+++ packaging/appimage/05-buildall.sh
@@ -8,21 +8,21 @@ set -e
 set -x
 
 echo "++ building dependencies and frameworks..."
-. /kdenlive/packaging/01-dependencies.sh
+. /kdenlive/packaging/appimage/01-dependencies.sh
 
 echo "++ building dependencies and frameworks DONE"
 
 echo "++ building AV libraries (ffmpeg, OpenCV) ..."
-. /kdenlive/packaging/02-avlibs.sh
+. /kdenlive/packaging/appimage/02-avlibs.sh
 
 echo "++ building AV libraries (ffmpeg, OpenCV) DONE"
 
 echo "++ building MLT and Kdenlive ..."
-. /kdenlive/packaging/03-kdenlive.sh
+. /kdenlive/packaging/appimage/03-kdenlive.sh
 
 echo "++ building MLT and Kdenlive DONE"
 
 echo "++ Preparing AppImage folder ..."
-. /kdenlive/packaging/04-create-app.sh
+. /kdenlive/packaging/appimage/04-create-app.sh
 
 echo "++ Preparing AppImage folder DONE"
