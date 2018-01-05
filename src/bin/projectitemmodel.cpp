1ee9cac62a49c77734074854ce2ffb51a6652c17
Jean-Baptiste Mardelle <jb@kdenlive.org>
2018-01-05 10:08:19 +0100

    Fix compilation (type error)

--- src/bin/projectitemmodel.cpp
+++ src/bin/projectitemmodel.cpp
@@ -704,7 +704,7 @@ bool ProjectItemModel::loadFolders(Mlt::Properties &folders)
         if (upLinks.count(f.first) == 0) {
             upLinks[f.first] = -1;
         }
-        if (f.first != QStringLiteral("-1") && downLinks.count(upLinks[f.first]) == 0) {
+        if (f.first != -1 && downLinks.count(upLinks[f.first]) == 0) {
             qDebug() << "Warning: parent folder " << upLinks[f.first] << "for folder" << f.first << "is invalid. Folder will be placed in topmost directory.";
             upLinks[f.first] = -1;
         }
