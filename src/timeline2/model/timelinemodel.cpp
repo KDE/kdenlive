d19bb34ae8f65e6f162ea5b486ae2264b1459ca8
Jean-Baptiste Mardelle <jb@kdenlive.org>
2018-03-01 13:40:38 +0100

    Revert "Fix insertion of several clips"
    This reverts commit 5ecd42394298cad227613e2db4711fa837f148c0.

--- src/timeline2/model/timelinemodel.cpp
+++ src/timeline2/model/timelinemodel.cpp
@@ -539,27 +539,9 @@ bool TimelineModel::requestClipInsertion(const QString &binClipId, int trackId, 
     QWriteLocker locker(&m_lock);
     Fun undo = []() { return true; };
     Fun redo = []() { return true; };
-    QStringList clips = binClipId.split(QLatin1Char(';'), QString::SkipEmptyParts);
-    bool result = false;
-    std::unordered_set<int> createdClips;
-    id = -1;
-    int tmpId;
-    for (const QString bin : clips) {
-        result = requestClipInsertion(bin, trackId, position, tmpId, logUndo, refreshView, undo, redo);
-        if (!result) {
-            break;
-        }
-        position += getClipPlaytime(tmpId);
-        createdClips.insert(tmpId);
-        if (id == -1) {
-            id = tmpId;
-        }
-    }
-    if (result && createdClips.size() > 1) {
-        requestClipsGroup(createdClips, false, GroupType::Selection);
-    }
+    bool result = requestClipInsertion(binClipId, trackId, position, id, logUndo, refreshView, undo, redo);
     if (result && logUndo) {
-        PUSH_UNDO(undo, redo, i18np("Insert Clip", "Insert Clips", clips.count()));
+        PUSH_UNDO(undo, redo, i18n("Insert Clip"));
     }
     return result;
 }
