a80ff117dd60c58095d457fab113a9aa00136ccd
Jean-Baptiste Mardelle <jb@kdenlive.org>
2019-06-26 12:58:12 +0200

    Fix keyframes corruption on dragging effect onto another clip

--- src/timeline2/model/clipmodel.cpp
+++ src/timeline2/model/clipmodel.cpp
@@ -319,7 +319,8 @@ bool ClipModel::addEffect(const QString &effectId)
 bool ClipModel::copyEffect(const std::shared_ptr<EffectStackModel> &stackModel, int rowId)
 {
     QWriteLocker locker(&m_lock);
-    m_effectStack->copyEffect(stackModel->getEffectStackRow(rowId), m_currentState);
+    QDomDocument doc;
+    m_effectStack->copyXmlEffect(stackModel->rowToXml(rowId, doc));
     return true;
 }
 
@@ -792,3 +793,4 @@ int ClipModel::getMaxDuration() const
     }
     return m_producer->get_length();
 }
+
