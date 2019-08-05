f761bb0f4ef0a70bc430254fca27dbcf6502d5d8
Yuri Chornoivan <yurchor@ukr.net>
2019-08-05 09:03:44 +0300

    Clean up output of extract-messages.sh a bit

--- Messages.sh
+++ Messages.sh
@@ -3,7 +3,7 @@
 kdenlive_subdirs="plugins renderer data src src/ui"
 
 $EXTRACTRC --tag=name --tag=description --tag=label --tag=comment --tag=paramlistdisplay data/transitions/*.xml data/effects/*.xml data/generators/*.xml data/kdenliveeffectscategory.rc >> rc.cpp
-$EXTRACTRC `find $kdenlive_subdirs -name \*.rc -a ! -name encodingprofiles.rc -a ! -name camcorderfilters.rc -o -name \*.ui` >> rc.cpp
+$EXTRACTRC `find $kdenlive_subdirs -name \*.rc -a ! -name encodingprofiles.rc -a ! -name camcorderfilters.rc -a ! -name externalproxies.rc -o -name \*.ui` >> rc.cpp
 
 $XGETTEXT `find $kdenlive_subdirs -name \*.cpp -o -name \*.h -o -name \*.qml` *.cpp -o $podir/kdenlive.pot
 rm -f rc.cpp
