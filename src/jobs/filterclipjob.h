41676cd8643d76c2c38d1c37d7b3289d442e7e27
Yuri Chornoivan <yurchor@ukr.net>
2019-12-20 21:25:21 +0200

    Fix build

--- src/jobs/filterclipjob.h
+++ src/jobs/filterclipjob.h
@@ -18,8 +18,8 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA          *
  ***************************************************************************/
 
-#ifndef FILTERCLIPJOB
-#define FILTERCLIPJOB
+#ifndef JOBS_FILTERCLIPJOB
+#define JOBS_FILTERCLIPJOB
 
 #include "meltjob.h"
 #include "assets/model/assetparametermodel.hpp"
