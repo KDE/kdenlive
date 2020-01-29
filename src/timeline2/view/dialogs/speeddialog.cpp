108366cb883c06f19d2c0c56748e6d538c85f1bc
Jean-Baptiste Mardelle <jb@kdenlive.org>
2020-01-29 13:17:33 +0100

    Re-use better dialog for timeline change clip speed (with slider)

--- src/timeline2/view/dialogs/speeddialog.cpp
+++ src/timeline2/view/dialogs/speeddialog.cpp
@@ -1,23 +1,35 @@
 #include "speeddialog.h"
-#include "ui_speeddialog_ui.h"
+#include "ui_clipspeed_ui.h"
 #include <QPushButton>
 
 SpeedDialog::SpeedDialog(QWidget *parent, double speed, double minSpeed, double maxSpeed, bool reversed)
     : QDialog(parent)
-    , ui(new Ui::SpeedDialog)
+    , ui(new Ui::ClipSpeed_UI)
 {
     ui->setupUi(this);
     setWindowTitle(i18n("Clip Speed"));
-    ui->doubleSpinBox->setDecimals(2);
-    ui->doubleSpinBox->setMinimum(minSpeed);
-    ui->doubleSpinBox->setMaximum(maxSpeed);
-    ui->doubleSpinBox->setValue(speed);
-    ui->doubleSpinBox->selectAll();
+    ui->speedSpin->setDecimals(2);
+    ui->speedSpin->setMinimum(minSpeed);
+    ui->speedSpin->setMaximum(maxSpeed);
+    ui->speedSlider->setMinimum(100 * minSpeed);
+    ui->speedSlider->setMaximum(qMin(100 * minSpeed + 100000, 100 *maxSpeed));
+    ui->speedSpin->setValue(speed);
+    ui->speedSlider->setValue(100 * speed);
+    ui->speedSlider->setTickInterval((ui->speedSlider->maximum() - ui->speedSlider->minimum()) / 10);
+    ui->speedSpin->selectAll();
+    ui->label_dest->setVisible(false);
+    ui->kurlrequester->setVisible(false);
+    ui->toolButton->setVisible(false);
     if (reversed) {
         ui->checkBox->setChecked(true);
     }
-    connect(ui->doubleSpinBox, static_cast<void (QDoubleSpinBox::*)(double)>(&QDoubleSpinBox::valueChanged), [&] (double value) {
+    connect(ui->speedSpin, static_cast<void (QDoubleSpinBox::*)(double)>(&QDoubleSpinBox::valueChanged), [&] (double value) {
+        ui->speedSlider->setValue(100 * value);
         ui->buttonBox->button((QDialogButtonBox::Ok))->setEnabled(!qFuzzyIsNull(value));
+    });
+    connect(ui->speedSlider, &QSlider::valueChanged, [&] (int value) {
+        ui->speedSpin->setValue(value / 100.);
+        ui->buttonBox->button((QDialogButtonBox::Ok))->setEnabled(value != 0);
     });
 }
 
@@ -28,7 +40,7 @@ SpeedDialog::~SpeedDialog()
 
 double SpeedDialog::getValue() const
 {
-    double val = ui->doubleSpinBox->value();
+    double val = ui->speedSpin->value();
     if (ui->checkBox->checkState() == Qt::Checked) {
         val *= -1;
     }
