436ad5e0ba8f7b975c8da7fd6a8783eb4ae5dda1
Jean-Baptiste Mardelle <jb@kdenlive.org>
2020-07-24 15:22:30 +0200

    Update AppImage dependency build scripts

--- packaging/appimage/build-dependencies.sh
+++ packaging/appimage/build-dependencies.sh
@@ -65,75 +65,116 @@ cd $BUILD_PREFIX/deps-build/
 # Configure the dependencies for building
 cmake $KDENLIVE_SOURCES/packaging/appimage/3rdparty -DCMAKE_INSTALL_PREFIX=$DEPS_INSTALL_PREFIX -DEXT_INSTALL_DIR=$DEPS_INSTALL_PREFIX -DEXT_DOWNLOAD_DIR=$DOWNLOADS_DIR -DEXT_BUILD_DIR=$BUILD_PREFIX
 
+CPU_CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+
+if [[ $CPU_CORES -gt 1 ]]; then
+    CPU_CORES=$((CPU_CORES-1))
+fi
+
+echo "CPU Cores to use : $CPU_CORES"
+
 # Now start building everything we need, in the appropriate order
 
-cmake --build . --target ext_lzma
-cmake --build . --target ext_xml
-cmake --build . --target ext_gettext
-cmake --build . --target ext_xslt
-cmake --build . --target ext_png
+cmake --build . --target ext_lzma  -j$CPU_CORES
+cmake --build . --target ext_xml  -j$CPU_CORES
+cmake --build . --target ext_gettext  -j$CPU_CORES
+cmake --build . --target ext_xslt  -j$CPU_CORES
+cmake --build . --target ext_png  -j$CPU_CORES
 
-  # cmake --build . --target ext_jpeg #this causes build failures in Qt 5.10
 
 export CC=/usr/bin/gcc-6
 export CXX=/usr/bin/g++-6
 
 cmake --build . --target ext_qt
-cmake --build . --target ext_boost
-cmake --build . --target ext_gpgme
-cmake --build . --target ext_frameworks
-cmake --build . --target ext_libsndfile
-cmake --build . --target ext_libsamplerate
-cmake --build . --target ext_nasm
-cmake --build . --target ext_yasm
-cmake --build . --target ext_alsa
-cmake --build . --target ext_sdl2
+cmake --build . --target ext_boost  -j$CPU_CORES
+cmake --build . --target ext_gpgme  -j$CPU_CORES
+cmake --build . --target ext_libsndfile  -j$CPU_CORES
+cmake --build . --target ext_libsamplerate  -j$CPU_CORES
+cmake --build . --target ext_nasm  -j$CPU_CORES
+cmake --build . --target ext_yasm  -j$CPU_CORES
+cmake --build . --target ext_alsa  -j$CPU_CORES
+cmake --build . --target ext_sdl2  -j$CPU_CORES
 
-cmake --build . --target ext_fftw3
-cmake --build . --target ext_fftw3f
+cmake --build . --target ext_fftw3  -j$CPU_CORES
+cmake --build . --target ext_fftw3f  -j$CPU_CORES
 
 # ladspa expects fft3w.pc pkgconfig files
 cp $DEPS_INSTALL_PREFIX/lib/pkgconfig/fftwf.pc $DEPS_INSTALL_PREFIX/lib/pkgconfig/fftw3f.pc
 cp $DEPS_INSTALL_PREFIX/lib/pkgconfig/fftw.pc $DEPS_INSTALL_PREFIX/lib/pkgconfig/fftw3.pc
 
-cmake --build . --target ext_x264
-cmake --build . --target ext_x265
+cmake --build . --target ext_x264  -j$CPU_CORES
+cmake --build . --target ext_x265  -j$CPU_CORES
 
 # libvpx does not compile with this gcc6 version
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
 
-cmake --build . --target ext_libvpx
+#cmake --build . --target ext_libvpx  -j$CPU_CORES
 
 export CC=/usr/bin/gcc-6
 export CXX=/usr/bin/g++-6
 
-cmake --build . --target ext_ffmpeg
-cmake --build . --target ext_sox
-cmake --build . --target ext_jack
-cmake --build . --target ext_ladspa
-cmake --build . --target ext_cairo
-cmake --build . --target ext_harfbuzz
-cmake --build . --target ext_pango
-cmake --build . --target ext_gdkpixbuf
-cmake --build . --target ext_gtk+
-cmake --build . --target ext_gavl
-cmake --build . --target ext_frei0r
-cmake --build . --target ext_vidstab
-cmake --build . --target ext_opencv
-cmake --build . --target ext_rubberband
 
-#export CC=/usr/bin/gcc-6
-#export CXX=/usr/bin/g++-6
+cmake --build . --target ext_opus  -j$CPU_CORES
+cmake --build . --target ext_ffmpeg  -j$CPU_CORES
+cmake --build . --target ext_sox  -j$CPU_CORES
+cmake --build . --target ext_jack  -j$CPU_CORES
+cmake --build . --target ext_ladspa  -j$CPU_CORES
+cmake --build . --target ext_cairo  -j$CPU_CORES
+cmake --build . --target ext_harfbuzz  -j$CPU_CORES
+cmake --build . --target ext_pango  -j$CPU_CORES
+cmake --build . --target ext_gdkpixbuf  -j$CPU_CORES
+cmake --build . --target ext_gtk+  -j$CPU_CORES
+cmake --build . --target ext_gavl  -j$CPU_CORES
+cmake --build . --target ext_frei0r  -j$CPU_CORES
+cmake --build . --target ext_vidstab  -j$CPU_CORES
+cmake --build . --target ext_opencv  -j$CPU_CORES
+
+export CC=/usr/bin/gcc-6
+export CXX=/usr/bin/g++-6
+
 
 #cmake --build . --target ext_movit
-cmake --build . --target ext_kbookmarks
-cmake --build . --target ext_kxmlgui
-cmake --build . --target ext_kconfigwidgets
-cmake --build . --target ext_knotifyconfig
-cmake --build . --target ext_knewstuff
-cmake --build . --target ext_kdeclarative
-cmake --build . --target ext_breezeicons
-cmake --build . --target ext_kcrash
+cmake --build . --target ext_frameworks
+
+cmake --build . --config RelWithDebInfo --target ext_extra-cmake-modules -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kconfig             -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_breeze-icons        -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kcoreaddons         -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kwindowsystem       -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_solid               -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_threadweaver        -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_karchive            -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kdbusaddons         -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_ki18n               -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kcrash              -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kcodecs             -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kauth               -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kguiaddons          -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kwidgetsaddons      -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kitemviews          -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kcompletion         -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kconfigwidgets      -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kservice            -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kiconthemes         -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_attica        -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kglobalaccel        -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kxmlgui             -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kbookmarks          -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kjobwidgets                 -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_sonnet                 -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_ktextwidgets                 -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kio                 -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_knotifyconfig       -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kpackage                -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_knewstuff           -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kdeclarative        -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kservice            -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_kimageformats       -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_knotifications -- -j$CPU_CORES
+#cmake --build . --config RelWithDebInfo --target ext_frameworkintegration -- -j$CPU_CORES
+
 cmake --build . --target ext_breeze
-cmake --build . --target ext_ruby
+#cmake --build . --target ext_kwayland
+#cmake --build . --target ext_plasma-framework
+#cmake --build . --target ext_ruby
