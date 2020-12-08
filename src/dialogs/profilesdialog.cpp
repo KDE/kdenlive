4b5a6031b653e86bf8792c242d344340f8171209
Jean-Baptiste Mardelle <jb@kdenlive.org>
2020-12-08 15:34:31 +0100

    Fix project profile creation dialog not updating properties on profile selection

--- src/dialogs/profilesdialog.cpp
+++ src/dialogs/profilesdialog.cpp
@@ -72,7 +72,7 @@ ProfilesDialog::ProfilesDialog(const QString &profileDescription, QWidget *paren
 
 void ProfilesDialog::connectDialog()
 {
-    connect(m_view.profiles_list, static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged), this, [&](int ix) { slotUpdateDisplay(m_view.profiles_list->itemText(ix)); });
+    connect(m_view.profiles_list, static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged), this, [&](int ix) { slotUpdateDisplay(m_view.profiles_list->itemData(ix).toString()); });
     connect(m_view.button_create, &QAbstractButton::clicked, this, &ProfilesDialog::slotCreateProfile);
     connect(m_view.button_save, &QAbstractButton::clicked, this, &ProfilesDialog::slotSaveProfile);
     connect(m_view.button_delete, &QAbstractButton::clicked, this, &ProfilesDialog::slotDeleteProfile);
