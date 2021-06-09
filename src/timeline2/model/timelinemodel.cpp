f305f37d1110a7711d4009732cccd3a04aa7a4eb
Jean-Baptiste Mardelle <jb@kdenlive.org>
2021-06-09 09:08:58 +0200

    Fix right clip of a mix cannot be moved backwards

--- src/timeline2/model/timelinemodel.cpp
+++ src/timeline2/model/timelinemodel.cpp
@@ -2355,7 +2355,7 @@ bool TimelineModel::requestGroupMove(int itemId, int groupId, int delta_track, i
                 if (getTrackById_const(current_track_id)->hasStartMix(item.first)) {
                     subPlaylist = m_allClips[item.first]->getSubPlaylistIndex();
                 }
-                if (!getTrackById_const(current_track_id)->isAvailable(target_position, playtime, subPlaylist)) {
+                if (!getTrackById_const(current_track_id)->isAvailable(target_position, current_in - target_position, subPlaylist)) {
                     if (!getTrackById_const(current_track_id)->isBlankAt(current_in - 1)) {
                         // No move possible, abort
                         bool undone = local_undo();
