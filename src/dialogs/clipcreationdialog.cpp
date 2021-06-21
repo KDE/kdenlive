3195a83785dd38703badf4cc50066d9bd4d069a1
Alexander Robb <alex765@protonmail.com>
2021-06-21 08:40:53 +0000

    Add webp mime type to clip creation dialog

--- src/dialogs/clipcreationdialog.cpp
+++ src/dialogs/clipcreationdialog.cpp
@@ -81,7 +81,7 @@ QStringList ClipCreationDialog::getExtensions()
     mimeTypes << QStringLiteral("image/gif") << QStringLiteral("image/jpeg") << QStringLiteral("image/png") << QStringLiteral("image/x-tga")
               << QStringLiteral("image/x-bmp") << QStringLiteral("image/svg+xml") << QStringLiteral("image/tiff") << QStringLiteral("image/x-xcf")
               << QStringLiteral("image/x-xcf-gimp") << QStringLiteral("image/x-vnd.adobe.photoshop") << QStringLiteral("image/x-pcx")
-              << QStringLiteral("image/x-exr") << QStringLiteral("image/x-portable-pixmap") << QStringLiteral("application/x-krita");
+              << QStringLiteral("image/x-exr") << QStringLiteral("image/x-portable-pixmap") << QStringLiteral("application/x-krita") << QStringLiteral("image/webp");
 
     QMimeDatabase db;
     QStringList allExtensions;
