KDE/kdenlive

commit 81ddab103f09a5ef827191d841d571ddcbe26e62

Fix some clang & clazy warnings

src/onlineresources/resourcewidget.cpp
@@ -78,8 +78,8 @@ ResourceWidget::ResourceWidget(QWidget *parent)
     }
     connect(service_list, SIGNAL(currentIndexChanged(int)), this, SLOT(slotChangeProvider()));
     loadConfig();
-    connect(provider_info, SIGNAL(leftClickedUrl(const QString&)), this, SLOT(slotOpenUrl(const QString&)));
-    connect(label_license, SIGNAL(leftClickedUrl(const QString&)), this, SLOT(slotOpenUrl(const QString&)));
+    connect(provider_info, SIGNAL(leftClickedUrl(QString&)), this, SLOT(slotOpenUrl(QString&)));
+    connect(label_license, SIGNAL(leftClickedUrl(QString&)), this, SLOT(slotOpenUrl(QString&)));
     connect(search_text, SIGNAL(returnPressed()), this, SLOT(slotStartSearch()));
     connect(search_results, &QListWidget::currentRowChanged, this, &ResourceWidget::slotUpdateCurrentItem);
     connect(button_preview, &QAbstractButton::clicked, this, [&](){

src/assets/view/widgets/slidewidget.cpp
@@ -220,5 +220,5 @@ const QString SlideWidget::getWipeString(wipeInfo info)
         break;
     }
     end.append(QString(" %1%").arg(info.endTransparency));
-    return QString("0=%1;-1=%2").arg(start).arg(end);
+    return QString("0=%1;-1=%2").arg(start, end);
 }

src/assets/view/widgets/urllistparamwidget.cpp
@@ -200,7 +200,6 @@ void UrlListParamWidget::openFile()
     QString urlString = QFileDialog::getOpenFileName(this, QString(), path, filter);
 
     if (!urlString.isEmpty()) {
-        QString path = QUrl(urlString).adjusted(QUrl::RemoveFilename).toString();
         KRecentDirs::add(QStringLiteral(":KdenliveUrlListParamFolder"), QUrl(urlString).adjusted(QUrl::RemoveFilename).toString());
         emit valueChanged(m_index, urlString, true);
         slotRefresh();

src/core.cpp
@@ -720,7 +720,7 @@ void Core::displayMessage(const QString &message, MessageType type, int timeout)
 
 void Core::loadingClips(int count)
 {
-    m_mainWindow->displayProgressMessage(i18n("Loading clips"), MessageType::ProcessingJobMessage, count);
+    emit m_mainWindow->displayProgressMessage(i18n("Loading clips"), MessageType::ProcessingJobMessage, count);
 }
 
 void Core::displayBinMessage(const QString &text, int type, const QList<QAction *> &actions, bool showClose, BinMessage::BinCategory messageCategory)

src/dialogs/kdenlivesettingsdialog.cpp
@@ -1127,7 +1127,7 @@ void KdenliveSettingsDialog::updateSettings()
         KdenliveSettings::setThumbColor1(m_configColors.kcfg_thumbColor1->color());
         KdenliveSettings::setThumbColor2(m_configColors.kcfg_thumbColor2->color());
         emit pCore->window()->getMainTimeline()->controller()->colorsChanged();
-        emit pCore->getMonitor(Kdenlive::ClipMonitor)->refreshAudioThumbs();
+        pCore->getMonitor(Kdenlive::ClipMonitor)->refreshAudioThumbs();
     }
 
     if (m_configSdl.kcfg_volume->value() != KdenliveSettings::volume()) {
@@ -1142,7 +1142,7 @@ void KdenliveSettingsDialog::updateSettings()
     if (m_configTimeline.kcfg_displayallchannels->isChecked() != KdenliveSettings::displayallchannels()) {
         KdenliveSettings::setDisplayallchannels(m_configTimeline.kcfg_displayallchannels->isChecked());
         emit audioThumbFormatChanged();
-        emit pCore->getMonitor(Kdenlive::ClipMonitor)->refreshAudioThumbs();
+        pCore->getMonitor(Kdenlive::ClipMonitor)->refreshAudioThumbs();
     }
 
     if (m_modified) {

src/dialogs/textbasededit.cpp
@@ -52,7 +52,7 @@ VideoTextEdit::VideoTextEdit(QWidget *parent)
     connect(this, &VideoTextEdit::cursorPositionChanged, [this]() {
         lineNumberArea->update();
     });
-    connect(verticalScrollBar(), &QScrollBar::valueChanged, [this]() {
+    connect(verticalScrollBar(), &QScrollBar::valueChanged, this, [this]() {
         lineNumberArea->update();
     });
     QRect rect =  this->contentsRect();

src/mainwindow.cpp
@@ -4476,7 +4476,7 @@ void MainWindow::slotSpeechRecognition()
 void MainWindow::slotCopyDebugInfo() {
     QString debuginfo = QStringLiteral("Kdenlive: %1\n").arg(KAboutData::applicationData().version());
     debuginfo.append(QStringLiteral("MLT: %1\n").arg(mlt_version_get_string()));
-    debuginfo.append(QStringLiteral("Qt: %1 (built against %2 %3)\n").arg(QString::fromLocal8Bit(qVersion())).arg(QT_VERSION_STR, QSysInfo::buildAbi()));
+    debuginfo.append(QStringLiteral("Qt: %1 (built against %2 %3)\n").arg(QString::fromLocal8Bit(qVersion()), QT_VERSION_STR, QSysInfo::buildAbi()));
     debuginfo.append(QStringLiteral("Frameworks: %2\n").arg(KCoreAddons::versionString()));
     debuginfo.append(QStringLiteral("System: %1\n").arg(QSysInfo::prettyProductName()));
     debuginfo.append(QStringLiteral("Kernel: %1 %2\n").arg(QSysInfo::kernelType(), QSysInfo::kernelVersion()));

commit 4d08de25cdc2969cf7a1856946887e3a83a4a503

Fix signal connection broke by 81ddab103f09a5ef827191d841d571ddcbe26e62

src/onlineresources/resourcewidget.cpp
@@ -78,8 +78,8 @@ ResourceWidget::ResourceWidget(QWidget *parent)
     }
     connect(service_list, SIGNAL(currentIndexChanged(int)), this, SLOT(slotChangeProvider()));
     loadConfig();
-    connect(provider_info, SIGNAL(leftClickedUrl(QString&)), this, SLOT(slotOpenUrl(QString&)));
-    connect(label_license, SIGNAL(leftClickedUrl(QString&)), this, SLOT(slotOpenUrl(QString&)));
+    connect(provider_info, SIGNAL(leftClickedUrl(const QString&)), this, SLOT(slotOpenUrl(const QString&)));
+    connect(label_license, SIGNAL(leftClickedUrl(const QString&)), this, SLOT(slotOpenUrl(const QString&)));
     connect(search_text, SIGNAL(returnPressed()), this, SLOT(slotStartSearch()));
     connect(search_results, &QListWidget::currentRowChanged, this, &ResourceWidget::slotUpdateCurrentItem);
     connect(button_preview, &QAbstractButton::clicked, this, [&](){