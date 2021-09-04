KDE/kdenlive

commit a69963ab998c07cb9b713913c413477c464a3028

Add advanced trimming tool: Slip
The slip tool allows to move in and out point of a (cut) clip by a certain amount while position and size of the clip stay the same.

Related to #1069

Commit includes:
- basis for advanced trimming tools (e.g. trimming preview in monitor)
- make editing tool available as enum in qml
- add slip tool
- add tests for slip

src/timeline2/view/qml/Ruler.qml
@@ -185,7 +185,9 @@ Item {
                         drag.smoothed: false
                         onDoubleClicked: timeline.editGuide(model.frame)
                         onClicked: {
-                            proxy.position = model.frame
+                            if (root.activeTool !== ProjectTool.SlipTool) {
+                                proxy.position = model.frame
+                            }
                         }
                         onEntered: {
                             rulerRoot.hoverGuide = true

src/definitions.h
@@ -131,7 +131,19 @@ enum ProjectItemType { ProjectClipType = 0, ProjectFolderType, ProjectSubclipTyp
 
 enum GraphicsRectItem { AVWidget = 70000, LabelWidget, TransitionWidget, GroupWidget };
 
-enum ProjectTool { SelectTool = 0, RazorTool = 1, SpacerTool = 2 };
+namespace ToolType {
+Q_NAMESPACE
+enum ProjectTool {
+    SelectTool = 0,
+    RazorTool = 1,
+    SpacerTool = 2,
+    RippleTool = 3,
+    RollTool = 4,
+    SlipTool = 5,
+    SlideTool = 6
+};
+Q_ENUM_NS(ProjectTool)
+}
 
 enum MonitorSceneType {
     MonitorSceneNone = 0,
@@ -140,7 +152,7 @@ enum MonitorSceneType {
     MonitorSceneCorners,
     MonitorSceneRoto,
     MonitorSceneSplit,
-    MonitorSceneRipple,
+    MonitorSceneTrimming,
     MonitorSplitTrack
 };
 

src/kdenliveui.rc
@@ -56,6 +56,7 @@
       <Action name="select_tool" />
       <Action name="razor_tool" />
       <Action name="spacer_tool" />
+      <Action name="slip_tool" />
     </Menu>
 
     <Menu name="clip" ><text>&amp;Clip</text>
@@ -282,6 +283,7 @@
     <Action name="select_tool" />
     <Action name="razor_tool" />
     <Action name="spacer_tool" />
+    <Action name="slip_tool" />
     <Separator />
     <Action name="timeline_timecode" />
     <Separator />

src/main.cpp
@@ -235,9 +235,9 @@ int main(int argc, char *argv[])
                                      "ClipState",                     // name in QML
                                      "Error: only enums");
     qmlRegisterUncreatableMetaObject(FileStatus::staticMetaObject, // static meta object
-                                     "com.enums",                     // import statement
-                                     1, 0,                            // major and minor version of the import
-                                     "ClipStatus",                     // name in QML
+                                     "com.enums",                  // import statement
+                                     1, 0,                         // major and minor version of the import
+                                     "ClipStatus",                 // name in QML
                                      "Error: only enums");
     qmlRegisterUncreatableMetaObject(ClipType::staticMetaObject, // static meta object
                                      "com.enums",                // import statement
@@ -245,9 +245,14 @@ int main(int argc, char *argv[])
                                      "ProducerType",             // name in QML
                                      "Error: only enums");
     qmlRegisterUncreatableMetaObject(AssetListType::staticMetaObject, // static meta object
+                                     "com.enums",                     // import statement
+                                     1, 0,                            // major and minor version of the import
+                                     "AssetType",                     // name in QML
+                                     "Error: only enums");
+    qmlRegisterUncreatableMetaObject(ToolType::staticMetaObject, // static meta object
                                      "com.enums",                // import statement
                                      1, 0,                       // major and minor version of the import
-                                     "AssetType",             // name in QML
+                                     "ProjectTool",              // name in QML
                                      "Error: only enums");
     if (parser.value(QStringLiteral("mlt-log")) == QStringLiteral("verbose")) {
         mlt_log_set_level(MLT_LOG_VERBOSE);

src/mainwindow.cpp
@@ -1205,10 +1205,41 @@ void MainWindow::setupActions()
     // toolbar->addAction(m_buttonSpacerTool);
     m_buttonSpacerTool->setCheckable(true);
     m_buttonSpacerTool->setChecked(false);
+
+    /* TODO Implement Ripple
+    // TODO icon available (and properly working) in KF 5.86
+    m_buttonRippleTool = new QAction(QIcon::fromTheme(QStringLiteral("kdenlive-ripple")), i18n("Ripple tool"), this);
+    m_buttonRippleTool->setCheckable(true);
+    m_buttonRippleTool->setChecked(false);*/
+
+    /* TODO Implement Roll
+    // TODO icon available (and properly working) in KF 5.86
+    m_buttonRollTool = new QAction(QIcon::fromTheme(QStringLiteral("kdenlive-rolling")), i18n("Roll tool"), this);
+
+    m_buttonRollTool->setCheckable(true);
+    m_buttonRollTool->setChecked(false);*/
+
+    // TODO icon available (and properly working) in KF 5.86
+    //m_buttonSlipTool = new QAction(QIcon::fromTheme(QStringLiteral("kdenlive-slip")), i18n("Slip tool"), this);
+    m_buttonSlipTool = new QAction(QIcon::fromTheme(QStringLiteral("transform-move-horizontal")), i18n("Slip tool"), this);
+    m_buttonSlipTool->setCheckable(true);
+    m_buttonSlipTool->setChecked(false);
+
+    /* TODO Implement Slide
+    // TODO icon available (and properly working) in KF 5.86
+    m_buttonSlideTool = new QAction(QIcon::fromTheme(QStringLiteral("kdenlive-slide")), i18n("Slide tool"), this);
+    m_buttonSlideTool->setCheckable(true);
+    m_buttonSlideTool->setChecked(false);*/
+
     auto *toolGroup = new QActionGroup(this);
     toolGroup->addAction(m_buttonSelectTool);
     toolGroup->addAction(m_buttonRazorTool);
     toolGroup->addAction(m_buttonSpacerTool);
+    //toolGroup->addAction(m_buttonRippleTool);
+    //toolGroup->addAction(m_buttonRollTool);
+    toolGroup->addAction(m_buttonSlipTool);
+    //toolGroup->addAction(m_buttonSlideTool);
+
     toolGroup->setExclusive(true);
     
     QAction *collapseItem = new QAction(QIcon::fromTheme(QStringLiteral("collapse-all")), i18n("Collapse/Expand Item"), this);
@@ -1349,6 +1380,10 @@ void MainWindow::setupActions()
     addAction(QStringLiteral("select_tool"), m_buttonSelectTool, Qt::Key_S);
     addAction(QStringLiteral("razor_tool"), m_buttonRazorTool, Qt::Key_X);
     addAction(QStringLiteral("spacer_tool"), m_buttonSpacerTool, Qt::Key_M);
+    //addAction(QStringLiteral("ripple_tool"), m_buttonRippleTool);
+    //addAction(QStringLiteral("roll_tool"), m_buttonRollTool);
+    addAction(QStringLiteral("slip_tool"), m_buttonSlipTool);
+    //addAction(QStringLiteral("slide_tool"), m_buttonSlideTool);
 
     addAction(QStringLiteral("automatic_transition"), m_buttonTimelineTags);
     addAction(QStringLiteral("show_video_thumbs"), m_buttonVideoThumbs);
@@ -2312,8 +2347,6 @@ void MainWindow::connectDocument()
     connect(trackView->projectView(), &CustomTrackView::guidesUpdated, this, &MainWindow::slotGuidesUpdated);
     connect(trackView->projectView(), &CustomTrackView::loadMonitorScene, m_projectMonitor, &Monitor::slotShowEffectScene);
     connect(trackView->projectView(), &CustomTrackView::setQmlProperty, m_projectMonitor, &Monitor::setQmlProperty);
-    connect(m_projectMonitor, SIGNAL(acceptRipple(bool)), trackView->projectView(), SLOT(slotAcceptRipple(bool)));
-    connect(m_projectMonitor, SIGNAL(switchTrimMode(int)), trackView->projectView(), SLOT(switchTrimMode(int)));
     connect(project, &KdenliveDoc::saveTimelinePreview, trackView, &Timeline::slotSaveTimelinePreview);
 
     connect(trackView, SIGNAL(showTrackEffects(int, TrackInfo)), this, SLOT(slotTrackSelected(int, TrackInfo)));
@@ -2419,9 +2452,6 @@ void MainWindow::connectDocument()
 
     // Update guides info in render widget
     // slotGuidesUpdated();
-
-    // set tool to select tool
-    setTrimMode(QString());
 
     m_buttonSelectTool->setChecked(true);
     connect(m_projectMonitorDock, &QDockWidget::visibilityChanged, m_projectMonitor, &Monitor::slotRefreshMonitor, Qt::UniqueConnection);
@@ -3152,11 +3182,19 @@ void MainWindow::slotClipEnd()
 void MainWindow::slotChangeTool(QAction *action)
 {
     if (action == m_buttonSelectTool) {
-        slotSetTool(SelectTool);
+        slotSetTool(ToolType::SelectTool);
     } else if (action == m_buttonRazorTool) {
-        slotSetTool(RazorTool);
+        slotSetTool(ToolType::RazorTool);
     } else if (action == m_buttonSpacerTool) {
-        slotSetTool(SpacerTool);
+        slotSetTool(ToolType::SpacerTool);
+    } if (action == m_buttonRippleTool) {
+        slotSetTool(ToolType::RippleTool);
+    } if (action == m_buttonRollTool) {
+        slotSetTool(ToolType::RollTool);
+    } if (action == m_buttonSlipTool) {
+        slotSetTool(ToolType::SlipTool);
+    } if (action == m_buttonSlideTool) {
+        slotSetTool(ToolType::SlideTool);
     }
 }
 
@@ -3180,7 +3218,7 @@ void MainWindow::slotChangeEdit(QAction *action)
         // Disable spacer tool in insert mode
         if (m_buttonSpacerTool->isChecked()) {
             m_buttonSelectTool->setChecked(true);
-            slotSetTool(SelectTool);
+            slotSetTool(ToolType::SelectTool);
         }
         m_buttonSpacerTool->setEnabled(false);
     } else {
@@ -3188,11 +3226,12 @@ void MainWindow::slotChangeEdit(QAction *action)
     }
 }
 
-void MainWindow::slotSetTool(ProjectTool tool)
+void MainWindow::slotSetTool(ToolType::ProjectTool tool)
 {
     if (pCore->currentDoc()) {
         showToolMessage();
         getMainTimeline()->setTool(tool);
+        getMainTimeline()->controller()->updateTrimmingMode();
     }
 }
 
@@ -3200,11 +3239,13 @@ void MainWindow::showToolMessage()
 {
     QString message;
     if (m_buttonSelectTool->isChecked()) {
-        message = i18n("<b>Shift drag</b> for rubber-band selection, <b>Shift click</b> for multiple selection, <b>Ctrl drag</b> to pan");
+        message = xi18nc("@info:whatsthis", "<shortcut>Shift drag</shortcut> for rubber-band selection, <shortcut>Shift click</shortcut> for multiple selection, <shortcut>Ctrl drag</shortcut> to pan");
     } else if (m_buttonRazorTool->isChecked()) {
-        message = i18n("<b>Shift</b> to preview cut frame");
+        message = xi18nc("@info:whatsthis", "<shortcut>Shift</shortcut> to preview cut frame");
     } else if (m_buttonSpacerTool->isChecked()) {
-        message = i18n("<b>Ctrl</b> to apply on current track only, <b>Shift</b> to also move guides. You can combine both modifiers.");
+        message = xi18nc("@info:whatsthis", "<shortcut>Ctrl</shortcut> to apply on current track only, <shortcut>Shift</shortcut> to also move guides. You can combine both modifiers.");
+    } else if (m_buttonSlipTool->isChecked()) {
+        message = xi18nc("@info:whatsthis", "<shortcut>Click</shortcut> on an item to slip, <shortcut>Shift</shortcut> to slip only current item of the group"); //TODO
     }
     m_messageLabel->setKeyMap(message);
 }
@@ -4226,27 +4267,6 @@ void MainWindow::forceIconSet(bool force)
         KMessageBox::Continue) {
         slotRestart();
     }
-}
-
-void MainWindow::slotSwitchTrimMode()
-{
-    // TODO refac
-    /*
-    if (pCore->projectManager()->currentTimeline()) {
-        pCore->projectManager()->currentTimeline()->projectView()->switchTrimMode();
-    }
-    */
-}
-
-void MainWindow::setTrimMode(const QString &mode){
-    Q_UNUSED(mode)
-    // TODO refac
-    /*
-    if (pCore->projectManager()->currentTimeline()) {
-        m_trimLabel->setText(mode);
-        m_trimLabel->setVisible(!mode.isEmpty());
-    }
-    */
 }
 
 TimelineWidget *MainWindow::getMainTimeline() const

src/mainwindow.h
@@ -250,6 +250,10 @@ private:
     QAction *m_buttonSelectTool;
     QAction *m_buttonRazorTool;
     QAction *m_buttonSpacerTool;
+    QAction *m_buttonRippleTool;
+    QAction *m_buttonRollTool;
+    QAction *m_buttonSlipTool;
+    QAction *m_buttonSlideTool;
     QAction *m_buttonSnap;
     QAction *m_saveAction;
     QSlider *m_zoomSlider;
@@ -395,7 +399,7 @@ private slots:
     void slotAddProjectClipList(const QList<QUrl> &urls);
     void slotChangeTool(QAction *action);
     void slotChangeEdit(QAction *action);
-    void slotSetTool(ProjectTool tool);
+    void slotSetTool(ToolType::ProjectTool tool);
     void slotSnapForward();
     void slotSnapRewind();
     void slotGuideForward();
@@ -512,9 +516,6 @@ private slots:
     void slotUpdateCompositing(QAction *compose);
     /** @brief Update compositing action to display current project setting. */
     void slotUpdateCompositeAction(int mode);
-    /** @brief Cycle through the different timeline trim modes. */
-    void slotSwitchTrimMode();
-    void setTrimMode(const QString &mode);
     /** @brief Set timeline toolbar icon size. */
     void setTimelineToolbarIconSize(QAction *a);
     void slotEditItemSpeed();

src/monitor/glwidget.cpp
@@ -953,7 +953,9 @@ int GLWidget::setProducer(const std::shared_ptr<Mlt::Producer> &producer, bool i
         m_proxy->resetPosition();
     }
     m_consumer->set("scrub_audio", 0);
-    m_proxy->setPosition(position > 0 ? position : m_producer->position());
+    if(position != -2) {
+        m_proxy->setPosition(position > 0 ? position : m_producer->position());
+    }
     return error;
 }
 

src/monitor/glwidget.h
@@ -53,7 +53,8 @@ class MonitorProxy;
 
 using thread_function_t = void *(*)(void *);
 
-/* QQuickView that renders an .
+/** @class GLWidget
+ *  @brief QQuickView that renders an .
  *
  * Creates an MLT consumer and renders a GL view from the consumer. This pipeline is one of:
  *
@@ -146,7 +147,13 @@ public:
 protected:
     void mouseReleaseEvent(QMouseEvent *event) override;
     void mouseDoubleClickEvent(QMouseEvent *event) override;
-    /** @brief Update producer, should ONLY be called from monitor */
+    /** @brief Update producer, should ONLY be called from monitor
+    * @param producer
+    * @param isActive
+    * @param position If == 0 producer position will be used.
+    * If == -1 consumer position will be used if possible.
+    * If == -2 position will not be set.
+    */
     int setProducer(const std::shared_ptr<Mlt::Producer> &producer, bool isActive, int position);
     int setProducer(const QString &file);
     QString frameToTime(int frames) const;

commit a06cefed768d0f9233eda6845b2670dc165b93fc

Fix seek to guide on click
Broken by a69963ab998c07cb9b713913c413477c464a3028

src/timeline2/view/qml/Ruler.qml
@@ -18,6 +18,7 @@
 
 import QtQuick 2.11
 import QtQuick.Controls 2.4
+import com.enums 1.0
 
 Item {
     id: rulerRoot