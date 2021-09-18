9c26e1048c945cfb3b2df27a4e130d0772579592
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-09-18 17:43:03 +0200

    SPDX cleanup and manual conversion

--- testingArea/audioOffset.cpp
+++ testingArea/audioOffset.cpp
@@ -1,11 +1,8 @@
 /*
-SPDX-FileCopyrightText: 2012 Simon A. Eugster (Granjow)  <simon.eu@gmail.com>
-This file is part of kdenlive. See www.kdenlive.org.
+    SPDX-FileCopyrightText: 2012 Simon A. Eugster (Granjow)  <simon.eu@gmail.com>
+    This file is part of kdenlive. See www.kdenlive.org.
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+    SPDX-License-Identifier: GPL-3.0-or-later
 */
 
 #include <QCoreApplication>
--- thumbnailer/mltpreview.h
+++ thumbnailer/mltpreview.h
@@ -1,19 +1,9 @@
 /*
-    Copyright (C) 2006-2008
-    by Marco Gulino <marco@kmobiletools.org>
-    adapted for MLT video preview by Jean-Baptiste Mardelle <jb@kdenlive.org>
+    SPDX-FileCopyrightText: 2006-2008Marco Gulino <marco@kmobiletools.org>
+    adapted for MLT video preview by Jean-Baptiste Mardelle
+    SPDX-FileCopyrightText: Jean-Baptiste Mardelle <jb@kdenlive.org>
 
     SPDX-License-Identifier: GPL-2.0-or-later
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the
-    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
 */
 
 #ifndef MLTPREVIEW_H
