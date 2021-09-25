82df289b43163486ca0ceaa656726021ef9ed3ac
Jean-Baptiste Mardelle <jb@kdenlive.org>
2021-09-25 06:26:52 +0200

    Remove broken "duplicate bin clip with timeremap" stuff that caused crash dropping playlists in timeline BUG: 441777 FIXED-IN:

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3577,12 +3577,6 @@ void MainWindow::buildDynamicActions()
                 [&]() { SpeedTask::start(this); });
     }
 
-    if (true /* TODO: check if timeremap link is available */) {
-        QAction *action = new QAction(i18n("Duplicate clip with time remap"), m_extraFactory->actionCollection());
-        ts->addAction(action->text(), action);
-        connect(action, &QAction::triggered, pCore->bin(), &Bin::remapCurrent);
-    }
-
     // TODO refac reimplement analyseclipjob
     /*
     QAction *action = new QAction(i18n("Analyse keyframes"), m_extraFactory->actionCollection());
