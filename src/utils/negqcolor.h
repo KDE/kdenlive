024e44d23d8b75bc3236eef9f0fb5f0be50c3bd2
Julius Künzel <jk.kdedev@smartlab.uber.space>
2021-12-07 01:27:01 +0100

    Re-use NegQColor
    Recognized thanks to new CI

---
+++ src/utils/negqcolor.h
@@ -0,0 +1,37 @@
+/*
+    SPDX-FileCopyrightText: 2017 Dušan Hanuš <hanus@pixelhouse.cz>
+    SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+*/
+
+#ifndef NEGQCOLOR_H
+#define NEGQCOLOR_H
+
+#include <QtGlobal>
+#include <QColor>
+
+class QColor;
+
+class NegQColor
+{
+public:
+    NegQColor();
+    int8_t sign_r = 1;
+    int8_t sign_g = 1;
+    int8_t sign_b = 1;
+    QColor qcolor;
+    static NegQColor fromHsvF(qreal h, qreal s, qreal l, qreal a = 1.0);
+    static NegQColor fromRgbF(qreal r, qreal g, qreal b, qreal a = 1.0);
+    qreal redF() const;
+    void setRedF(qreal val);
+    qreal greenF() const;
+    void setGreenF(qreal val);
+    qreal blueF() const;
+    void setBlueF(qreal val);
+    qreal valueF() const;
+    void setValueF(qreal val);
+    int hue() const;
+    qreal hueF() const;
+    qreal saturationF() const;
+};
+
+#endif // NEGQCOLOR_H
