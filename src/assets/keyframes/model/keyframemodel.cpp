KDE/kdenlive

commit b5de156b825399b0765b9e4f6dc8e738ab31f904

Fix concurrency issue breaking effect parameters. Related to #1359

src/assets/keyframes/model/keyframemodel.cpp
@@ -883,7 +883,7 @@ void KeyframeModel::parseAnimProperty(const QString &prop)
 {
     Fun undo = []() { return true; };
     Fun redo = []() { return true; };
-    disconnect(this, &KeyframeModel::modelChanged, this, &KeyframeModel::sendModification);
+    QSignalBlocker bk(this);
     removeAllKeyframes(undo, redo);
     int in = 0;
     int out = 0;
@@ -938,7 +938,6 @@ void KeyframeModel::parseAnimProperty(const QString &prop)
         }
         addKeyframe(GenTime(frame, pCore->getCurrentFps()), convertFromMltType(type), value, true, undo, redo);
     }
-    connect(this, &KeyframeModel::modelChanged, this, &KeyframeModel::sendModification);
 }
 
 void KeyframeModel::resetAnimProperty(const QString &prop)
@@ -947,7 +946,7 @@ void KeyframeModel::resetAnimProperty(const QString &prop)
     Fun redo = []() { return true; };
 
     // Delete all existing keyframes
-    disconnect(this, &KeyframeModel::modelChanged, this, &KeyframeModel::sendModification);
+    QSignalBlocker bk(this);
     removeAllKeyframes(undo, redo);
 
     Mlt::Properties mlt_prop;
@@ -1014,7 +1013,6 @@ void KeyframeModel::resetAnimProperty(const QString &prop)
     PUSH_LAMBDA(update_local, undo);
     PUSH_LAMBDA(update_local, redo);
     PUSH_UNDO(undo, redo, i18n("Reset %1", effectName));
-    connect(this, &KeyframeModel::modelChanged, this, &KeyframeModel::sendModification);
 }
 
 void KeyframeModel::parseRotoProperty(const QString &prop)

src/widgets/doublewidget.cpp
@@ -57,9 +57,8 @@ void DoubleWidget::setSpinSize(int width)
 
 void DoubleWidget::setValue(double value)
 {
-    m_dragVal->blockSignals(true);
+    QSignalBlocker bk(m_dragVal);
     m_dragVal->setValue(value * m_factor);
-    m_dragVal->blockSignals(false);
 }
 
 void DoubleWidget::enableEdit(bool enable)

commit 24f760e4fd1cd3ab66023d5038665e166a6b8a1c

Fix resetting effect does not clear timeline keyframe view, resulting in possible crash. Caused introduced in b5de156b

src/assets/keyframes/model/keyframemodel.cpp
@@ -883,7 +883,7 @@ void KeyframeModel::parseAnimProperty(const QString &prop)
 {
     Fun undo = []() { return true; };
     Fun redo = []() { return true; };
-    QSignalBlocker bk(this);
+    disconnect(this, &KeyframeModel::modelChanged, this, &KeyframeModel::sendModification);
     removeAllKeyframes(undo, redo);
     int in = 0;
     int out = 0;
@@ -938,6 +938,7 @@ void KeyframeModel::parseAnimProperty(const QString &prop)
         }
         addKeyframe(GenTime(frame, pCore->getCurrentFps()), convertFromMltType(type), value, true, undo, redo);
     }
+    connect(this, &KeyframeModel::modelChanged, this, &KeyframeModel::sendModification);
 }
 
 void KeyframeModel::resetAnimProperty(const QString &prop)