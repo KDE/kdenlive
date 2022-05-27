81e07119ea035c1704e0a7ddaa6a248ef43935c6
Jean-Baptiste Mardelle <jb@kdenlive.org>
2022-05-27 13:56:11 +0200

    Fix tests on system without avformat module

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -46,13 +46,21 @@ QString createAVProducer(Mlt::Profile &prof, std::shared_ptr<ProjectItemModel> b
     std::shared_ptr<Mlt::Producer> producer = std::make_shared<Mlt::Producer>(prof, QFileInfo(sourcesPath + "/small.mkv").absoluteFilePath().toStdString().c_str());
 
     // In case the test system does not have avformat support, we can switch to the integrated blipflash producer
-    /*if (!producer->is_valid()) {
+    int length = -1;
+    if (!producer || !producer->is_valid()) {
+        length = 40;
         producer.reset(new Mlt::Producer(prof, "blipflash"));
-    }*/
+        producer->set("length", length);
+        producer->set_in_and_out(0, length - 1);
+        producer->set("kdenlive:duration", length);
+    }
     REQUIRE(producer->is_valid());
 
     QString binId = QString::number(binModel->getFreeClipId());
     auto binClip = ProjectClip::construct(binId, QIcon(), binModel, producer);
+    if (length > -1) {
+        binClip->forceLimitedDuration();
+    }
     Fun undo = []() { return true; };
     Fun redo = []() { return true; };
     REQUIRE(binModel->addItem(binClip, binModel->getRootFolder()->clipId(), undo, redo));
