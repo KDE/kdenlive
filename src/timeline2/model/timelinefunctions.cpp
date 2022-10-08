834732dc785c1e5da64e7b3d3c18f201d56b5370
Jean-Baptiste Mardelle <jb@kdenlive.org>
2022-10-08 16:30:36 +0200

    Fix tests

--- src/timeline2/model/timelinefunctions.cpp
+++ src/timeline2/model/timelinefunctions.cpp
@@ -1853,7 +1853,9 @@ bool TimelineFunctions::pasteClips(const std::shared_ptr<TimelineItemModel> &tim
                 pCore->projectItemModel()->requestAddBinClip(updatedId, currentProd, folderId, undo, redo, callBack);
             }
         }
-        pCore->getMonitor(Kdenlive::ProjectMonitor)->requestSeek(position + pasteDuration);
+        if (pCore->window()) {
+            pCore->getMonitor(Kdenlive::ProjectMonitor)->requestSeek(position + pasteDuration);
+        }
     }
 
     if (!docId.isEmpty() && docId != pCore->currentDoc()->getDocumentProperty(QStringLiteral("documentid"))) {
@@ -1866,7 +1868,9 @@ bool TimelineFunctions::pasteClips(const std::shared_ptr<TimelineItemModel> &tim
             pCore->projectItemModel()->requestAddFolder(folderId, i18n("Pasted clips"), rootId, undo, redo);
         }
         QDomNodeList binClips = copiedItems.documentElement().elementsByTagName(QStringLiteral("producer"));
-        pCore->getMonitor(Kdenlive::ProjectMonitor)->requestSeek(position + pasteDuration);
+        if (pCore->window()) {
+            pCore->getMonitor(Kdenlive::ProjectMonitor)->requestSeek(position + pasteDuration);
+        }
         for (int i = 0; i < binClips.count(); ++i) {
             QDomElement currentProd = binClips.item(i).toElement();
             QString clipId = Xml::getXmlProperty(currentProd, QStringLiteral("kdenlive:id"));
