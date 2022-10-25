1ba5910716079c7d131b2617d30772e5980810af
Julius Künzel <jk.kdedev@smartlab.uber.space>
2022-10-25 20:31:46 +0200

    Add missing license header

--- src/widgets/markercategorychooser.cpp
+++ src/widgets/markercategorychooser.cpp
@@ -1,3 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Julius Künzel <jk.kdedev@smartlab.uber.space>
+ * SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+ */
 #include "markercategorychooser.h"
 #include "../bin/model/markerlistmodel.hpp"
 
--- src/widgets/markercategorychooser.h
+++ src/widgets/markercategorychooser.h
@@ -1,3 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Julius Künzel <jk.kdedev@smartlab.uber.space>
+ * SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+ */
 #ifndef MARKERCATEGORYCHOOSER_H
 #define MARKERCATEGORYCHOOSER_H
 
