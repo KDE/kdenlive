daa0c02cd1a503f2b5722d785e5bffeeb1953923
Julius Künzel <jk.kdedev@smartlab.uber.space>
2022-12-04 13:12:02 +0100

    [kdenlive_render] Port kdenlive_render to QCommandLineParser
    At the same time we remove the totally outdated and hence wrong
    documentation.
    
    However this also changes how to use the tool (ie. which arguments to
    give)

--- renderer/kdenlive_render.cpp
+++ renderer/kdenlive_render.cpp
@@ -7,19 +7,177 @@
 #include "../src/lib/localeHandling.h"
 #include "mlt++/Mlt.h"
 #include "renderjob.h"
-#include <QApplication>
+#include <../config-kdenlive.h>
+#include <QCommandLineParser>
+#include <QCoreApplication>
 #include <QDebug>
 #include <QDir>
 #include <QDomDocument>
 
 int main(int argc, char **argv)
 {
-    QApplication app(argc, argv);
-    QStringList args = app.arguments();
-    QStringList preargs;
-    QString subtitleFile;
-    if (args.count() >= 4) {
-        // Remove program name
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("kdenlive_render");
+    QCoreApplication::setApplicationVersion(KDENLIVE_VERSION);
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Kdenlive video renderer for MLT");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    parser.addPositionalArgument("mode", "Render mode. Either \"delivery\" or \"preview-chunks\".");
+    parser.parse(QCoreApplication::arguments());
+    QStringList args = parser.positionalArguments();
+    const QString mode = args.isEmpty() ? QString() : args.first();
+
+    if (mode == "preview-chunks") {
+
+        parser.clearPositionalArguments();
+        parser.addPositionalArgument("preview-chunks", "Render splited in to multiple files for timeline preview.");
+        parser.addPositionalArgument("source", "Source file (usually MLT XML).");
+        parser.addPositionalArgument("destination", "Destination directory.");
+        parser.addPositionalArgument("chunks", "Chunks to render.");
+        parser.addPositionalArgument("chunk_size", "Chunks to render.");
+        parser.addPositionalArgument("profile_path", "Path to profile.");
+        parser.addPositionalArgument("file_extension", "Rendered file extension.");
+        parser.addPositionalArgument("args", "Space separated libavformat arguments.", "[arg1 arg2 ...]");
+
+        parser.process(app);
+        args = parser.positionalArguments();
+        if (args.count() < 7) {
+            qCritical() << "Error: not enough arguments specified\n";
+            parser.showHelp(1);
+            // the command above will quit the app with return 1;
+        }
+        // After initialising the MLT factory, set the locale back from user default to C
+        // to ensure numbers are always serialised with . as decimal point.
+        Mlt::Factory::init();
+        LocaleHandling::resetAllLocale();
+
+        // mode
+        args.removeFirst();
+        // Source playlist path
+        QString playlist = args.at(0);
+        args.removeFirst();
+        // destination - where to save result
+        QDir baseFolder(args.at(0));
+        args.removeFirst();
+        // chunks to render
+        QStringList chunks = args.at(0).split(QLatin1Char(','), Qt::SkipEmptyParts);
+        args.removeFirst();
+        // chunk size in frames
+        int chunkSize = args.at(0).toInt();
+        args.removeFirst();
+        // path to profile
+        Mlt::Profile profile(args.at(0).toUtf8().constData());
+
+        args.removeFirst();
+        // rendered file extension
+        QString extension = args.at(0);
+        args.removeFirst();
+        // avformat consumer params
+        QStringList consumerParams = args.at(0).split(QLatin1Char(' '), Qt::SkipEmptyParts);
+        args.removeFirst();
+
+        profile.set_explicit(1);
+        Mlt::Producer prod(profile, nullptr, playlist.toUtf8().constData());
+        if (!prod.is_valid()) {
+            fprintf(stderr, "INVALID playlist: %s \n", playlist.toUtf8().constData());
+            return 1;
+        }
+        const char *localename = prod.get_lcnumeric();
+        QLocale::setDefault(QLocale(localename));
+
+        int currentFrame = 0;
+        int rangeStart = 0;
+        int rangeEnd = 0;
+        QString frame;
+        while (!chunks.isEmpty()) {
+            if (rangeEnd == 0) {
+                // We are not processing a range
+                frame = chunks.first();
+            }
+            if (rangeEnd > 0) {
+                // We are processing a range
+                currentFrame += chunkSize + 1;
+                frame = QString::number(currentFrame);
+                if (currentFrame >= rangeEnd) {
+                    // End of range
+                    rangeStart = 0;
+                    rangeEnd = 0;
+                    // Range is processed, remove from stack
+                    chunks.removeFirst();
+                }
+            } else if (frame.contains(QLatin1Char('-'))) {
+                rangeStart = frame.section(QLatin1Char('-'), 0, 0).toInt();
+                rangeEnd = frame.section(QLatin1Char('-'), 1, 1).toInt();
+                currentFrame = rangeStart;
+                frame = QString::number(currentFrame);
+            } else {
+                // Frame will be processed, remove from stack
+                chunks.removeFirst();
+            }
+            fprintf(stderr, "START:%d \n", frame.toInt());
+            QString fileName = QStringLiteral("%1.%2").arg(frame, extension);
+            if (baseFolder.exists(fileName)) {
+                // Don't overwrite an existing file
+                fprintf(stderr, "DONE:%d \n", frame.toInt());
+                continue;
+            }
+            QScopedPointer<Mlt::Producer> playlst(prod.cut(frame.toInt(), frame.toInt() + chunkSize));
+            QScopedPointer<Mlt::Consumer> cons(
+                new Mlt::Consumer(profile, QString("avformat:%1").arg(baseFolder.absoluteFilePath(fileName)).toUtf8().constData()));
+            for (const QString &param : qAsConst(consumerParams)) {
+                if (param.contains(QLatin1Char('='))) {
+                    cons->set(param.section(QLatin1Char('='), 0, 0).toUtf8().constData(), param.section(QLatin1Char('='), 1).toUtf8().constData());
+                }
+            }
+            if (!cons->is_valid()) {
+                fprintf(stderr, " = =  = INVALID CONSUMER\n\n");
+                return 1;
+            }
+            cons->set("terminate_on_pause", 1);
+            cons->connect(*playlst);
+            playlst.reset();
+            cons->run();
+            cons->stop();
+            cons->purge();
+            fprintf(stderr, "DONE:%d \n", frame.toInt());
+        }
+        // Mlt::Factory::close();
+        fprintf(stderr, "+ + + RENDERING FINISHED + + + \n");
+        return 0;
+    }
+
+    if (mode == "delivery") {
+        parser.clearPositionalArguments();
+        parser.addPositionalArgument("delivery", "Render to a final output file.");
+        parser.addPositionalArgument("renderer", "Path to MLT melt renderer.");
+        parser.addPositionalArgument("source", "Source file (usually MLT XML).");
+        parser.addPositionalArgument("destination", "Destination file.");
+
+        QCommandLineOption pidOption("pid", "Process ID to send back progress.", "pid");
+        parser.addOption(pidOption);
+
+        QCommandLineOption inOption("in", "Start rendering at frame pos", "frame");
+        parser.addOption(inOption);
+
+        QCommandLineOption outOption("out", "End rendering at frame pos.", "frame");
+        parser.addOption(outOption);
+
+        QCommandLineOption subtitleOption("subtitle", "Subtitle file.", "file");
+        parser.addOption(subtitleOption);
+
+        parser.process(app);
+        args = parser.positionalArguments();
+
+        if (args.count() < 4) {
+            qCritical() << "Error: not enough arguments specified\n";
+            parser.showHelp(1);
+            // the command above will quit the app with return 1;
+        }
+
+        // mode
         args.removeFirst();
         // renderer path (melt)
         QString render = args.at(0);
@@ -30,120 +188,13 @@ int main(int argc, char **argv)
         // target - where to save result
         QString target = args.at(0);
         args.removeFirst();
-        int pid = 0;
-        // pid to send back progress
-        if (args.count() > 0 && args.at(0).startsWith(QLatin1String("-pid:"))) {
-            pid = args.at(0).section(QLatin1Char(':'), 1).toInt();
-            args.removeFirst();
+
+        int pid = parser.value(pidOption).toInt();
+        int out = parser.value(outOption).toInt();
+        if (out == 0) {
+            out = -1;
         }
-        int in = -1;
-        int out = -1;
-
-        if (args.count() > 1 && args.at(0) == QLatin1String("-out")) {
-            args.removeFirst();
-            out = args.at(0).toInt();
-            args.removeFirst();
-        }
-
-        // Do we want a split render
-        if (args.count() > 5 && args.at(0) == QLatin1String("-split")) {
-            args.removeFirst();
-            // chunks to render
-            QStringList chunks = args.at(0).split(QLatin1Char(','), Qt::SkipEmptyParts);
-            args.removeFirst();
-            // chunk size in frames
-            int chunkSize = args.at(0).toInt();
-            args.removeFirst();
-            // chunk size in frames
-            QString profilePath = args.at(0);
-            args.removeFirst();
-            // rendered file extension
-            QString extension = args.at(0);
-            args.removeFirst();
-            // avformat consumer params
-            QStringList consumerParams = args.at(0).split(QLatin1Char(' '), Qt::SkipEmptyParts);
-            args.removeFirst();
-            QDir baseFolder(target);
-
-            // After initialising the MLT factory, set the locale back from user default to C
-            // to ensure numbers are always serialised with . as decimal point.
-            Mlt::Factory::init();
-            LocaleHandling::resetAllLocale();
-
-            Mlt::Profile profile(profilePath.toUtf8().constData());
-            profile.set_explicit(1);
-            Mlt::Producer prod(profile, nullptr, playlist.toUtf8().constData());
-            if (!prod.is_valid()) {
-                fprintf(stderr, "INVALID playlist: %s \n", playlist.toUtf8().constData());
-                return 1;
-            }
-            const char *localename = prod.get_lcnumeric();
-            QLocale::setDefault(QLocale(localename));
-
-            int currentFrame = 0;
-            int rangeStart = 0;
-            int rangeEnd = 0;
-            QString frame;
-            while (!chunks.isEmpty()) {
-                if (rangeEnd == 0) {
-                    // We are not processing a range
-                    frame = chunks.first();
-                }
-                if (rangeEnd > 0) {
-                    // We are processing a range
-                    currentFrame += chunkSize + 1;
-                    frame = QString::number(currentFrame);
-                    if (currentFrame >= rangeEnd) {
-                        // End of range
-                        rangeStart = 0;
-                        rangeEnd = 0;
-                        // Range is processed, remove from stack
-                        chunks.removeFirst();
-                    }
-                } else if (frame.contains(QLatin1Char('-'))) {
-                    rangeStart = frame.section(QLatin1Char('-'), 0, 0).toInt();
-                    rangeEnd = frame.section(QLatin1Char('-'), 1, 1).toInt();
-                    currentFrame = rangeStart;
-                    frame = QString::number(currentFrame);
-                } else {
-                    // Frame will be processed, remove from stack
-                    chunks.removeFirst();
-                }
-                fprintf(stderr, "START:%d \n", frame.toInt());
-                QString fileName = QStringLiteral("%1.%2").arg(frame, extension);
-                if (baseFolder.exists(fileName)) {
-                    // Don't overwrite an existing file
-                    fprintf(stderr, "DONE:%d \n", frame.toInt());
-                    continue;
-                }
-                QScopedPointer<Mlt::Producer> playlst(prod.cut(frame.toInt(), frame.toInt() + chunkSize));
-                QScopedPointer<Mlt::Consumer> cons(
-                    new Mlt::Consumer(profile, QString("avformat:%1").arg(baseFolder.absoluteFilePath(fileName)).toUtf8().constData()));
-                for (const QString &param : qAsConst(consumerParams)) {
-                    if (param.contains(QLatin1Char('='))) {
-                        cons->set(param.section(QLatin1Char('='), 0, 0).toUtf8().constData(), param.section(QLatin1Char('='), 1).toUtf8().constData());
-                    }
-                }
-                if (!cons->is_valid()) {
-                    fprintf(stderr, " = =  = INVALID CONSUMER\n\n");
-                    return 1;
-                }
-                cons->set("terminate_on_pause", 1);
-                cons->connect(*playlst);
-                playlst.reset();
-                cons->run();
-                cons->stop();
-                cons->purge();
-                fprintf(stderr, "DONE:%d \n", frame.toInt());
-            }
-            // Mlt::Factory::close();
-            fprintf(stderr, "+ + + RENDERING FINISHED + + + \n");
-            return 0;
-        }
-        if (args.count() > 1 && args.at(0) == QLatin1String("-subtitle")) {
-            args.removeFirst();
-            subtitleFile = args.takeFirst();
-        }
+        QString subtitleFile = parser.value(subtitleOption);
 
         // older MLT version, does not support embedded consumer in/out in xml, and current
         // MLT (6.16) does not pass it onto the multi / movit consumer, so read it manually and enforce
@@ -160,31 +211,18 @@ int main(int argc, char **argv)
                 playlist.append(QStringLiteral("?multi=1"));
             }
         }
+        int in = -1;
         auto *rJob = new RenderJob(render, playlist, target, pid, in, out, subtitleFile, &app);
         QObject::connect(rJob, &RenderJob::renderingFinished, rJob, [&]() {
             rJob->deleteLater();
             app.quit();
         });
-        app.setQuitOnLastWindowClosed(false);
+        // app.setQuitOnLastWindowClosed(false);
         QMetaObject::invokeMethod(rJob, "start", Qt::QueuedConnection);
         return app.exec();
-    } else {
-        fprintf(stderr,
-                "Kdenlive video renderer for MLT.\nUsage: "
-                "kdenlive_render [-erase] [-kuiserver] [-locale:LOCALE] [in=pos] [out=pos] [render] [profile] [rendermodule] [player] [src] [dest] [[arg1] "
-                "[arg2] ...]\n"
-                "  -erase: if that parameter is present, src file will be erased at the end\n"
-                "  -kuiserver: if that parameter is present, use KDE job tracker\n"
-                "  -locale:LOCALE : set a locale for rendering. For example, -locale:fr_FR.UTF-8 will use a french locale (comma as numeric separator)\n"
-                "  in=pos: start rendering at frame pos\n"
-                "  out=pos: end rendering at frame pos\n"
-                "  render: path to MLT melt renderer\n"
-                "  profile: the MLT video profile\n"
-                "  rendermodule: the MLT consumer used for rendering, usually it is avformat\n"
-                "  player: path to video player to play when rendering is over, use '-' to disable playing\n"
-                "  src: source file (usually MLT XML)\n"
-                "  dest: destination file\n"
-                "  args: space separated libavformat arguments\n");
-        return 1;
     }
+
+    qCritical() << "Error: unknown mode" << mode << "\n";
+    parser.showHelp(1);
+    // the command above will quit the app with return 1;
 }
