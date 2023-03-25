b1c8ad1e3ba7f5ce0cff588492bb4e432413f370
Jean-Baptiste Mardelle <jb@kdenlive.org>
2023-03-25 16:21:01 +0100

    Fix tests

--- src/timeline2/model/compositionmodel.hpp
+++ src/timeline2/model/compositionmodel.hpp
@@ -37,7 +37,7 @@ public:
        @param transitionId is the id of the transition to be inserted
        @param id Requested id of the clip. Automatic if -1
     */
-    static int construct(const std::weak_ptr<TimelineModel> &parent, const QString &transitionId, const QString &originalDecimalPoint, int length = 0,
+    static int construct(const std::weak_ptr<TimelineModel> &parent, const QString &transitionId, const QString &originalDecimalPoint, int length = 1,
                          int id = -1, std::unique_ptr<Mlt::Properties> sourceProperties = nullptr);
 
     friend class TrackModel;
