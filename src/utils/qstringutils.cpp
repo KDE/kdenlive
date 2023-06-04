b65eb05c46a3c06dae5c8872fc5c2fa999f53671
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-06-04 23:25:48 +0200

    Add QStringUtils with common actions around strings
    Reduces code duplication, increases readability and cover these actions
    by tests.

---
+++ src/utils/qstringutils.cpp
@@ -0,0 +1,28 @@
+/*
+    SPDX-FileCopyrightText: 2023 Julius Künzel <jk.kdedev@smartlab.uber.space>
+    SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+*/
+
+#include "qstringutils.h"
+
+#include <QString>
+#include <QStringList>
+
+QString QStringUtils::getUniqueName(const QStringList &names, const QString &name)
+{
+    int i = 0;
+    QString newName = name;
+    while (names.contains(newName)) {
+        // name is not unique, add a suffix
+        newName = name + QString("-%1").arg(i);
+        i++;
+    }
+    return newName;
+}
+
+QString QStringUtils::appendToFilename(const QString &filename, const QString &appendix)
+{
+    QString name = filename.section(QLatin1Char('.'), 0, -2);
+    QString extension = filename.section(QLatin1Char('.'), -1);
+    return name + appendix + QLatin1Char('.') + extension;
+}
---
+++ src/utils/qstringutils.h
@@ -0,0 +1,24 @@
+/*
+    SPDX-FileCopyrightText: 2023 Julius Künzel <jk.kdedev@smartlab.uber.space>
+    SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+*/
+
+#pragma once
+
+class QString;
+class QStringList;
+
+class QStringUtils
+{
+public:
+    /** @returns a name based on @param name that is unique in @param names.
+     *  This is achived by adding a number as suffix to the @param name in case name
+     *  does already exist in @param names.
+     */
+    static QString getUniqueName(const QStringList &names, const QString &name);
+    /** Append @param appendix to @param filename before the file extension.
+     *  The part of the string after the last dot will be treated as file extension.
+     *  Hence it does not work with extensions like .tar.gz
+     */
+    static QString appendToFilename(const QString &filename, const QString &appendix);
+};
