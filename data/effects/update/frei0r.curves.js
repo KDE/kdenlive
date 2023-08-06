0e5e5882fca242295d97d678747c216e12e368af
Camille Moulin <camille.moulin@free.fr>
2023-08-06 12:58:37 +0200

    chore(REUSE): Add headers in data/effects/update

--- data/effects/update/frei0r.curves.js
+++ data/effects/update/frei0r.curves.js
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: Till Theato <root@ttill.de>, Jean-Baptiste Mardelle <jb@kdenlive.org>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 
 var update = new Object();
 
