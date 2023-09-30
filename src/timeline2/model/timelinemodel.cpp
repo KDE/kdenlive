54ed2e8acfe85d035efab1585afcce9f666f308a
Jean-Baptiste Mardelle <jb@kdenlive.org>
2023-09-30 13:45:46 +0200

    Fix another case blocking separate track move

--- src/timeline2/model/timelinemodel.cpp
+++ src/timeline2/model/timelinemodel.cpp
@@ -2558,6 +2558,9 @@ bool TimelineModel::requestGroupMove(int itemId, int groupId, int delta_track, i
             int newItemTrackId = getTrackIndexFromPosition(newTrackPosition);
             int newIn = item.second + delta_pos;
             if (!getTrackById_const(newItemTrackId)->isAvailableWithExceptions(newIn, getClipPlaytime(item.first) - 1, sorted_clips_ids)) {
+                if (!moveMirrorTracks && item.first != itemId) {
+                    continue;
+                }
                 delta_track = 0;
                 break;
             }
