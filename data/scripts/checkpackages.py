5cfb3a17a9fc879e2bf5aa5b297e459e6613075f
Jean-Baptiste Mardelle <jb@kdenlive.org>
2024-02-05 13:16:43 +0100

    Speech to text : switch to importlib instead of deprecated pkg_resources

--- data/scripts/checkpackages.py
+++ data/scripts/checkpackages.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-# SPDX-FileCopyrightText: 2021 Jean-Baptiste Mardelle <jb@kdenlive.org>
+# SPDX-FileCopyrightText: 2024 Jean-Baptiste Mardelle <jb@kdenlive.org>
 # SPDX-FileCopyrightText: 2022 Julius Künzel <jk.kdedev@smartlab.uber.space>
 # SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 
 import sys
 import subprocess
-import pkg_resources
-
+import importlib.util
 
 def print_help():
     print("""
@@ -31,6 +30,7 @@ if '--help' in sys.argv:
     sys.exit()
 
 required = set()
+missing = set()
 
 for arg in sys.argv[1:]:
     if not arg.startswith("--"):
@@ -40,8 +40,10 @@ if len(required) == 0:
     print_help()
     sys.exit("Error: You need to provide at least one package name")
 
-installed = {pkg.key for pkg in pkg_resources.working_set}
-missing = required - installed
+for package_name in required:
+    if importlib.util.find_spec(package_name) is None:
+        missing.add(package_name)
+
 if '--check' in sys.argv:
     if len(missing) > 0:
         print("Missing pachages: ", missing)
