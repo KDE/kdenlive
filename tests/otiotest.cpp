7f88bc3bd662ba68c9456f2cce20c288fede2611
Darby Johnston <darbyjohnston@yahoo.com>
2025-02-18 10:30:14 -0800

    Remove old code
    Signed-off-by: Darby Johnston <darbyjohnston@yahoo.com>

--- tests/otiotest.cpp
+++ tests/otiotest.cpp
@@ -33,29 +33,6 @@ TEST_CASE("Export/import tracks and clips", "[OTIO]")
         auto timeline = document.getTimeline(document.uuid());
         pCore->projectManager()->testSetActiveTimeline(timeline);
 
-        // Create an OTIO timeline.
-        /*OTIO_NS::SerializableObject::Retainer<OTIO_NS::Timeline> otioTimeline(
-            new OTIO_NS::Timeline);
-        OTIO_NS::SerializableObject::Retainer<OTIO_NS::Track> otioTrack(
-            new OTIO_NS::Track);
-        otioTimeline->tracks()->append_child(otioTrack);
-        QString path = sourcesPath + "/dataset/red.mp4";
-        OTIO_NS::SerializableObject::Retainer<OTIO_NS::ExternalReference> otioMediaReference(
-            new OTIO_NS::ExternalReference(path.toStdString()));
-        OTIO_NS::TimeRange sourceRange(
-            OTIO_NS::RationalTime(0.0, 25.0),
-            OTIO_NS::RationalTime(5.0, 25.0));
-        OTIO_NS::SerializableObject::Retainer<OTIO_NS::Clip> otioClip(
-            new OTIO_NS::Clip(
-                "red",
-                otioMediaReference,
-                sourceRange));
-        otioTrack->append_child(otioClip);
-        // TODO: Replace with QTemporaryDir.
-        QDir dir = QDir::temp();
-        QString otioImportFileName = dir.filePath("import_single_clip.otio");
-        REQUIRE(otioTimeline->to_json_file(otioImportFileName.toStdString()));*/
-
         // Import the OTIO timeline.
         OtioImport otioImport;
         QString path = sourcesPath + "/dataset/test-clips.otio";
