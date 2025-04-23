244feed0441f3b17e9988c4d8e940c48340dbca5
Jean-Baptiste Mardelle <jb@kdenlive.org>
2025-04-23 13:15:01 +0200

    Fix Titles cannot be created on first run

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -144,7 +144,7 @@ MainWindow::MainWindow(QWidget *parent)
     kdenliveCategoryMap.insert(QStringLiteral("bintags"), category);
 }
 
-void MainWindow::init(const QString &mltPath)
+void MainWindow::init()
 {
     QString desktopStyle = QApplication::style()->objectName();
     // Load themes
@@ -156,8 +156,6 @@ void MainWindow::init(const QString &mltPath)
     new RenderServer(this);
     QString defaultProfile = KdenliveSettings::default_profile();
 
-    // Initialise MLT connection
-    MltConnection::construct(mltPath);
     pCore->setCurrentProfile(defaultProfile.isEmpty() ? ProjectManager::getDefaultProjectFormat() : defaultProfile);
     m_commandStack = new QUndoGroup();
 
