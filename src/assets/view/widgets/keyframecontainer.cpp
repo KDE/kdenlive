KDE/kdenlive

commit b8ffac30eaf563a04873d085d975083f54cc3b69

Transform effect: add rotation capabilities to monitor and effect stack tooling
Implements https://invent.kde.org/multimedia/kdenlive/-/issues/2006

src/assets/view/widgets/keyframecontainer.cpp
@@ -371,7 +371,6 @@ KeyframeContainer::KeyframeContainer(std::shared_ptr<AssetParameterModel> model,
     m_editorviewcontainer->setFixedHeight(m_editorviewcontainer->currentWidget()->height());
     m_baseHeight = m_editorviewcontainer->height() + m_toolbar->sizeHint().height();
     m_addedHeight = mrg.top() + mrg.bottom();
-    addParameter(index);
     m_fixedHeight = m_baseHeight + m_addedHeight;
     Q_EMIT updateHeight();
 }
@@ -429,10 +428,15 @@ void KeyframeContainer::slotRefreshParams()
         }
         i++;
     }
+    bool hasRotation = false;
     for (const auto &w : m_parameters) {
         auto type = m_model->data(w.first, AssetParameterModel::TypeRole).value<ParamType>();
+        QString name = m_model->data(w.first, AssetParameterModel::NameRole).toString();
         if (type == ParamType::KeyframeParam) {
             (static_cast<DoubleWidget *>(w.second))->setValue(m_keyframes->getInterpolatedValue(pos, w.first).toDouble());
+            if (name == QLatin1String("rotation")) {
+                hasRotation = true;
+            }
         } else if (type == ParamType::AnimatedRect) {
             const QString val = m_keyframes->getInterpolatedValue(pos, w.first).toString();
             const QStringList vals = val.split(QLatin1Char(' '));
@@ -456,7 +460,7 @@ void KeyframeContainer::slotRefreshParams()
             (static_cast<ChooseColorWidget *>(w.second)->slotColorModified(QColorUtils::stringToColor(value)));
         }
     }
-    if (m_monitorHelper && m_model->isActive() && m_curveeditorcontainer->isEnabled()) {
+    if (m_monitorHelper && m_model->isActive() && (m_curveeditorcontainer->isEnabled() || hasRotation)) {
         m_monitorHelper->refreshParams(pos);
     }
 }
@@ -572,6 +576,43 @@ void KeyframeContainer::resetKeyframes()
     slotRefreshParams();
 }
 
+void KeyframeContainer::initNeededSceneAndHelper()
+{
+    // Loop over all parameters to determine the needed scene and helper
+    m_monitorHelper = nullptr;
+    m_neededScene = MonitorSceneType::MonitorSceneDefault;
+    for (int i = 0; i < m_model->rowCount(); ++i) {
+        QModelIndex index = m_model->index(i, 0);
+        auto type = m_model->data(index, AssetParameterModel::TypeRole).value<ParamType>();
+        QString paramName = m_model->data(index, AssetParameterModel::NameRole).toString();
+        QString assetId = m_model->getAssetId();
+        if (assetId == QLatin1String("qtblend")) {
+            m_neededScene = MonitorSceneType::MonitorSceneRotatedGeometry;
+            m_monitorHelper = new KeyframeMonitorHelper(pCore->getMonitor(m_model->monitorId), m_model, m_neededScene, m_parent);
+            break;
+        } else if (type == ParamType::Roto_spline) {
+            m_neededScene = MonitorSceneType::MonitorSceneRoto;
+            m_monitorHelper = new RotoHelper(pCore->getMonitor(m_model->monitorId), m_model, m_parent);
+            break;
+        } else if (assetId == QLatin1String("frei0r.c0rners")) {
+            m_neededScene = MonitorSceneType::MonitorSceneCorners;
+            m_monitorHelper = new CornersHelper(pCore->getMonitor(m_model->monitorId), m_model, m_parent);
+            break;
+        } else if (assetId.contains(QLatin1String("frei0r.alphaspot"))) {
+            m_neededScene = MonitorSceneType::MonitorSceneGeometry;
+            m_monitorHelper = new RectHelper(pCore->getMonitor(m_model->monitorId), m_model, m_parent);
+            break;
+        } else if (type == ParamType::AnimatedRect) {
+            m_neededScene = MonitorSceneType::MonitorSceneGeometry;
+            m_monitorHelper = new KeyframeMonitorHelper(pCore->getMonitor(m_model->monitorId), m_model, m_neededScene, m_parent);
+            break;
+        }
+    }
+    if (m_monitorHelper) {
+        connect(this, &KeyframeContainer::addIndex, m_monitorHelper, &KeyframeMonitorHelper::addIndex);
+    }
+}
+
 void KeyframeContainer::addParameter(const QPersistentModelIndex &index)
 {
     // Retrieve parameters from the model
@@ -597,12 +638,11 @@ void KeyframeContainer::addParameter(const QPersistentModelIndex &index)
     // Construct object
     QLabel *labelWidget = nullptr;
     QWidget *paramWidget = nullptr;
+    QString paramName = m_model->data(index, AssetParameterModel::NameRole).toString();
     if (type == ParamType::AnimatedRect) {
-        m_neededScene = MonitorSceneType::MonitorSceneGeometry;
         int inPos = m_model->data(index, AssetParameterModel::ParentInRole).toInt();
         QPair<int, int> range(inPos, inPos + m_model->data(index, AssetParameterModel::ParentDurationRole).toInt());
         const QString value = m_keyframes->getInterpolatedValue(getPosition(), index).toString();
-        m_monitorHelper = new KeyframeMonitorHelper(pCore->getMonitor(m_model->monitorId), m_model, index, MonitorSceneGeometry, m_parent);
         QRect rect;
         double opacity = 0;
         QStringList vals = value.split(QLatin1Char(' '));
@@ -612,9 +652,13 @@ void KeyframeContainer::addParameter(const QPersistentModelIndex &index)
                 opacity = vals.at(4).toDouble();
             }
         }
+        Q_EMIT addIndex(index);
         // qtblend uses an opacity value in the (0-1) range, while older geometry effects use (0-100)
         m_geom.reset(new GeometryWidget(pCore->getMonitor(m_model->monitorId), range, rect, opacity, m_sourceFrameSize, false,
                                         m_model->data(m_index, AssetParameterModel::OpacityRole).toBool(), m_parent, m_layout));
+        if (m_neededScene == MonitorSceneType::MonitorSceneRotatedGeometry) {
+            m_geom->setRotatable(true);
+        }
         connect(m_geom.get(), &GeometryWidget::valueChanged, this, [this, index](const QString &v, int ix, int frame) {
             Q_EMIT activateEffect();
             m_keyframes->updateKeyframe(GenTime(frame, pCore->getCurrentFps()), QVariant(v), ix, index);
@@ -624,6 +668,7 @@ void KeyframeContainer::addParameter(const QPersistentModelIndex &index)
                 pCore->getMonitor(m_model->monitorId)->setUpEffectGeometry(r);
             }
         });
+
     } else if (type == ParamType::ColorWheel) {
         auto colorWheelWidget = new LumaLiftGainParam(m_model, index, m_parent);
         connect(colorWheelWidget, &LumaLiftGainParam::valuesChanged, this,
@@ -645,8 +690,7 @@ void KeyframeContainer::addParameter(const QPersistentModelIndex &index)
         });
         paramWidget = colorWheelWidget;
     } else if (type == ParamType::Roto_spline) {
-        m_monitorHelper = new RotoHelper(pCore->getMonitor(m_model->monitorId), m_model, index, m_parent);
-        m_neededScene = MonitorSceneType::MonitorSceneRoto;
+        Q_EMIT addIndex(index);
     } else if (type == ParamType::Color) {
         QString value = m_keyframes->getInterpolatedValue(getPosition(), index).toString();
         bool alphaEnabled = m_model->data(index, AssetParameterModel::AlphaRole).toBool();
@@ -661,34 +705,30 @@ void KeyframeContainer::addParameter(const QPersistentModelIndex &index)
 
     } else {
         if (m_model->getAssetId() == QLatin1String("frei0r.c0rners")) {
-            if (m_neededScene == MonitorSceneDefault && !m_monitorHelper) {
-                m_neededScene = MonitorSceneType::MonitorSceneCorners;
-                m_monitorHelper = new CornersHelper(pCore->getMonitor(m_model->monitorId), m_model, index, m_parent);
-                connect(this, &KeyframeContainer::addIndex, m_monitorHelper, &CornersHelper::addIndex);
-            } else {
-                if (type == ParamType::KeyframeParam) {
-                    int paramName = m_model->data(index, AssetParameterModel::NameRole).toInt();
-                    if (paramName < 8) {
-                        Q_EMIT addIndex(index);
-                    }
+            if (type == ParamType::KeyframeParam) {
+                // We're only interested in the first 8 parameters of the corners effect representing x/y coordinates of the corners.
+                // frei0r.c0rners named these params as integers, starting from 0 so we can convert then name to an int and do a int comparison.
+                int paramNameAsInt = paramName.toInt();
+                if (paramNameAsInt < 8) {
+                    Q_EMIT addIndex(index);
                 }
             }
         }
         if (m_model->getAssetId().contains(QLatin1String("frei0r.alphaspot"))) {
-            if (m_neededScene == MonitorSceneDefault && !m_monitorHelper) {
-                m_neededScene = MonitorSceneType::MonitorSceneGeometry;
-                m_monitorHelper = new RectHelper(pCore->getMonitor(m_model->monitorId), m_model, index, m_parent);
-                connect(this, &KeyframeContainer::addIndex, m_monitorHelper, &RectHelper::addIndex);
-            } else {
-                if (type == ParamType::KeyframeParam) {
-                    QString paramName = m_model->data(index, AssetParameterModel::NameRole).toString();
-                    if (paramName.contains(QLatin1String("Position X")) || paramName.contains(QLatin1String("Position Y")) ||
-                        paramName.contains(QLatin1String("Size X")) || paramName.contains(QLatin1String("Size Y"))) {
-                        Q_EMIT addIndex(index);
-                    }
+
+            if (type == ParamType::KeyframeParam) {
+                if (paramName.contains(QLatin1String("Position X")) || paramName.contains(QLatin1String("Position Y")) ||
+                    paramName.contains(QLatin1String("Size X")) || paramName.contains(QLatin1String("Size Y"))) {
+                    Q_EMIT addIndex(index);
                 }
             }
         }
+        if (m_model->getAssetId() == QLatin1String("qtblend")) {
+            if (paramName == QLatin1String("rotation")) {
+                Q_EMIT addIndex(index);
+            }
+        }
+
         double value = m_keyframes->getInterpolatedValue(getPosition(), index).toDouble();
         double min = m_model->data(index, AssetParameterModel::MinRole).toDouble();
         double max = m_model->data(index, AssetParameterModel::MaxRole).toDouble();
@@ -703,6 +743,13 @@ void KeyframeContainer::addParameter(const QPersistentModelIndex &index)
             Q_EMIT activateEffect();
             m_keyframes->updateKeyframe(GenTime(getPosition(), pCore->getCurrentFps()), QVariant(v), -1, index);
         });
+        if (m_geom) {
+            connect(doubleWidget, &DoubleWidget::valueChanged, this, [this](double v) {
+                if (m_geom) {
+                    m_geom->slotUpdateRotation(v);
+                }
+            });
+        }
         doubleWidget->setDragObjectName(QString::number(index.row()));
         paramWidget = doubleWidget;
         labelWidget = doubleWidget->createLabel();

src/CMakeLists.txt
@@ -158,6 +158,7 @@ ecm_target_qml_sources(kdenliveLib
     monitor/view/OverlayCenter.qml
     monitor/view/OverlayCenterDiagonal.qml
     monitor/view/OverlayThirds.qml
+    monitor/view/OverlayLabel.qml
     timeline2/view/qml/Timeline.qml
     timeline2/view/qml/TimelineLogic.js
     timeline2/view/qml/TrackHead.qml

src/assets/keyframes/model/corners/cornershelper.cpp
@@ -14,8 +14,8 @@ SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 
 #include <QSize>
 #include <utility>
-CornersHelper::CornersHelper(Monitor *monitor, std::shared_ptr<AssetParameterModel> model, QPersistentModelIndex index, QObject *parent)
-    : KeyframeMonitorHelper(monitor, std::move(model), std::move(index), MonitorSceneCorners, parent)
+CornersHelper::CornersHelper(Monitor *monitor, std::shared_ptr<AssetParameterModel> model, QObject *parent)
+    : KeyframeMonitorHelper(monitor, std::move(model), MonitorSceneCorners, parent)
 {
 }
 

src/assets/keyframes/model/corners/cornershelper.hpp
@@ -28,7 +28,7 @@ public:
        @param model is the asset this parameter belong to
        @param index is the index of this parameter in its model
      */
-    explicit CornersHelper(Monitor *monitor, std::shared_ptr<AssetParameterModel> model, QPersistentModelIndex index, QObject *parent = nullptr);
+    explicit CornersHelper(Monitor *monitor, std::shared_ptr<AssetParameterModel> model, QObject *parent = nullptr);
     /** @brief Send data update to the monitor
      */
     void refreshParams(int pos) override;

src/assets/keyframes/model/keyframemonitorhelper.cpp
@@ -12,15 +12,14 @@ SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 
 #include <core.h>
 #include <utility>
-KeyframeMonitorHelper::KeyframeMonitorHelper(Monitor *monitor, std::shared_ptr<AssetParameterModel> model, const QPersistentModelIndex &index,
-                                             MonitorSceneType sceneType, QObject *parent)
+KeyframeMonitorHelper::KeyframeMonitorHelper(Monitor *monitor, std::shared_ptr<AssetParameterModel> model, MonitorSceneType sceneType, QObject *parent)
     : QObject(parent)
     , m_monitor(monitor)
     , m_model(std::move(model))
     , m_active(false)
     , m_requestedSceneType(sceneType)
 {
-    m_indexes << index;
+    m_rotatable = sceneType == MonitorSceneType::MonitorSceneRotatedGeometry;
 }
 
 bool KeyframeMonitorHelper::connectMonitor(bool activate)
@@ -31,9 +30,11 @@ bool KeyframeMonitorHelper::connectMonitor(bool activate)
     m_active = activate;
     if (activate) {
         connect(m_monitor, &Monitor::effectPointsChanged, this, &KeyframeMonitorHelper::slotUpdateFromMonitorData, Qt::UniqueConnection);
+        connect(m_monitor, &Monitor::effectRotationChanged, this, &KeyframeMonitorHelper::slotUpdateRotationFromMonitorData, Qt::UniqueConnection);
     } else {
         m_monitor->setEffectKeyframe(false, true);
         disconnect(m_monitor, &Monitor::effectPointsChanged, this, &KeyframeMonitorHelper::slotUpdateFromMonitorData);
+        disconnect(m_monitor, &Monitor::effectRotationChanged, this, &KeyframeMonitorHelper::slotUpdateRotationFromMonitorData);
     }
     return m_active;
 }
@@ -45,6 +46,7 @@ bool KeyframeMonitorHelper::isPlaying() const
 
 void KeyframeMonitorHelper::addIndex(const QPersistentModelIndex &index)
 {
+    QString name = m_model->data(index, AssetParameterModel::NameRole).toString();
     m_indexes << index;
 }
 
@@ -75,33 +77,41 @@ void KeyframeMonitorHelper::refreshParams(int pos)
     QVariantList points;
     QVariantList types;
     QString rectAtPosData;
+    double rotation = 0;
     std::shared_ptr<KeyframeModelList> keyframes = m_model->getKeyframeModel();
     for (const auto &ix : std::as_const(m_indexes)) {
         auto type = m_model->data(ix, AssetParameterModel::TypeRole).value<ParamType>();
-        if (type != ParamType::AnimatedRect) {
+        QString name = m_model->data(ix, AssetParameterModel::NameRole).toString();
+        if (!(type == ParamType::AnimatedRect || name == QLatin1String("rotation"))) {
             continue;
         }
-        KeyframeModel *kfr = keyframes->getKeyModel(ix);
-        bool ok;
-        rectAtPosData = kfr->getInterpolatedValue(pos).toString();
-        Keyframe kf = kfr->getNextKeyframe(GenTime(-1), &ok);
-        while (ok) {
-            if (kf.second == KeyframeType::Curve) {
-                types << 1;
-            } else {
-                types << 0;
+        if (name == QLatin1String("rotation")) {
+            rotation = keyframes->getInterpolatedValue(pos, ix).toDouble();
+        } else {
+            KeyframeModel *kfr = keyframes->getKeyModel(ix);
+            bool ok;
+            rectAtPosData = kfr->getInterpolatedValue(pos).toString();
+            Keyframe kf = kfr->getNextKeyframe(GenTime(-1), &ok);
+            while (ok) {
+                if (kf.second == KeyframeType::Curve) {
+                    types << 1;
+                } else {
+                    types << 0;
+                }
+                QString rectData = kfr->getInterpolatedValue(kf.first).toString();
+                QStringList data = rectData.split(QLatin1Char(' '));
+                if (data.size() > 3) {
+                    QRectF r(data.at(0).toInt(), data.at(1).toInt(), data.at(2).toInt(), data.at(3).toInt());
+                    points.append(QVariant(r.center()));
+                }
+                kf = kfr->getNextKeyframe(kf.first, &ok);
             }
-            QString rectData = kfr->getInterpolatedValue(kf.first).toString();
-            QStringList data = rectData.split(QLatin1Char(' '));
-            if (data.size() > 3) {
-                QRectF r(data.at(0).toInt(), data.at(1).toInt(), data.at(2).toInt(), data.at(3).toInt());
-                points.append(QVariant(r.center()));
-            }
-            kf = kfr->getNextKeyframe(kf.first, &ok);
         }
-        break;
     }
     if (m_monitor) {
+        if (m_rotatable) {
+            m_monitor->setEffectSceneProperty(QStringLiteral("rotation"), rotation);
+        }
         if (!rectAtPosData.isEmpty()) {
             QStringList data = rectAtPosData.split(QLatin1Char(' '));
             if (data.size() > 3) {
@@ -152,5 +162,19 @@ void KeyframeMonitorHelper::slotUpdateFromMonitorData(const QVariantList &center
             i++;
         }
         break;
+    }
+}
+
+void KeyframeMonitorHelper::slotUpdateRotationFromMonitorData(double rotation)
+{
+    if (!m_indexes.isEmpty()) {
+        for (const auto &ix : std::as_const(m_indexes)) {
+            QString paramName = m_model->data(ix, AssetParameterModel::NameRole).toString();
+            if (paramName == QLatin1String("rotation")) {
+                m_model->setParameter(paramName, QString::number(rotation), true, ix);
+                Q_EMIT updateKeyframeData(ix, QVariant(rotation));
+                break;
+            }
+        }
     }
 }

src/assets/keyframes/model/keyframemonitorhelper.hpp
@@ -27,13 +27,19 @@ class KeyframeMonitorHelper : public QObject
     Q_OBJECT
 
 public:
-    /** @brief Construct a keyframe list bound to the given effect
-       @param init_value is the value taken by the param at time 0.
-       @param model is the asset this parameter belong to
-       @param index is the index of this parameter in its model
+    /**
+     * @brief Construct a keyframe monitor helper for the given effect/asset.
+     *
+     * The helper is initialized with an empty list of managed parameter indexes.
+     * You must call addIndex() for every parameter that should be managed by this helper.
+     * This allows the same helper instance to manage one or multiple parameters as needed.
+     *
+     * @param monitor The Monitor instance this helper will interact with (for effect overlays, etc).
+     * @param model The asset parameter model this helper will manage keyframes for.
+     * @param sceneType The type of monitor scene this helper is associated with (geometry, corners, etc).
+     * @param parent Optional QObject parent.
      */
-    explicit KeyframeMonitorHelper(Monitor *monitor, std::shared_ptr<AssetParameterModel> model, const QPersistentModelIndex &index, MonitorSceneType sceneType,
-                                   QObject *parent = nullptr);
+    explicit KeyframeMonitorHelper(Monitor *monitor, std::shared_ptr<AssetParameterModel> model, MonitorSceneType sceneType, QObject *parent = nullptr);
     /** @brief Send signals to the monitor to update the qml overlay.
        @param returns : true if the monitor's connection was changed to active.
     */
@@ -59,12 +65,21 @@ protected:
     QList<QPersistentModelIndex> m_indexes;
     bool m_active;
     MonitorSceneType m_requestedSceneType{MonitorSceneNone};
+    /** @brief Whether this helper manages rotation parameters (for now only used for Transform effect based on qtblend)
+     */
+    bool m_rotatable{false};
 
 private Q_SLOTS:
     virtual void slotUpdateFromMonitorData(const QVariantList &v);
+    virtual void slotUpdateRotationFromMonitorData(double rotation);
 
 public Q_SLOTS:
-    /** @brief For classes that manage several parameters, add a param index to the list
+    /**
+     * @brief Register a parameter index to be managed by this helper.
+     *
+     * This should be called for every parameter that needs monitor interaction and keyframe management.
+     * The index is added to the internal list of managed parameters, enabling the helper to update and synchronize
+     * keyframe data between the monitor overlay and the parameter model.
      */
     void addIndex(const QPersistentModelIndex &index);
 

src/assets/keyframes/model/rect/recthelper.cpp
@@ -14,8 +14,8 @@ SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 
 #include <QSize>
 #include <utility>
-RectHelper::RectHelper(Monitor *monitor, std::shared_ptr<AssetParameterModel> model, QPersistentModelIndex index, QObject *parent)
-    : KeyframeMonitorHelper(monitor, std::move(model), std::move(index), MonitorSceneGeometry, parent)
+RectHelper::RectHelper(Monitor *monitor, std::shared_ptr<AssetParameterModel> model, QObject *parent)
+    : KeyframeMonitorHelper(monitor, std::move(model), MonitorSceneGeometry, parent)
 {
 }
 

src/assets/keyframes/model/rect/recthelper.hpp
@@ -28,7 +28,7 @@ public:
        @param model is the asset this parameter belong to
        @param index is the index of this parameter in its model
      */
-    explicit RectHelper(Monitor *monitor, std::shared_ptr<AssetParameterModel> model, QPersistentModelIndex index, QObject *parent = nullptr);
+    explicit RectHelper(Monitor *monitor, std::shared_ptr<AssetParameterModel> model, QObject *parent = nullptr);
     /** @brief Send data update to the monitor
      */
     /** @brief Send signals to the monitor to update the qml overlay.

commit 3559ef34a28a663344beba51c5923053e4880065

Don't attempt to update geometry scene when its not actually loaded
AFAIK it did not have any negative effects except spamming the logs with
```
QMetaObject::invokeMethod: No such method ClipMonitor_QMLTYPE_80::updatePoints(QVariant,QVariant)
```

This became apparent when using built-in effects which adds a disabled transform effect to all clips.

This bug was introduced in b8ffac30 but I don't remember or understand why I wanted to force this update even if the effect is disabled... This commit reverts this change and removes the hasRotation condition.

src/assets/view/widgets/keyframecontainer.cpp
@@ -432,15 +432,10 @@ void KeyframeContainer::slotRefreshParams()
         }
         i++;
     }
-    bool hasRotation = false;
     for (const auto &w : m_parameters) {
         auto type = m_model->data(w.first, AssetParameterModel::TypeRole).value<ParamType>();
-        QString name = m_model->data(w.first, AssetParameterModel::NameRole).toString();
         if (type == ParamType::KeyframeParam) {
             (static_cast<DoubleWidget *>(w.second))->setValue(m_keyframes->getInterpolatedValue(pos, w.first).toDouble());
-            if (name == QLatin1String("rotation")) {
-                hasRotation = true;
-            }
         } else if (type == ParamType::AnimatedRect) {
             const QString val = m_keyframes->getInterpolatedValue(pos, w.first).toString();
             const QStringList vals = val.split(QLatin1Char(' '));
@@ -464,7 +459,7 @@ void KeyframeContainer::slotRefreshParams()
             (static_cast<ChooseColorWidget *>(w.second)->slotColorModified(QColorUtils::stringToColor(value)));
         }
     }
-    if (m_monitorHelper && m_model->isActive() && (m_curveeditorcontainer->isEnabled() || hasRotation)) {
+    if (m_monitorHelper && m_model->isActive() && m_curveeditorcontainer->isEnabled()) {
         m_monitorHelper->refreshParams(pos);
     }
 }