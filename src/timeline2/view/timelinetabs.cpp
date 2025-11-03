64d2b3a82cc3156f438fa64021d5492d9985232e
Jean-Baptiste Mardelle <jb@kdenlive.org>
2025-11-03 18:55:21 +0100

    FIx possible crash using newer ui.rc file

--- src/timeline2/view/timelinetabs.cpp
+++ src/timeline2/view/timelinetabs.cpp
@@ -367,7 +367,9 @@ void TimelineTabs::buildClipMenu()
     m_timelineClipMenu->addAction(coll->action(QStringLiteral("send_sequence")));
 
     QMenu *markerMenu = static_cast<QMenu *>(pCore->window()->factory()->container(QStringLiteral("marker_menu"), pCore->window()));
-    m_timelineClipMenu->addMenu(markerMenu);
+    if (markerMenu) {
+        m_timelineClipMenu->addMenu(markerMenu);
+    }
     QMenu *alignMenu = new QMenu(i18n("Align to Reference"), this);
     m_timelineClipMenu->addMenu(alignMenu);
     alignMenu->addAction(coll->action(QStringLiteral("set_audio_align_ref")));
