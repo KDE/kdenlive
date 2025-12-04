3db8396f9b3da483b292230773752480bcba8ac3
Jean-Baptiste Mardelle <jb@kdenlive.org>
2025-12-04 10:41:46 +0100

    Extract json strings for 25.12 too

--- Messages.sh
+++ Messages.sh
@@ -4,6 +4,20 @@
 
 kdenlive_subdirs="plugins renderer data src src/ui"
 
+# Extract displayed strings from JSON layouts
+for file in data/layouts/*.json; do
+    if [ -f "$file" ]; then
+        name_string=$(jq '.kdenliveInfo[] | .displayName' $file)
+        # Use the same syntax as extractrc
+        if [ $name_string == "null" ]; then
+            echo "Layout without name: $file"
+        else
+            echo "// i18n: file: $file" >> rc.cpp
+            echo "i18n($name_string);" >> rc.cpp
+        fi
+    fi
+done
+
 $EXTRACTRC --strip-cdata --tag=name --tag=description --tag=label --tag=comment --tag=paramlistdisplay data/transitions/*.xml data/transitions/frei0r/*.xml data/effects/*.xml data/effects/frei0r/*.xml data/effects/avfilter/*.xml data/effects/ladspa/*.xml data/effects/sox/*.xml data/generators/*.xml data/kdenliveeffectscategory.rc >> rc.cpp
 $EXTRACTRC `find $kdenlive_subdirs -name \*.rc -a ! -name encodingprofiles.rc -a ! -name camcorderfilters.rc -a ! -name externalproxies.rc -o -name \*.ui` >> rc.cpp
 
