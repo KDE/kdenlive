f38033aa10eff54a3e2a1782c10edc6ae4955e25
Julius Künzel <julius.kuenzel@kde.org>
2026-01-02 12:47:45 +0100

    Minor refactoring for monitor fullscreen code

--- src/monitor/monitor.cpp
+++ src/monitor/monitor.cpp
@@ -1101,97 +1101,96 @@ bool Monitor::monitorIsFullScreen() const
     return m_glWidget->isFullScreen();
 }
 
+const QScreen *Monitor::getScreenForFullscreen()
+{
+    if (qApp->screens().count() == 1) {
+        return qApp->screens().first();
+    }
+    QString requestedMonitor = KdenliveSettings::fullscreen_monitor();
+    if (m_id == Kdenlive::ProjectMonitor) {
+        if (!KdenliveSettings::project_monitor_fullscreen().isEmpty()) {
+            requestedMonitor = KdenliveSettings::project_monitor_fullscreen();
+        }
+    } else {
+        if (!KdenliveSettings::clip_monitor_fullscreen().isEmpty()) {
+            requestedMonitor = KdenliveSettings::clip_monitor_fullscreen();
+        }
+    }
+    int ix = -1;
+    if (!requestedMonitor.isEmpty()) {
+        // If the platform does not provide screen serial number, use indexes
+        for (const QScreen *screen : qApp->screens()) {
+            ix++;
+            QString screenId = QStringLiteral("%1:%2").arg(QString::number(ix), screen->serialNumber());
+            bool match = requestedMonitor == screenId;
+            // Check if monitor's index changed
+            if (!match && !screen->serialNumber().isEmpty()) {
+                match = requestedMonitor.section(QLatin1Char(':'), 1) == screen->serialNumber();
+            }
+            if (match) {
+                if (m_id == Kdenlive::ProjectMonitor) {
+                    KdenliveSettings::setProject_monitor_fullscreen(screenId);
+                } else {
+                    KdenliveSettings::setClip_monitor_fullscreen(screenId);
+                }
+                return screen;
+            }
+        }
+    }
+
+    ix = 0;
+    for (const QScreen *screen : qApp->screens()) {
+        // Autodetect second monitor
+        QRect screenRect = screen->geometry();
+        if (screenRect.contains(pCore->window()->geometry().center())) {
+            // This is our current monitor, but we have at least 2 and want to find a different one
+            ix++;
+            continue;
+        }
+        QString screenId = QStringLiteral("%1:%2").arg(QString::number(ix), screen->serialNumber());
+        if (qApp->screens().count() > 2) {
+            // We have 3 monitors, use each
+            if (m_id == Kdenlive::ProjectMonitor) {
+                if (KdenliveSettings::clip_monitor_fullscreen().isEmpty()) {
+                    KdenliveSettings::setProject_monitor_fullscreen(screenId);
+                } else {
+                    if (KdenliveSettings::clip_monitor_fullscreen() == screenId) {
+                        continue;
+                    }
+                }
+            } else {
+                if (KdenliveSettings::project_monitor_fullscreen().isEmpty()) {
+                    KdenliveSettings::setClip_monitor_fullscreen(screenId);
+                } else {
+                    if (KdenliveSettings::project_monitor_fullscreen() == screenId) {
+                        continue;
+                    }
+                }
+            }
+        } else {
+            // Move monitor widget to the second screen (one screen for Kdenlive, the other one for the Monitor widget)
+            if (m_id == Kdenlive::ProjectMonitor) {
+                KdenliveSettings::setProject_monitor_fullscreen(screenId);
+            } else {
+                KdenliveSettings::setClip_monitor_fullscreen(screenId);
+            }
+        }
+        return screen;
+    }
+
+    return nullptr;
+}
+
 void Monitor::slotSwitchFullScreen(bool minimizeOnly)
 {
     m_glMonitor->refreshZoom = true;
     if (!m_glWidget->isFullScreen() && !minimizeOnly) {
-        // Move monitor widget to the second screen (one screen for Kdenlive, the other one for the Monitor widget)
-        if (qApp->screens().count() > 1) {
-            QString requestedMonitor = KdenliveSettings::fullscreen_monitor();
-            if (m_id == Kdenlive::ProjectMonitor) {
-                if (!KdenliveSettings::project_monitor_fullscreen().isEmpty()) {
-                    requestedMonitor = KdenliveSettings::project_monitor_fullscreen();
-                }
-            } else {
-                if (!KdenliveSettings::clip_monitor_fullscreen().isEmpty()) {
-                    requestedMonitor = KdenliveSettings::clip_monitor_fullscreen();
-                }
-            }
-            bool screenFound = false;
-            int ix = -1;
-            if (!requestedMonitor.isEmpty()) {
-                // If the platform does now provide screen serial number, use indexes
-                for (const QScreen *screen : qApp->screens()) {
-                    ix++;
-                    bool match = requestedMonitor == QStringLiteral("%1:%2").arg(QString::number(ix), screen->serialNumber());
-                    // Check if monitor's index changed
-                    if (!match && !screen->serialNumber().isEmpty()) {
-                        match = requestedMonitor.section(QLatin1Char(':'), 1) == screen->serialNumber();
-                    }
-                    if (match) {
-                        // Match
-                        m_glWidget->setParent(nullptr);
-                        m_glWidget->move(screen->geometry().topLeft());
-                        m_glWidget->resize(screen->geometry().size());
-                        screenFound = true;
-                        if (m_id == Kdenlive::ProjectMonitor) {
-                            KdenliveSettings::setProject_monitor_fullscreen(QStringLiteral("%1:%2").arg(QString::number(ix), screen->serialNumber()));
-                        } else {
-                            KdenliveSettings::setClip_monitor_fullscreen(QStringLiteral("%1:%2").arg(QString::number(ix), screen->serialNumber()));
-                        }
-                        break;
-                    }
-                }
-            }
-            if (!screenFound) {
-                ix = 0;
-                for (const QScreen *screen : qApp->screens()) {
-                    // Autodetect second monitor
-                    QRect screenRect = screen->geometry();
-                    if (!screenRect.contains(pCore->window()->geometry().center())) {
-                        if (qApp->screens().count() > 2) {
-                            // We have 3 monitors, use each
-                            if (m_id == Kdenlive::ProjectMonitor) {
-                                if (KdenliveSettings::clip_monitor_fullscreen().isEmpty()) {
-                                    KdenliveSettings::setProject_monitor_fullscreen(QStringLiteral("%1:%2").arg(QString::number(ix), screen->serialNumber()));
-                                } else {
-                                    if (KdenliveSettings::clip_monitor_fullscreen() ==
-                                        QStringLiteral("%1:%2").arg(QString::number(ix), screen->serialNumber())) {
-                                        continue;
-                                    }
-                                }
-                            } else {
-                                if (KdenliveSettings::project_monitor_fullscreen().isEmpty()) {
-                                    KdenliveSettings::setClip_monitor_fullscreen(QStringLiteral("%1:%2").arg(QString::number(ix), screen->serialNumber()));
-                                } else {
-                                    if (KdenliveSettings::project_monitor_fullscreen() ==
-                                        QStringLiteral("%1:%2").arg(QString::number(ix), screen->serialNumber())) {
-                                        continue;
-                                    }
-                                }
-                            }
 
-                        } else {
-                            if (m_id == Kdenlive::ProjectMonitor) {
-                                KdenliveSettings::setProject_monitor_fullscreen(QStringLiteral("%1:%2").arg(QString::number(ix), screen->serialNumber()));
-                            } else {
-                                KdenliveSettings::setClip_monitor_fullscreen(QStringLiteral("%1:%2").arg(QString::number(ix), screen->serialNumber()));
-                            }
-                        }
-                        m_glWidget->setParent(nullptr);
-                        m_glWidget->move(screenRect.topLeft());
-                        m_glWidget->resize(screenRect.size());
-                        screenFound = true;
-                        break;
-                    }
-                    ix++;
-                }
-            }
-            if (!screenFound) {
-                m_glWidget->setParent(nullptr);
-            }
-        } else {
-            m_glWidget->setParent(nullptr);
+        const QScreen *screen = getScreenForFullscreen();
+        m_glWidget->setParent(nullptr);
+        if (screen) {
+            m_glWidget->move(screen->geometry().topLeft());
+            m_glWidget->resize(screen->geometry().size());
         }
         m_glWidget->showFullScreen();
         m_glMonitor->enableMouseTimer(true);
